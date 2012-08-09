26c6bae76d16580574ae044e33eb3cd5ffe67f16
kanda <kanda@havit.tfs.local>
2012-08-09 15:08:24 +0000

    ObjectInfo (dodatek).

---
+++ Havit.Business/ObjectInfo.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Havit.Business
+{
+	/// <summary>
+	/// Reprezentuje informace o objektu (tøídì).
+	/// </summary>
+	public class ObjectInfo
+	{
+		/// <summary>
+		/// Vytvoøí instanci tøídy.
+		/// </summary>
+		/// <param name="dbSchema">Název schémata databázové tabulky.</param>
+		/// <param name="dbTable">Název databázové tabulky.</param>
+		/// <param name="readOnly">Urèuje, zda je tøída jen ke ètení.</param>
+		/// <param name="properties">Properties tøídy.</param>
+		public ObjectInfo(string dbSchema, string dbTable, bool readOnly, PropertyInfoCollection properties)
+		{
+			if (properties == null)
+				throw new ArgumentNullException("properties");
+
+			this.dbSchema = dbSchema;
+			this.dbTable = dbTable;
+			this.readOnly = readOnly;
+			this.properties = properties;
+
+			foreach (PropertyInfo propertyInfo in properties)
+			{
+				propertyInfo.Parent = this;
+			}
+		}
+
+		/// <summary>
+		/// Indikuje, zda je objekt urèen jen ke ètení.
+		/// </summary>
+		public bool ReadOnly
+		{
+			get { return readOnly; }
+		}
+		private bool readOnly;
+
+		/// <summary>
+		/// Název schématu databázové tabulky.
+		/// </summary>
+		public string DbSchema
+		{
+			get { return dbSchema; }
+		}
+		private string dbSchema;
+
+		/// <summary>
+		/// Název databázové tabulky.
+		/// </summary>
+		public string DbTable
+		{
+			get { return dbTable; }
+		}
+		private string dbTable;
+
+		/// <summary>
+		/// Property ve tøídì.
+		/// </summary>
+		public PropertyInfoCollection Properties
+		{
+			get { return properties; }
+		}
+		private PropertyInfoCollection properties;
+
+	}
+}

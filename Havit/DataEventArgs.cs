961c88277a6279fef34346e268c07290b680b333
kanda <kanda@havit.tfs.local>
2012-08-09 15:14:59 +0000

    DataEventArgs, DataEventHandler

---
+++ Havit/DataEventArgs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Havit
+{
+	/// <summary>
+	/// Argumenty události DataEventHandler, kterými si vyvolání události a její obsluha mohou předávat data (a to jedním i druhým směrem).
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class DataEventArgs<T> : EventArgs
+	{
+		#region Data (property)
+		/// <summary>
+		/// Datový objekt, který se předává mezi volajícím a volaným.
+		/// </summary>
+		public T Data { get; set; }
+		#endregion
+
+		#region Contructors
+		/// <summary>
+		/// Konstruktor.
+		/// </summary>
+		public DataEventArgs()
+		{
+
+		}
+
+		/// <summary>
+		/// Konstruktor.
+		/// </summary>
+		public DataEventArgs(T data)
+		{
+			this.Data = data;
+		}
+		#endregion
+	}
+}
---
+++ Havit/DataEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Havit
+{
+	/// <summary>
+	/// Delegát pro událost předávající mezi volajícím a volaným datový objekt. 
+	/// </summary>
+	/// <typeparam name="T">Typ předávaného objektu.</typeparam>
+	/// <param name="sender">Volající.</param>
+	/// <param name="dataEventArgs">Argumenty události předávající datový objekt (pro jeden směr) nebo očekávající nastavení datového objektu (pro druhý směr).</param>
+	public delegate void DataEventHandler<T>(object sender, DataEventArgs<T> dataEventArgs);
+}

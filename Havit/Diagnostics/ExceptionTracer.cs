947937668217e2fab53d0b3a323bbe95b6a8560d
Haken, Robert <haken@havit.cz>
2012-08-09 15:11:05 +0000

    ExceptionTracer - před převodem na VS2008

---
+++ Havit/Diagnostics/ExceptionTracer.cs
@@ -0,0 +1,259 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Havit.Diagnostics
+{
+	/// <summary>
+	/// Tøída zajišující posílání vıjimek do trace prostøednictvím TraceSource.
+	/// Mimo explicitního volání lze tøídu pøihlásit k odbìru neošetøenıch vıjimek, vèetnì WinForms.
+	/// <example>
+	/// Pøíklad pouití v ConsoleApplication:
+	/// <code>
+	/// namespace ExceptionLogging
+	/// {
+	///		class Program
+	///		{
+	///			static void Main(string[] args)
+	///			{
+	///				ExceptionTracer.Default.SubscribeToUnhandledExceptions(false);
+	///
+	///				ExceptionTracer.Default.TraceException(new ArgumentNullException("param", "Chybááá!"));
+	///
+	///				throw new InvalidOperationException("Chybka!");
+	///			}
+	///		}
+	/// }
+	/// </code>
+	/// Pøíklad pouití ve WindowsApplication:
+	/// <code>
+	///	static void Main()
+	///	{
+	///		ExceptionTracer.Default.SubscribeToUnhandledExceptions(true);
+	///
+	///		Application.EnableVisualStyles();
+	///		Application.SetCompatibleTextRenderingDefault(false);
+	///		Application.Run(new Form1());
+	///	}
+	/// </code>
+	/// Pøíklad konfigurace App.config:
+	/// <code>
+	/// &lt;configuration&gt;
+	///		&lt;system.diagnostics&gt;
+	///			&lt;sources&gt;
+	///				&lt;source name="Exceptions" switchValue="Error"&gt;
+	///					&lt;listeners&gt;
+	///						&lt;add name="LogFileListener"
+	///							type="System.Diagnostics.TextWriterTraceListener"
+	///							 initializeData="Exceptions.log"
+	///						/&gt;
+	///						&lt;add name="XmlListener"
+	///							 initializeData="Exceptions.xml"
+	///							 type="System.Diagnostics.XmlWriterTraceListener"
+	///					/&gt;
+	///					&lt;/listeners&gt;
+	///				&lt;/source&gt;
+	///			&lt;/sources&gt;
+	///		&lt;/system.diagnostics&gt;
+	/// &lt;/configuration&gt;
+	/// </code>
+	/// </example>
+	/// </summary>
+	public class ExceptionTracer
+	{
+		private const TraceEventType traceExceptionMethodDefaultEventType = TraceEventType.Error;
+		private const int traceExceptionMethodDefaultEventId = 0;
+
+		#region TraceSourceName
+		/// <summary>
+		/// Jméno TraceSource, pøes kterı se budou vıjimky emitovat.
+		/// </summary>
+		public string TraceSourceName
+		{
+			get;
+			private set;
+		}
+		#endregion
+
+		#region constructor
+		/// <summary>
+		/// Vytvoøí instanci ExceptionTraceru, kterı bude svùj vıstup smìøovat pøes TraceSource se zadanım jménem.
+		/// </summary>
+		/// <param name="traceSourceName">jméno TraceSource, pøes kterı se budou vıjimky emitovat</param>
+		public ExceptionTracer(string traceSourceName)
+		{
+			this.TraceSourceName = traceSourceName;
+		}
+		#endregion
+
+		#region SubscribeToUnhandledExceptions
+		/// <summary>
+		/// Pøihlásí ExceptionTracer k odbìru všech neobslouenıch vıjimek (event AppDomain.CurrentDomain.UnhandledException).
+		/// </summary>
+		public void SubscribeToUnhandledExceptions(bool includeWindowsFormsThreadExceptions)
+		{
+			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+			if (includeWindowsFormsThreadExceptions)
+			{
+				SubscribeToWindowsFormsThreadExceptions();
+			}
+		}
+
+		/// <summary>
+		/// Obsluha události AppDomain.CurrentDomain.UnhandledException.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.UnhandledExceptionEventArgs"/> instance containing the event data.</param>
+		private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+		{
+			if (e.ExceptionObject is Exception)
+			{
+				TraceException((Exception)e.ExceptionObject, TraceEventType.Critical);
+			}
+		}
+		#endregion
+
+		#region SubscribeToWindowsFormsThreadExceptions
+		/// <summary>
+		/// Pøihlásí ExceptionTracer k odbìru všech neobslouenıch vıjimek WinForm (event Application.ThreadException).
+		/// </summary>
+		public void SubscribeToWindowsFormsThreadExceptions()
+		{
+			Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+		}
+
+		/// <summary>
+		/// Obsluha události Application.ThreadException.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.Threading.ThreadExceptionEventArgs"/> instance containing the event data.</param>
+		private void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+		{
+			TraceException(e.Exception, TraceEventType.Critical);
+
+			// pùvodní implementace obsluhy vıjimky
+			using (ThreadExceptionDialog excptDlg = new ThreadExceptionDialog(e.Exception))
+			{
+				DialogResult result = excptDlg.ShowDialog();
+				if (result == DialogResult.Abort)
+				{
+					Application.Exit();
+				}
+			}
+		}
+		#endregion
+
+		#region TraceException
+		/// <summary>
+		/// Pošle do trace zadanou vıjimku.
+		/// </summary>
+		/// <param name="exception">vıjimka k zaznamenání</param>
+		/// <param name="eventType">typ události, pod kterım se má vıjimka zaznamenat</param>
+		/// <param name="eventId">ID eventu, pod kterım se má vıjimka zaznamenat</param>
+		public void TraceException(Exception exception, TraceEventType eventType, int eventId)
+		{
+			if (exception == null)
+			{
+				throw new ArgumentNullException("exception");
+			}
+
+			RunUsingTraceSource(delegate(TraceSource ts)
+			{
+				ts.TraceEvent(eventType, eventId, FormatException(exception));
+			});
+		}
+
+		/// <summary>
+		/// Pošle do trace zadanou vıjimku.
+		/// </summary>
+		/// <param name="exception">vıjimka k zaznamenání</param>
+		/// <param name="eventType">typ události, pod kterım se má vıjimka zaznamenat</param>
+		public void TraceException(Exception exception, TraceEventType eventType)
+		{
+			TraceException(exception, eventType, traceExceptionMethodDefaultEventId);
+		}
+
+
+		/// <summary>
+		/// Pošle do trace zadanou vıjimku.
+		/// </summary>
+		/// <param name="exception">vıjimka k zaznamenání</param>
+		public void TraceException(Exception exception)
+		{
+			TraceException(exception, traceExceptionMethodDefaultEventType, traceExceptionMethodDefaultEventId);
+		}
+		#endregion
+
+		#region FormatException (private)
+		/// <summary>
+		/// Naformátuje vıjimku pro zápis do trace.
+		/// </summary>
+		/// <param name="exception">vıjimka</param>
+		/// <returns>textovı vıstup, kterı se pošle do trace (informace o vıjimce)</returns>
+		private string FormatException(Exception exception)
+		{
+			// do budoucna je moné rozšíøit objektovı model o ExceptionTraceFormatter, atp.
+			return exception.ToString();
+		}
+		#endregion
+
+		#region RunUsingTraceSource (private)
+		/// <summary>
+		/// Vykoná akci pomocí TraceSource pouívaného ExceptionListenerem.
+		/// </summary>
+		/// <param name="action">akce k vykonání (delegát)</param>
+		private void RunUsingTraceSource(Action<TraceSource> action)
+		{
+			Debug.Assert(action != null);
+
+			TraceSource ts = new TraceSource(this.TraceSourceName);
+
+			action(ts);
+
+			ts.Flush();
+			ts.Close();
+		}
+		#endregion
+
+		/*********************************************************************/
+
+		#region DefaultTraceSourceName (const)
+		/// <summary>
+		/// Název vıchozího TraceSource, pøes kterı jsou vıjimky emitovány.
+		/// </summary>
+		public const string DefaultTraceSourceName = "Exceptions";
+		#endregion
+
+		#region Default (static singleton)
+		/// <summary>
+		/// Vıchozí ExceptionTracer smìøující vıstup pøes TraceSource s DefaultTraceSourceName.
+		/// </summary>
+		public static ExceptionTracer Default
+		{
+			get
+			{
+				if (_default == null)
+				{
+					lock (defaultLock)
+					{
+						if (_default == null)
+						{
+							_default = new ExceptionTracer(DefaultTraceSourceName);
+						}
+					}
+				}
+				return _default;
+			}
+			set
+			{
+				_default = value;
+			}
+		}
+		private static ExceptionTracer _default;
+		private static object defaultLock = new object();
+		#endregion
+	}
+}

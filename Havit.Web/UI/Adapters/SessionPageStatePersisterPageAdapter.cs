6883cc0f0755774ad37cf15aab9daede64ac16e9
Haken, Robert <haken@havit.cz>
2012-08-09 15:09:05 +0000

    NEW: SessionPageStatePersisterPageAdapter

---
+++ Havit.Web/UI/Adapters/SessionPageStatePersisterPageAdapter.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.UI.Adapters;
+using System.Web.UI;
+
+namespace Havit.Web.UI.Adapters
+{
+	/// <summary>
+	/// <see cref="PageAdapter"/>, který jako <see cref="PageStatePersister"/> v metodì <see cref="GetStatePersister()"/>
+	/// vrací <see cref="SessionPageStatePersister"/>.
+	/// </summary>
+	public class SessionPageStatePersisterPageAdapter : PageAdapter
+	{
+		/// <summary>
+		/// Returns an object that is used by the Web page to maintain the control and view states. 
+		/// </summary>
+		/// <returns>
+		/// An object derived from <see cref="PageStatePersister"/> that supports creating and extracting the combined control and view states for the <see cref="Page"/>.
+		/// </returns>
+		public override PageStatePersister GetStatePersister()
+		{
+			return new SessionPageStatePersister(this.Page);
+		}
+	}
+}

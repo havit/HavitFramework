dd3206393eb79e4d31600c8e1711a4b4417009a0
Haken, Robert <haken@havit.cz>
2012-08-09 15:08:04 +0000

    Založení třídy PropertyHolder.cs

---
+++ Havit.Business/PropertyHolder.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Havit.Business
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class PropertyHolder<T>
+	{
+
+	}
+}

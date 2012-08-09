4de10de1e5310eb55bd5d54855371141ea417ba1
kanda <kanda@havit.tfs.local>
2012-08-09 15:14:25 +0000

    SoapExceptionHealthMonitoringHandler

---
+++ Havit.Web/Services/Extenstions/SoapExceptionHealthMonitoringHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Management;
+using System.Web.Services.Protocols;
+
+namespace Havit.Web.Services.Extenstions
+{
+	/// <summary>
+	/// V případě chyby ve zpracování web metody (webové služby) zajistí oznámení chyby health monitoringem.
+	/// </summary>
+	public class SoapExceptionHealthMonitoringHandler : System.Web.Services.Protocols.SoapExtension
+	{
+		#region ProcessMessage
+		public override void ProcessMessage(System.Web.Services.Protocols.SoapMessage message)
+		{
+			try
+			{
+				if ((message != null) && (message.Stage == SoapMessageStage.AfterSerialize))
+				{
+					if (message.Exception != null)
+					{
+						Exception exception = message.Exception;
+						if ((exception is SoapException) && (exception.InnerException != null))
+						{
+							exception = exception.InnerException;
+						}
+						new RecoveredWebRequestErrorEvent(exception.Message, message, exception).Raise();
+					}
+				}
+			}
+			catch // pokud by zde nedejbože došlo k nějaké další výjimce, tak ji zamaskujeme
+			{
+				// NOOP
+			}
+		}
+		#endregion
+
+		#region GetInitializer
+		public override object GetInitializer(Type serviceType)
+		{
+			return null;
+		}
+
+		public override object GetInitializer(LogicalMethodInfo methodInfo, SoapExtensionAttribute attribute)
+		{
+			return null;
+		}
+		#endregion
+
+		#region Initialize
+		public override void Initialize(object initializer)
+		{
+		}
+		#endregion
+
+		#region RecoveredWebRequestErrorEvent (nested class)
+		/// <summary>
+		/// Pomocná třída pro oznámení události health monitoringem.
+		/// </summary>
+		private class RecoveredWebRequestErrorEvent : WebRequestErrorEvent
+		{
+			public RecoveredWebRequestErrorEvent(string message, object eventSource, Exception exception)
+				: base(message, eventSource, WebEventCodes.WebExtendedBase + 999, exception)
+			{
+			}
+		}
+		#endregion
+	}
+
+}

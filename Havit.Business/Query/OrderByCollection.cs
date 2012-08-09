0b96fd83cd22eeee28ef62cd7f9de4cfafee030f
kanda <kanda@havit.tfs.local>
2012-08-09 15:08:44 +0000

    Zavedení OrderByCollection.

---
+++ Havit.Business/Query/OrderByCollection.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Havit.Collections;
+
+namespace Havit.Business.Query
+{
+	public class OrderByCollection: Havit.Collections.SortItemCollection
+	{
+		/// <summary>
+		/// Pøidá na konec kolekce položku pro vzestupné øazení.
+		/// </summary>
+		public void Add(FieldPropertyInfo propertyInfo)
+		{
+			Add(propertyInfo, SortDirection.Ascending);
+		}
+
+		/// <summary>
+		/// Pøidá na konec kolekce položku pro øazení.
+		/// </summary>
+		public void Add(FieldPropertyInfo propertyInfo, SortDirection direction)
+		{
+			Add(new FieldPropertySortItem(propertyInfo, direction));			
+		}
+
+		/// <summary>
+		/// Pøidá do kolekce položku pro vzestuné øazení.
+		/// </summary>
+		public void Insert(int index, FieldPropertyInfo propertyInfo)
+		{
+			Insert(index, propertyInfo, SortDirection.Ascending);
+		}
+
+		/// <summary>
+		/// Pøidá do kolekce položku pro øazení.
+		/// </summary>
+		public void Insert(int index, FieldPropertyInfo propertyInfo, SortDirection direction)
+		{
+			Insert(index, new FieldPropertySortItem(propertyInfo, direction));			
+		}
+	}
+}

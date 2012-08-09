675c4cbb21d72a837d11f2db39102e8e464e96e8
kanda <kanda@havit.tfs.local>
2012-08-09 15:11:45 +0000

    SqlSmallDateTime

---
+++ Havit.Data/SqlTypes/SqlSmallDateTime.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Havit.Data.SqlTypes
+{
+    /// <summary>
+    /// Reprezentuje pomocné hodnoty pro databázový typ smalldatetime.
+    /// </summary>
+    public class SqlSmallDateTime
+    {
+        /// <summary>
+        /// Minimální hodnota použitelná pro databázový typ smalldatetime.
+        /// </summary>
+        public static readonly SqlSmallDateTime MinValue = new SqlSmallDateTime(new DateTime(1900, 1, 1));
+
+        /// <summary>
+        /// Maximální hodnota použitelná pro databázový typ smalldatetime.
+        /// </summary>
+        public static readonly SqlSmallDateTime MaxValue = new SqlSmallDateTime(new DateTime(2079, 6, 6, 23, 59, 00));
+
+        #region Constructor
+        private SqlSmallDateTime(DateTime value)
+        {
+            _value = value;
+        }
+        #endregion
+
+        #region Value
+        /// <summary>
+        /// Hodnota reprezentovaná jako DateTime.
+        /// </summary>
+        public DateTime Value
+        {
+            get
+            {
+                return _value;
+            }
+        }
+        private DateTime _value;
+        #endregion
+    }
+}

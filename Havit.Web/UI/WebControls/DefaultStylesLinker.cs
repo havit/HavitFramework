f8b11e0e6c7683cd4a54f04c52460b8941c93212
Haken, Robert <haken@havit.cz>
2012-08-09 15:10:50 +0000

    NEW: DefaultStylesLinker

---
+++ Havit.Web/UI/WebControls/DefaultStylesLinker.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Web.UI;
+
+[assembly: WebResource("Havit.Web.UI.WebControls.DefaultStyles.css", "text/css")]
+
+namespace Havit.Web.UI.WebControls
+{
+	/// <summary>
+	/// Control, který vytvoøí odkazy (link) na výchozí CSS styly HAVIT umístìné ve frameworkové assembly.
+	/// </summary>
+	/// <example>
+	/// <code>
+	/// &lt;head runat="server"&gt;
+	///		&lt;havit:DefaultStylesLinker runat="server"&gt;
+	/// &lt;/head&gt;
+	/// </code>
+	/// </example>
+	public class DefaultStylesLinker : CompositeControl
+	{
+		/// <summary>
+		/// Vytvoøí child-controly.
+		/// </summary>
+		protected override void CreateChildControls()
+		{
+			HtmlLink defaultStylesHtmlLink = new HtmlLink();
+			defaultStylesHtmlLink.Href = Page.ClientScript.GetWebResourceUrl(typeof(DefaultStylesLinker), "Havit.Web.UI.WebControls.DefaultStyles.css");
+			defaultStylesHtmlLink.Attributes.Add("type", "text/css");
+
+			this.Controls.Add(defaultStylesHtmlLink);
+		}
+	}
+}
---
+++ WebApplicationTest/DefaultStylesLinkerTest.aspx
@@ -0,0 +1,16 @@
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DefaultStylesLinkerTest.aspx.cs" Inherits="WebApplicationTest.DefaultStylesLinkerTest" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Untitled Page</title>
+    <havit:DefaultStylesLinker runat="server" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    </div>
+    </form>
+</body>
+</html>

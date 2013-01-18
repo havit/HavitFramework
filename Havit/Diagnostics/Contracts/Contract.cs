f81cc3f38bba5232e07f32f3a8bdda2b665ab884
kanda <kanda@havit.tfs.local>
2013-01-18 08:28:06 +0000

    Havit Code Contracts

---
+++ Havit/Diagnostics/Contracts/Contract.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Havit.Diagnostics.Contracts
+{
+	/// <summary>
+	/// Contracts. Metody pro zajištění podmínek v kódu za běhu aplikace.
+	/// Nahrazuje standardní CodeContracts, jejichž rewriter trvá velmi dlouho.
+	/// Na rozdíl od CodeContracs se z popisu chyby nedozvíme, o jakou chybu skutečně jde (není znění podmínky).
+	/// </summary>
+	public static class Contract
+	{
+		#region Requires
+		/// <summary>
+		/// Pokud není podmínka condition splněna (hodnota je false), vyhodí výjimku ContractException.
+		/// </summary>
+		public static void Requires(bool condition, string userMessage = null)
+		{
+			if (!condition)
+			{
+				ThrowException<ContractException>(userMessage);
+			}
+		}
+
+		/// <summary>
+		/// Pokud není podmínka condition splněna (hodnota je false), vyhodí výjimku TException.
+		/// </summary>
+		/// <typeparam name="TException">Typ výjimky, která je v případě nesplnění podmínky vyhozena.</typeparam>
+		public static void Requires<TException>(bool condition, string userMessage = null)
+			where TException : Exception
+		{
+			if (!condition)
+			{
+				ThrowException<TException>(userMessage);
+			}
+		}
+		#endregion
+
+		#region Assert
+		/// <summary>
+		/// Pokud není podmínka condition splněna (hodnota je false), vyhodí výjimku ContractException.
+		/// </summary>
+		public static void Assert(bool condition, string userMessage = null)
+		{
+			if (!condition)
+			{
+				ThrowException<ContractException>(userMessage);
+			}
+		}
+		#endregion
+
+		#region ThrowException
+		private static void ThrowException<TException>(string message)
+			where TException : Exception
+		{
+			string exceptionMessage = String.IsNullOrEmpty(message) ? "Contract failed." : "Contract failed: " + message;
+			Exception resultException;
+			try
+			{
+				resultException = (Exception)Activator.CreateInstance(typeof(TException), exceptionMessage);
+			}
+			catch
+			{
+				resultException = new ContractException(exceptionMessage);
+			}
+
+			throw resultException;
+		}
+		#endregion
+	}
+}
---
+++ Havit/Diagnostics/Contracts/ContractException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Havit.Diagnostics.Contracts
+{
+	/// <summary>
+	/// Výjimka vyhazovaná třídou Contract v případě nespnění podmínky contractu.
+	/// </summary>
+	public sealed class ContractException : Exception
+	{
+		#region Constructors
+		/// <summary>
+		/// Konstructor.
+		/// </summary>
+		public ContractException(string message) : base(message)
+		{
+		}
+
+		/// <summary>
+		/// Konstruktor.
+		/// </summary>
+		public ContractException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+
+		private ContractException() : base()
+		{
+		}
+
+		private ContractException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+		}
+		#endregion
+
+	}
+}

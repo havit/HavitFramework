56bdfb5c6ba22ed3275c1d1f86e0d062676c3ba0
karda <karda@havit.tfs.local>
2013-03-14 09:59:47 +0000

    Task 4117:Komponenta pro načítání dat z obchodního rejstříku

---
+++ Havit.Services.Ares/ObchodniRejstrik.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Xml.Linq;
+
+namespace Havit.Services.Ares
+{
+	/// <summary>
+	/// Třída implementující načítání dat z obchodního rejstříku (ARES).
+	/// </summary>
+	public class ObchodniRejstrik
+	{
+		#region Const
+		private const string AresIcoRequestUrl = "http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico="; 
+		#endregion
+
+		#region Private members
+		/// <summary>
+		/// IČ subjektu, kterého údaje chceme získat.
+		/// </summary>
+		private string Ico { get; set; }
+
+		/// <summary>
+		/// Odklad vykonání requestu v sekundách.
+		/// </summary>
+		private int RequestDelay { get; set; }
+		#endregion
+
+		#region ObchodniRejstrik
+		/// <summary>
+		/// Konstruktor.
+		/// </summary>
+		/// <param name="ico">IČO společnosti.</param>
+		/// <param name="requestDelay">Odloží vykonání requestu o požadovaný čas v sekundách (default 0 s).</param>
+		public ObchodniRejstrik(string ico, int requestDelay = 0)
+		{
+			Ico = ico;
+			RequestDelay = requestDelay;
+		}
+		#endregion
+
+		#region GetAresResponseXDocument
+		/// <summary>
+		/// Odešle dotaz do obchodního rejstříku pro dané IČ a vrátí odpověd jako XDocument objekt.
+		/// </summary>
+		private XDocument GetAresResponseXDocument()
+		{
+			XDocument aresResponseXDocument = null;
+
+			string requestUrl = String.Format("{0}{1}", AresIcoRequestUrl, Ico);
+
+			if (RequestDelay > 0)
+			{
+				Thread.Sleep(RequestDelay * 1000); // s -> ms
+			}
+
+			try
+			{
+				WebRequest aresRequest = HttpWebRequest.Create(requestUrl);
+				HttpWebResponse aresResponse = (HttpWebResponse)aresRequest.GetResponse();
+
+				aresResponseXDocument = XDocument.Load(new StreamReader(aresResponse.GetResponseStream()));
+			}
+			catch (WebException e)
+			{
+				throw new ApplicationException(String.Format("Chyba \"{0}\" při pokusu o získání dat ze služby ARES ({1}).", e.Message, requestUrl));
+
+			}
+			catch (Exception e)
+			{
+				throw new ApplicationException(String.Format("Chyba \"{0}\" při pokusu o získání dat ze služby ARES ({1}).", e.Message, requestUrl));
+			}
+
+			return aresResponseXDocument;
+		}
+		#endregion
+
+		#region GetObchodniRejstrikResponse
+		/// <summary>
+		/// Vrací strukturovanou odpověd z obchodního rejstříka.
+		/// </summary>
+		public ObchodniRejstrikResponse GetObchodniRejstrikResponse()
+		{
+			ObchodniRejstrikResponse obchodniRejstrikResponse = new ObchodniRejstrikResponse();
+
+			XDocument aresResponseXDocument = GetAresResponseXDocument();
+
+			if (aresResponseXDocument.Root != null)
+			{
+				var dataTypesNamespace = aresResponseXDocument.Root.Elements().Select(item => item.GetNamespaceOfPrefix("D")).FirstOrDefault();
+
+				if (dataTypesNamespace != null)
+				{
+					XElement eElement = aresResponseXDocument.Descendants(XName.Get("E", dataTypesNamespace.ToString())).FirstOrDefault();
+					if (eElement != null)
+					{
+						// vyplníme vlastnost chyby a vrátíme objekt odpovědi (další elementy nezpracovávame)
+						obchodniRejstrikResponse.ResponseErrorMessage = eElement.Value;
+						return obchodniRejstrikResponse;
+					}
+
+					obchodniRejstrikResponse.Ico = (string)aresResponseXDocument.Descendants(XName.Get("ICO", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.Dic = (string)aresResponseXDocument.Descendants(XName.Get("DIC", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.NazevObchodniFirmy = (string)aresResponseXDocument.Descendants(XName.Get("OF", dataTypesNamespace.ToString())).FirstOrDefault();
+					
+					XElement dvElement = aresResponseXDocument.Descendants(XName.Get("DV", dataTypesNamespace.ToString())).FirstOrDefault();
+
+					if (dvElement != null)
+					{
+						obchodniRejstrikResponse.DenZapisu = DateTime.ParseExact(dvElement.Value, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+					}
+
+					obchodniRejstrikResponse.NazevSoudu = (string)aresResponseXDocument.Descendants(XName.Get("T", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.KodSoudu = (string)aresResponseXDocument.Descendants(XName.Get("K", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.SpisovaZnacka = (string)aresResponseXDocument.Descendants(XName.Get("OV", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.PravniForma = (string)aresResponseXDocument.Descendants(XName.Get("NPF", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.StavSubjektu = (string)aresResponseXDocument.Descendants(XName.Get("SSU", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.SidloUlice = (string)aresResponseXDocument.Descendants(XName.Get("NU", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.SidloCisloPopisne = (string)aresResponseXDocument.Descendants(XName.Get("CD", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.SidloCisloOrientacni = (string)aresResponseXDocument.Descendants(XName.Get("CO", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.SidloMesto = (string)aresResponseXDocument.Descendants(XName.Get("N", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.SidloMestskaCast = (string)aresResponseXDocument.Descendants(XName.Get("NCO", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.SidloPsc = (string)aresResponseXDocument.Descendants(XName.Get("PSC", dataTypesNamespace.ToString())).FirstOrDefault();
+					obchodniRejstrikResponse.SidloStat = (string)aresResponseXDocument.Descendants(XName.Get("NS", dataTypesNamespace.ToString())).FirstOrDefault();
+				}
+			}
+			
+			return obchodniRejstrikResponse;
+		}
+		#endregion
+	}
+}
---
+++ Havit.Services.Ares/ObchodniRejstrikResponse.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Services.Ares
+{
+	/// <summary>
+	/// Strongtypová obálko pro data z odpovědi ze služby ARES - Obchodní rejstřík.
+	/// </summary>
+	public class ObchodniRejstrikResponse
+	{
+		/// <summary>
+		/// IČO obchodní firmy zapsané v OR.
+		/// </summary>
+		public string Ico { get; set; }
+
+		/// <summary>
+		/// DIČ obchodní firmy zapsané v OR.
+		/// </summary>
+		public string Dic { get; set; }
+
+		/// <summary>
+		/// Název pod kterým je firma zapsaná v OR.
+		/// </summary>
+		public string NazevObchodniFirmy { get; set; }
+
+		/// <summary>
+		/// Den zápisu firmy do OR.
+		/// </summary>
+		public DateTime DenZapisu { get; set; }
+
+		/// <summary>
+		/// Název soudu kterým je firma registrovaná v OR.
+		/// </summary>
+		public string NazevSoudu { get; set; }
+
+		/// <summary>
+		/// Kód soudu kterým je firma registrovaná v OR.
+		/// </summary>
+		public string KodSoudu { get; set; }
+
+		/// <summary>
+		/// Spisová značka pod kterou je firma v OR vedena (oddíl + vložka).
+		/// </summary>
+		public string SpisovaZnacka { get; set; }
+
+		/// <summary>
+		/// Právní forma firmy.
+		/// </summary>
+		public string PravniForma { get; set; }
+
+		/// <summary>
+		/// Stav subjektu v OR.
+		/// </summary>
+		public string StavSubjektu { get; set; }
+
+		/// <summary>
+		/// Ulice sídla firmy.
+		/// </summary>
+		public string SidloUlice { get; set; }
+
+		/// <summary>
+		/// Popisné číslo sídla firmy.
+		/// </summary>
+		public string SidloCisloPopisne { get; set; }
+
+		/// <summary>
+		/// Orientační číslo sídla firmy.
+		/// </summary>
+		public string SidloCisloOrientacni { get; set; }
+
+		/// <summary>
+		/// Město sídla firmy.
+		/// </summary>
+		public string SidloMesto { get; set; }
+
+		/// <summary>
+		/// Městký část sídla firmy.
+		/// </summary>
+		public string SidloMestskaCast { get; set; }
+
+		/// <summary>
+		/// PSČ sídla firmy.
+		/// </summary>
+		public string SidloPsc { get; set; }
+
+		/// <summary>
+		/// Stát sídla firmy.
+		/// </summary>
+		public string SidloStat { get; set; }
+
+		/// <summary>
+		/// Chybová zpráva odpovědi služby ARES.
+		/// </summary>
+		public string ResponseErrorMessage { get; set; }
+
+		/// <summary>
+		/// Indikuje, zda-li se vyskytla v odpovědi služby ARES - Obchodní rejstřík  chyba.
+		/// </summary>
+		public bool HasError
+		{
+			get { return !String.IsNullOrWhiteSpace(ResponseErrorMessage); }
+
+		}
+	}
+}

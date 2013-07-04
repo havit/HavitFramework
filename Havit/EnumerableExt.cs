6ebe2f1d23f668749031481f55423b1bf7715251
kanda <kanda@havit.tfs.local>
2013-07-04 09:34:57 +0000

    EnumerableExt (Left/Right/FullOuter-join) + unit testy

---
+++ Havit/EnumerableExt.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Havit
+{
+	/// <summary>
+	/// Extension metody pro IEnumerable&lt;T&gt;.
+	/// </summary>
+	public static class EnumerableExt
+	{
+		#region LeftJoin
+		/// <summary>
+		/// Left outer join.
+		/// </summary>
+		public static IEnumerable<TResult> LeftJoin<TLeft, TRight, TKey, TResult>(this IEnumerable<TLeft> leftSource,
+												 IEnumerable<TRight> rightSource,
+												 Func<TLeft, TKey> leftKeySelector,
+												 Func<TRight, TKey> rightKeySelector,
+												 Func<TLeft, TRight, TResult> resultSelector)
+		{
+			IEnumerable<TResult> _result =
+				from l in leftSource
+				join r in rightSource
+				on leftKeySelector(l) equals rightKeySelector(r) into joinData
+				from right in joinData.DefaultIfEmpty()
+				select resultSelector(l, right);
+
+			return _result;
+		}
+		#endregion
+
+		#region RightJoin
+		/// <summary>
+		/// Right outer join.
+		/// </summary>
+		public static IEnumerable<TResult> RightJoin<TLeft, TRight, TKey, TResult>(this IEnumerable<TLeft> leftSource,
+												 IEnumerable<TRight> rightSource,
+												 Func<TLeft, TKey> leftKeySelector,
+												 Func<TRight, TKey> rightKeySelector,
+												 Func<TLeft, TRight, TResult> resultSelector)
+		{
+			IEnumerable<TResult> _result =
+				from r in rightSource
+				join l in leftSource
+				on rightKeySelector(r) equals leftKeySelector(l) into joinData
+				from left in joinData.DefaultIfEmpty()
+				select resultSelector(left, r);
+
+			return _result;
+		}
+		#endregion
+
+		#region FullOuterJoin
+		/// <summary>
+		/// Full outer join.
+		/// Na rozdíl od ostatních metod je full outer join vyhodnocen okamžitě, výsledky nejsou ovlivněny změnou 
+		/// source dat (leftSource, rightSource) za zavoláním této metody a před vlastním vyhodnocením dotazu (IEnumerable).
+		/// </summary>
+		public static IEnumerable<TResult> FullOuterJoin<TLeft, TRight, TKey, TResult>(this IEnumerable<TLeft> leftSource,
+												 IEnumerable<TRight> rightSource,
+												 Func<TLeft, TKey> leftKeySelector,
+												 Func<TRight, TKey> rightKeySelector,
+												 Func<TLeft, TRight, TResult> resultSelector)
+		{
+			var leftLookup = leftSource.ToLookup(leftKeySelector);
+			var rightLookup = rightSource.ToLookup(rightKeySelector);
+
+			var keys = new HashSet<TKey>(leftLookup.Select(p => p.Key));
+			keys.UnionWith(rightLookup.Select(p => p.Key));
+
+			IEnumerable<TResult> _result = from key in keys
+					   from xLeft in leftLookup[key].DefaultIfEmpty()
+					   from xRight in rightLookup[key].DefaultIfEmpty()
+					   select resultSelector(xLeft, xRight);
+
+			return _result.ToList();
+		}
+		#endregion
+	}
+}
---
+++ HavitTest/EnumerableExtTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Havit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace HavitTest
+{
+	[TestClass]
+	public class EnumerableExtTest
+	{
+		[TestMethod]
+		public void LeftJoinTest()
+		{
+			IEnumerable<int> data1 = Enumerable.Range(1, 5).Concat(Enumerable.Range(1, 5)); // 1..5, 1..5
+			IEnumerable<int> data2 = Enumerable.Range(4, 6).Concat(Enumerable.Range(4, 6)); // 4..9, 4..9;
+
+			var joinedData1 = data1.LeftJoin(data2, left => left, right => right, (left, right) => new { Left = left, Right = right }).ToList();
+			var joinedData2 = data1.LeftJoin(data2, left => left, right => right, (left, right) => 0).ToList();
+			Assert.AreEqual(joinedData1.Count, 14);
+			Assert.AreEqual(joinedData2.Count, 14);
+		}
+		
+		[TestMethod]
+		public void RightJoinTest()
+		{
+			IEnumerable<int> data1 = Enumerable.Range(1, 5).Concat(Enumerable.Range(1, 5)); // 1..5, 1..5
+			IEnumerable<int> data2 = Enumerable.Range(4, 6).Concat(Enumerable.Range(4, 6)); // 4..9, 4..9;
+
+			var joinedData1 = data1.RightJoin(data2, left => left, right => right, (left, right) => new { Left = left, Right = right }).ToList();
+			var joinedData2 = data1.RightJoin(data2, left => left, right => right, (left, right) => 0).ToList();
+			Assert.AreEqual(joinedData1.Count, 16);
+			Assert.AreEqual(joinedData2.Count, 16);
+		}
+
+		[TestMethod]
+		public void FullJoinTest()
+		{
+			IEnumerable<int> data1 = Enumerable.Range(1, 5).Concat(Enumerable.Range(1, 5)); // 1..5, 1..5
+			IEnumerable<int> data2 = Enumerable.Range(4, 6).Concat(Enumerable.Range(4, 6)); // 4..9, 4..9;
+
+			var joinedData = data1.FullOuterJoin(data2, left => left, right => right, (left, right) => 0).ToList();
+			Assert.AreEqual(joinedData.Count, 22); // 5ka je v obou kolekcích
+		}
+
+	}
+}

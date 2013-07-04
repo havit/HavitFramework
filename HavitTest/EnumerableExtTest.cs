055bf322b679db38c738bfd0290c8e21a94d3ee1
kanda <kanda@havit.tfs.local>
2013-07-04 09:46:06 +0000

    Kosmetika.

--- HavitTest/EnumerableExtTest.cs
+++ HavitTest/EnumerableExtTest.cs
@@ -11,6 +11,7 @@ namespace HavitTest
 	[TestClass]
 	public class EnumerableExtTest
 	{
+		#region LeftJoinTest
 		[TestMethod]
 		public void LeftJoinTest()
 		{
@@ -22,7 +23,9 @@ namespace HavitTest
 			Assert.AreEqual(joinedData1.Count, 14);
 			Assert.AreEqual(joinedData2.Count, 14);
 		}
+		#endregion
 		
+		#region RightJoinTest
 		[TestMethod]
 		public void RightJoinTest()
 		{
@@ -34,16 +37,21 @@ namespace HavitTest
 			Assert.AreEqual(joinedData1.Count, 16);
 			Assert.AreEqual(joinedData2.Count, 16);
 		}
+		#endregion
 
+		#region FullJoinTest
 		[TestMethod]
 		public void FullJoinTest()
 		{
 			IEnumerable<int> data1 = Enumerable.Range(1, 5).Concat(Enumerable.Range(1, 5)); // 1..5, 1..5
 			IEnumerable<int> data2 = Enumerable.Range(4, 6).Concat(Enumerable.Range(4, 6)); // 4..9, 4..9;
 
-			var joinedData = data1.FullOuterJoin(data2, left => left, right => right, (left, right) => 0).ToList();
-			Assert.AreEqual(joinedData.Count, 22); // 5ka je v obou kolekcích
+			var joinedData1 = data1.FullOuterJoin(data2, left => left, right => right, (left, right) => new { Left = left, Right = right }).ToList();
+			var joinedData2 = data1.FullOuterJoin(data2, left => left, right => right, (left, right) => 0).ToList();
+			Assert.AreEqual(joinedData1.Count, 22); // 5ka je v obou kolekcích
+			Assert.AreEqual(joinedData2.Count, 22); // 5ka je v obou kolekcích
 		}
+		#endregion
 
 	}
 }

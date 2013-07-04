26c1203281c2ecbeface18d50cc370a7bc7de826
kanda <kanda@havit.tfs.local>
2013-07-04 10:51:28 +0000

    Změna namespace EnumerableExt

--- Havit/Linq/EnumerableExt.cs
+++ Havit/Linq/EnumerableExt.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Havit
+namespace Havit.Linq
 {
 	/// <summary>
 	/// Extension metody pro IEnumerable&lt;T&gt;.
--- HavitTest/EnumerableExtTest.cs
+++ HavitTest/EnumerableExtTest.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Havit;
+using Havit.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;

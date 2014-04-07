70492b99a92b2dff9a29a445e44ecd32d7d4d9cd
kanda <kanda@havit.tfs.local>
2014-04-07 15:26:34 +0000

    Stylecop kosmetika (zase, ach jo)

--- WebApplicationTest/HavitWebBootstrapTests/ValidatorsTest.aspx.cs
+++ WebApplicationTest/HavitWebBootstrapTests/ValidatorsTest.aspx.cs
@@ -9,16 +9,20 @@ namespace WebApplicationTest.HavitWebBootstrapTests
 {
 	public partial class ValidatorsTest : System.Web.UI.Page
 	{
+		#region OnLoad
 		protected override void OnLoad(EventArgs e)
 		{
 			base.OnLoad(e);
 			Form.DefaultButton = SectionBButton.UniqueID;
 		}
+		#endregion
 
+		#region CustomValidator_ServerValidate
 		protected void CustomValidator_ServerValidate(object source, ServerValidateEventArgs args)
 		{
 			args.IsValid = args.Value.StartsWith("a", StringComparison.CurrentCultureIgnoreCase);
 		}
+		#endregion
 
 	}
 }

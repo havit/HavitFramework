e6288e241b874a62176d2aed918f31c248bd706a
kanda <kanda@havit.tfs.local>
2014-07-28 08:25:50 +0000

    Kosmetika

--- Havit.Web/Bootstrap/UI/WebControls/ModalDialogUserControlBase.cs
+++ Havit.Web/Bootstrap/UI/WebControls/ModalDialogUserControlBase.cs
@@ -67,40 +67,4 @@ namespace Havit.Web.Bootstrap.UI.WebControls
 		}
 		#endregion
 	}
-
-	/// <summary>
-	/// Extends ModalDialogUserControlBase by persisting dialog Result value.
-	/// </summary>
-	public class ModalDialogUserControlBase<T> : ModalDialogUserControlBase
-	{
-		#region OnDialogShown
-		/// <summary>
-		/// Sets result value to type T default value.
-		/// </summary>
-		protected override void OnDialogShown(EventArgs eventArgs)
-		{
-			base.OnDialogShown(eventArgs);
-			Result = default(T);
-		}
-		#endregion
-
-		#region Result
-		/// <summary>
-		/// User result of modal dialog activity.
-		/// Result value is automatically reset when the dialog in shown, it is set to the type T default value.
-		/// Value is persisted in ViewState (so it must be serializable).
-		/// </summary>
-		public T Result
-		{
-			get
-			{
-				return (T)(ViewState["Result"] ?? default(T));
-			}
-			set
-			{
-				ViewState["Result"] = value;
-			}
-		}
-		#endregion
-	}
 }
---
+++ Havit.Web/Bootstrap/UI/WebControls/ModalDialogUserControlBaseT.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Havit.Web.UI.WebControls;
+
+namespace Havit.Web.Bootstrap.UI.WebControls
+{
+	/// <summary>
+	/// Extends ModalDialogUserControlBase by persisting dialog Result value.
+	/// </summary>
+	public class ModalDialogUserControlBase<T> : ModalDialogUserControlBase
+	{
+		#region OnDialogShown
+		/// <summary>
+		/// Sets result value to type T default value.
+		/// </summary>
+		protected override void OnDialogShown(EventArgs eventArgs)
+		{
+			base.OnDialogShown(eventArgs);
+			Result = default(T);
+		}
+		#endregion
+
+		#region Result
+		/// <summary>
+		/// User result of modal dialog activity.
+		/// Result value is automatically reset when the dialog in shown, it is set to the type T default value.
+		/// Value is persisted in ViewState (so it must be serializable).
+		/// </summary>
+		public T Result
+		{
+			get
+			{
+				return (T)(ViewState["Result"] ?? default(T));
+			}
+			set
+			{
+				ViewState["Result"] = value;
+			}
+		}
+		#endregion
+	}
+}

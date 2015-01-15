f7dd71e65a3123878b70d782b807f1458be5964f
kanda <kanda@havit.tfs.local>
2015-01-15 10:18:45 +0000

    Revize scriptů pro publikaci HFW (+ migrace na NuGet balíčky)

--- PublishScripts/Publish-Framework.ps1
+++ PublishScripts/Publish-Framework.ps1
@@ -1,32 +1,12 @@
-xcopy Framework\Havit.dll \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.pdb \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.xml \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Business.dll \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Business.pdb \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Business.xml \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Data.dll \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Data.pdb \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Data.xml \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Drawing.dll \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Drawing.pdb \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Drawing.xml \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Enterprise.Web.dll \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Enterprise.Web.pdb \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Enterprise.Web.xml \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.PayMuzo.dll \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.PayMuzo.pdb \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.PayMuzo.xml \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Services.dll \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Services.pdb \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Services.xml \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Web.dll \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Web.pdb \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Web.xml \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Xml.dll \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Xml.pdb \\topol.havit.local\library\framework /y
-xcopy Framework\Havit.Xml.xml \\topol.havit.local\library\framework /y
+xcopy NuGet\Havit.2.*.nupkg \\topol.havit.local\Library\NuGet\packages /y
+xcopy NuGet\Havit.Business.*.nupkg \\topol.havit.local\Library\NuGet\packages /y
+xcopy NuGet\Havit.Data.*.nupkg \\topol.havit.local\Library\NuGet\packages /y
+xcopy NuGet\Havit.Drawing.*.nupkg \\topol.havit.local\Library\NuGet\packages /y
+xcopy NuGet\Havit.Enterprise.Web.*.nupkg \\topol.havit.local\Library\NuGet\packages /y
+xcopy NuGet\Havit.PayMuzo.*.nupkg \\topol.havit.local\Library\NuGet\packages /y
+xcopy NuGet\Havit.Services.*.nupkg \\topol.havit.local\Library\NuGet\packages /y
+xcopy NuGet\Havit.Web.*.nupkg \\topol.havit.local\Library\NuGet\packages /y
 
 xcopy Havit.Documentation.Homepage \\topol.havit.local\Inetpub\havit.local\hfw /e /y
 xcopy Documentation \\topol.havit.local\Inetpub\havit.local\hfw\Documentation /e /y
 
-# $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")

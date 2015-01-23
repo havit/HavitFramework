94afee3cb03b25dcff681b285e232142803d3c07
kanda <kanda@havit.tfs.local>
2015-01-23 12:01:33 +0000

    Havit.Web.CastleWindsor.WebForms -> Havit.CastleWindsor.WebForms (pokračování)

---
+++ PublishScripts/Publish-Havit.CastleWindsor.WebForms.ps1
@@ -0,0 +1,1 @@
+xcopy NuGet\Havit.CastleWindsor.WebForms.*.nupkg \\topol.havit.local\Library\NuGet\packages /y
--- PublishScripts/Publish-Havit.Web.CastleWindsor.WebForms.ps1
+++
@@ -1,1 +0,0 @@
-xcopy NuGet\Havit.Web.CastleWindsor.WebForms.*.nupkg \\topol.havit.local\Library\NuGet\packages /y

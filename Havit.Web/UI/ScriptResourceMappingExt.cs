be0743f632a798c28bdc127b666dc5a654f3c314
rada <rada@havit.tfs.local>
2015-01-27 13:58:42 +0000

    v async režimu nevyhazujeme vyjímku pro nenačtené skripty ve script manageru TODO vymyslet lepší způsob - vlastní script manager s persistencí?

--- Havit.Web/UI/ScriptResourceMappingExt.cs
+++ Havit.Web/UI/ScriptResourceMappingExt.cs
@@ -32,10 +32,11 @@ namespace Havit.Web.UI
 					throw new InvalidOperationException(String.Format("Missing script resource mapping '{0}'. Please add a ScriptResourceMapping named '{0}' (case-sensitive).", resourceName));
 
 				case TryEnsureScriptRegistrationResult.ScriptResourceMappingWhileAsyncPostback:
-					if (HttpContext.Current.IsDebuggingEnabled)
-					{
-						throw new InvalidOperationException(String.Format("Attempting to register '{0}' during asynchronous postback, which is not possible. Register script in the page or in master page.", resourceName));
-					}
+					// TODO: lépe udělat persistence skriptů - vlastní script manager?
+					//if (HttpContext.Current.IsDebuggingEnabled)
+					//{
+					//	throw new InvalidOperationException(String.Format("Attempting to register '{0}' during asynchronous postback, which is not possible. Register script in the page or in master page.", resourceName));
+					//}
 					break;
 			}
 		}
@@ -95,10 +96,11 @@ namespace Havit.Web.UI
 			{
 				ScriptManager.ScriptResourceMapping.AddDefinition(resourceFullName, new ScriptResourceDefinition { Path = page.ClientScript.GetWebResourceUrl(type, embeddedResourceName) });
 			}
-			else if (currentScriptManager.IsInAsyncPostBack && !scriptResourceExists && HttpContext.Current.IsDebuggingEnabled)
-			{
-				throw new InvalidOperationException(String.Format("Attempting to register embedded resource script '{0}' during asynchronous postback, which is not possible. Register script in the page or in master page.", embeddedResourceName));
-			}
+			// TODO: lépe udělat persistence skriptů - vlastní script manager?
+			//else if (currentScriptManager.IsInAsyncPostBack && !scriptResourceExists && HttpContext.Current.IsDebuggingEnabled)
+			//{
+			//	throw new InvalidOperationException(String.Format("Attempting to register embedded resource script '{0}' during asynchronous postback, which is not possible. Register script in the page or in master page.", embeddedResourceName));
+			//}
 			else
 			{
 				if (control == null)

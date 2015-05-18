671a20bedae75ed94a98fc02a59efa4a1f0629c0
Kanda, Jiří <kanda@havit.cz>
2015-05-18 07:27:41 +0000

    ExpressionExt - pomocné metody pro Expression

---
+++ Havit/Linq/Expressions/ExpressionExt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Linq.Expressions
+{
+	/// <summary>
+	/// Pomocné metody pro práci s Expression.
+	/// </summary>
+	public static class ExpressionExt
+	{
+		#region ReplaceParameter
+		/// <summary>
+		/// Vymění v expression jeden parameter druhým. Slouží například k náhradě na mujObjekt => mujObjekt.Id na item => item.Id.
+		/// </summary>
+		/// <param name="expression">Expression, ve kterém je prováděna náhrada parametru.</param>
+		/// <param name="sourceParameter">Parametr, který má být nalezen a nahrazen.</param>
+		/// <param name="targetParameter">Parametr, kterým bude sourceParametr nahrazen.</param>
+		public static Expression ReplaceParameter(Expression expression, ParameterExpression sourceParameter, ParameterExpression targetParameter)
+		{
+			return new ReplaceParameterVisitor(sourceParameter, targetParameter).Visit(expression);
+		}
+		#endregion
+
+		#region RemoveConvert
+		/// <summary>
+		/// Odstraní z expression Convert, pokud je přítomen. Nahrazuje jej jen na nejvyšší úrovni.
+		/// Slouží k náhradě item => (object)item.Id na item => item.Id.
+		/// </summary>
+		/// <param name="expression">Expression, ve které se Convert vyhledává.</param>
+		public static Expression RemoveConvert(this Expression expression)
+		{
+			while ((expression.NodeType == ExpressionType.Convert) || (expression.NodeType == ExpressionType.ConvertChecked))
+			{
+				expression = ((UnaryExpression)expression).Operand;
+			}
+			return expression;
+		}
+		#endregion
+	}
+}
---
+++ Havit/Linq/Expressions/ReplaceParameterVisitor.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+
+namespace Havit.Linq.Expressions
+{
+	internal class ReplaceParameterVisitor : ExpressionVisitor
+	{
+		#region Private fields
+		private readonly ParameterExpression fromParameter;
+		private readonly ParameterExpression toParameter;
+		#endregion
+
+		#region Constructor
+		/// <summary>
+		/// Konstruktor.
+		/// </summary>
+		public ReplaceParameterVisitor(ParameterExpression fromParameter, ParameterExpression toParameter)
+		{
+			this.fromParameter = fromParameter;
+			this.toParameter = toParameter;
+		}
+		#endregion
+
+		#region VisitParameter
+		/// <summary>
+		/// Nahradí parametr.
+		/// </summary>
+		protected override Expression VisitParameter(ParameterExpression node)
+		{
+			return (node == fromParameter) ? toParameter : base.VisitParameter(node);
+		}
+		#endregion
+	}
+}

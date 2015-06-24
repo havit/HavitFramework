ca39a5fe221247a0d8cbd3850f14242fa76e5514
Kanda, Jiří <kanda@havit.cz>
2015-06-24 10:10:16 +0000

    Havit.Services.Azure + NuGetPackage

---
+++ Havit.Services.Azure/FileServices/AzureBlobStorageService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Havit.Services.FileServices;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Havit.Services.Azure.FileServices
+{
+	/// <summary>
+	/// Úložiště souborů jako Azure Blob Storage
+	/// </summary>
+	public class AzureBlobStorageService : IFileStorageService
+	{
+		#region Private fields
+		private readonly string blobStorageConnectionString;
+		private readonly string containerName;
+		#endregion
+
+		#region Constructor
+		/// <summary>
+		/// Konstruktor.
+		/// </summary>
+		/// <param name="blobStorageConnectionString">Connection string pro připojení k Azure Blob Storage.</param>
+		/// <param name="containerName">Container v Blob Storage pro práci se soubory.</param>
+		public AzureBlobStorageService(string blobStorageConnectionString, string containerName)
+		{
+			this.blobStorageConnectionString = blobStorageConnectionString;
+			this.containerName = containerName;
+		}
+		#endregion
+
+		#region Exists
+		/// <summary>
+		/// Vrátí true, pokud uložený soubor v úložišti existuje. Jinak false.
+		/// </summary>
+		public bool Exists(string fileName)
+		{
+			CloudBlockBlob blob = GetBlobReference(fileName);
+			return blob.Exists();
+		}
+		#endregion
+
+		#region ReadToStream
+		public void ReadToStream(string fileName, Stream stream)
+		{
+			CloudBlockBlob blob = GetBlobReference(fileName);
+			blob.DownloadToStream(stream);			
+		}
+		#endregion
+
+		#region Read
+		/// <summary>
+		/// Vrátí stream s obsahem soubor z úložiště.
+		/// </summary>
+		public Stream Read(string fileName)
+		{
+			MemoryStream memoryStream = new MemoryStream();
+			ReadToStream(fileName, memoryStream);
+			memoryStream.Seek(0, SeekOrigin.Begin);
+			
+			return memoryStream;
+		}
+		#endregion
+
+		#region Save
+		/// <summary>
+		/// Uloží stream do úložiště.
+		/// </summary>
+		public void Save(string fileName, Stream fileContent, string contentType)
+		{
+			CloudBlockBlob blob = GetBlobReference(fileName, true);
+			blob.Properties.ContentType = contentType;
+			blob.UploadFromStream(fileContent);
+		}
+		#endregion
+
+		#region Delete
+		/// <summary>
+		/// Smaže soubor v úložišti.
+		/// </summary>>
+		public void Delete(string fileName)
+		{
+			CloudBlockBlob blob = GetBlobReference(fileName);
+			blob.Delete();
+		}
+		#endregion
+
+		#region GetBlobReference
+		private CloudBlockBlob GetBlobReference(string blobName, bool createContainerWhenNotExists = false)
+		{
+			CloudStorageAccount storageAccount = CloudStorageAccount.Parse(blobStorageConnectionString);
+			// Create the blob client.
+			CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+			// Retrieve reference to a previously created container.
+			CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+			if (createContainerWhenNotExists && !container.Exists())
+			{
+				container.CreateIfNotExists(BlobContainerPublicAccessType.Off);
+			}
+			return container.GetBlockBlobReference(blobName);
+		}
+		#endregion
+	}
+}
---
+++ Havit.Services/FileServices/FileSystemStorageService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Services.FileServices
+{
+	/// <summary>
+	/// IFileStorageService s file systémem pro datové úložiště.
+	/// </summary>
+	public class FileSystemStorageService : IFileStorageService
+	{
+		#region Private fields
+		private readonly string storagePath;
+		#endregion
+
+		#region Constructor
+		/// <summary>
+		/// Konstruktor.
+		/// </summary>
+		/// <param name="storagePath">Cesta k "rootu" použitého úložiště ve file systému.</param>
+		public FileSystemStorageService(string storagePath)
+		{
+			this.storagePath = storagePath;
+		}
+		#endregion
+
+		#region Exists
+		/// <summary>
+		/// Vrátí true, pokud uložený soubor v úložišti existuje. Jinak false.
+		/// </summary>
+		public bool Exists(string fileName)
+		{
+			return File.Exists(System.IO.Path.Combine(storagePath, fileName));
+		}
+		#endregion
+
+		#region Read
+		/// <summary>
+		/// Vrátí stream s obsahem soubor z úložiště.
+		/// </summary>
+		public Stream Read(string fileName)
+		{
+			return File.OpenRead(System.IO.Path.Combine(storagePath, fileName));
+		}
+		#endregion
+
+		#region ReadToStream
+		/// <summary>
+		/// Zapíše obsah souboru z úložiště do streamu.
+		/// </summary>
+		public void ReadToStream(string fileName, Stream stream)
+		{
+			using (Stream fileStream = File.OpenRead(System.IO.Path.Combine(storagePath, fileName)))
+			{
+				fileStream.CopyTo(stream);
+			}
+		}
+		#endregion
+
+		#region Save
+		/// <summary>
+		/// Uloží stream do úložiště.
+		/// </summary>
+		public void Save(string fileName, Stream fileContent, string contentType)
+		{
+			using (FileStream fileStream = File.OpenWrite(System.IO.Path.Combine(storagePath, fileName)))
+			{
+				fileContent.CopyTo(fileStream);
+			}
+		}
+		#endregion
+
+		#region Delete
+		/// <summary>
+		/// Smaže soubor v úložišti.
+		/// </summary>
+		public void Delete(string fileName)
+		{
+			System.IO.File.Delete(System.IO.Path.Combine(storagePath, fileName));
+		}
+		#endregion
+	}
+}

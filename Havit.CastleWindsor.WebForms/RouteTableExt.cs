e7448380cccd6fb69318f9511c0512dbcb993e65
Kanda, Jiří <kanda@havit.cz>
2015-09-07 14:17:04 +0000

    Rozšíření routování s využitím dependency injection.

--- Havit.CastleWindsor.WebForms/RouteTableExt.cs
+++ Havit.CastleWindsor.WebForms/RouteTableExt.cs
@@ -3,16 +3,48 @@
 namespace Havit.CastleWindsor.WebForms
 {
 	/// <summary>
-	/// Extension metody pro routování
+	/// Extension metody pro routování na stránky s využitím Dependency Injection.
 	/// </summary>
 	public static class RouteTableExt
 	{
 		/// <summary>
-		/// Vytvoří novou route na .aspx pomocí DependencyInjectionPageRouteHandler
+		/// Vytvoří novou route pomocí DependencyInjectionPageRouteHandler
 		/// </summary>
-		public static void MapDIPageRoute(this RouteCollection routes, string name, string urlPattern, string filePath, bool checkPhysicalAccess = true)
+		public static void MapDependencyInjectionPageRoute(this RouteCollection routes, string routeName, string routeUrl, string physicalFile)
 		{
-			routes.Add(name, new Route(urlPattern, new DependencyInjectionPageRouteHandler(filePath, checkPhysicalAccess)));
+			MapDependencyInjectionPageRoute(routes, routeName, routeUrl, physicalFile, true, null, null, null);
+		}
+
+		/// <summary>
+		/// Vytvoří novou route pomocí DependencyInjectionPageRouteHandler
+		/// </summary>
+		public static void MapDependencyInjectionPageRoute(this RouteCollection routes, string routeName, string routeUrl, string physicalFile, bool checkPhysicalUrlAccess)
+		{
+			MapDependencyInjectionPageRoute(routes, routeName, routeUrl, physicalFile, checkPhysicalUrlAccess, null, null, null);
+		}
+
+		/// <summary>
+		/// Vytvoří novou route pomocí DependencyInjectionPageRouteHandler
+		/// </summary>
+		public static void MapDependencyInjectionPageRoute(this RouteCollection routes, string routeName, string routeUrl, string physicalFile, bool checkPhysicalUrlAccess, RouteValueDictionary defaults)
+		{
+			MapDependencyInjectionPageRoute(routes, routeName, routeUrl, physicalFile, checkPhysicalUrlAccess, defaults, null, null);
+		}
+
+		/// <summary>
+		/// Vytvoří novou route pomocí DependencyInjectionPageRouteHandler
+		/// </summary>
+		public static void MapDependencyInjectionPageRoute(this RouteCollection routes, string routeName, string routeUrl, string physicalFile, bool checkPhysicalUrlAccess, RouteValueDictionary defaults, RouteValueDictionary constraints)
+		{
+			MapDependencyInjectionPageRoute(routes, routeName, routeUrl, physicalFile, checkPhysicalUrlAccess, defaults, constraints, null);
+		}
+
+		/// <summary>
+		/// Vytvoří novou route pomocí DependencyInjectionPageRouteHandler
+		/// </summary>
+		public static void MapDependencyInjectionPageRoute(this RouteCollection routes, string routeName, string routeUrl, string physicalFile, bool checkPhysicalUrlAccess, RouteValueDictionary defaults, RouteValueDictionary constraints, RouteValueDictionary dataTokens)
+		{
+			routes.Add(routeName, new Route(routeUrl, defaults, constraints, dataTokens, new DependencyInjectionPageRouteHandler(physicalFile, checkPhysicalUrlAccess)));
 		}
 	}
 }

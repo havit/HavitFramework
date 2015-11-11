9d3038f87da1e441b895d87048adb6b2e151c23d
Kanda, Jiří <kanda@havit.cz>
2015-11-11 11:23:44 +0000

    22260 Přejmenovat Havit.Entity.Glimpse na Havit.Data.Entity.Glimpse

---
+++ PublishScripts/Publish-Havit.Data.Entity6.Glimpse.ps1
@@ -0,0 +1,1 @@
+xcopy NuGet\Havit.Data.Entity6.Glimpse.*.nupkg \\topol.havit.local\Library\NuGet\packages /y
---
+++ PublishScripts/Publish-Havit.Data.Entity6.ps1
@@ -0,0 +1,1 @@
+xcopy NuGet\Havit.Data.Entity6.1.*.nupkg \\topol.havit.local\Library\NuGet\packages /y

9d12f714df1b574b76b0bc9a128d0604e8ead3fa
Musil, Petr <musil@moon.fish>
2016-03-17 15:25:07 +0000

    Přesun LocalizationService z Havit.Data.Entity6.Patterns do Havit.Data.Patterns.

--- Havit.Data.Patterns/Infrastructure/EntityKeyAccessor.cs
+++ Havit.Data.Patterns/Infrastructure/EntityKeyAccessor.cs
@@ -6,7 +6,6 @@ namespace Havit.Data.Patterns.Infrastructure
 	/// <summary>
 	/// Třída pro získávání identifikátoru modelových objektů.
 	/// </summary>
-	/// <seealso cref="Havit.Data.Patterns.Infrastructure.IEntityKeyAccessor{Havit.Model.Localizations.ILanguage, System.Int32}" />
 	public class EntityKeyAccessor : IEntityKeyAccessor<ILanguage, int>
 	{
 		/// <summary>
--- Havit.Data.Patterns/Infrastructure/IEntityKeyAccessor.cs
+++ Havit.Data.Patterns/Infrastructure/IEntityKeyAccessor.cs
@@ -5,7 +5,6 @@ namespace Havit.Data.Patterns.Infrastructure
 	/// <summary>
 	/// Třída pro získávání identifikátoru modelových objektů.
 	/// </summary>
-	/// <seealso cref="Havit.Data.Patterns.Infrastructure.IEntityKeyAccessor{Havit.Model.Localizations.ILanguage, System.Int32}" />
 	public interface IEntityKeyAccessor<TEntity, TEntityKey>
 		where TEntity : class, ILanguage
 	{

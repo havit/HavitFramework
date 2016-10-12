4e9923128fa2604094a5af7b7481c70a83cb359b
Kanda, Jiří <kanda@havit.cz>
2016-10-12 09:42:48 +0000

    DateTimeExt.Min/Max

---
+++ Havit.Tests/DateTimeExtTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Havit.Tests
+{
+	[TestClass]
+	public class DateTimeExtTest
+	{
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void DateTimeExt_Min_ThrowExceptionForEmptyParameters()
+		{
+			// Act
+			DateTimeExt.Min();
+			
+			// Assert by method attribute
+		}
+
+		[TestMethod]
+		public void DateTimeExt_Min()
+		{
+			// Act + Assert
+			Assert.AreEqual(DateTime.MinValue, DateTimeExt.Min(DateTime.Now, DateTime.MinValue));
+			Assert.AreEqual(DateTime.MinValue, DateTimeExt.Min(DateTime.MinValue, DateTime.Now));
+			Assert.AreEqual(DateTime.MinValue, DateTimeExt.Min(DateTime.MinValue, DateTime.MinValue, DateTime.Now));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void DateTimeExt_Max_ThrowExceptionForEmptyParameters()
+		{
+			// Act
+			DateTimeExt.Max();
+
+			// Assert by method attribute
+		}
+
+		[TestMethod]
+		public void DateTimeExt_Max()
+		{
+			// Act + Assert
+			Assert.AreEqual(DateTime.MaxValue, DateTimeExt.Max(DateTime.Now, DateTime.MaxValue));
+			Assert.AreEqual(DateTime.MaxValue, DateTimeExt.Max(DateTime.MaxValue, DateTime.Now));
+			Assert.AreEqual(DateTime.MaxValue, DateTimeExt.Max(DateTime.MaxValue, DateTime.MaxValue, DateTime.Now));
+		}
+	}
+}
---
+++ Havit/DateTimeExt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit
+{
+	/// <summary>
+	/// Rozšiřující funkce pro práci s daty (datumy) <see cref="DateTime"/>.	
+	/// </summary>
+	public static class DateTimeExt
+	{		
+		/// <summary>
+		/// Vrátí nejmenší (nejdřívější) ze zadaných dat.
+		/// </summary>
+		public static DateTime Min(params DateTime[] values)
+		{
+			return values.Min();
+		}
+
+		/// <summary>
+		/// Vrátí největší (nejposlednější) ze zadaných dat.
+		/// </summary>
+		public static DateTime Max(params DateTime[] values)
+		{
+			return values.Max();
+		}
+	}
+}

cf6d481154ec3e391924b5fe9ca1a4f7e847ef39
Kanda, Jiří <kanda@havit.cz>
2016-10-19 13:43:44 +0000

    Kosmetika v AssemblyInfo.cs

--- Havit.Services.Azure/Properties/AssemblyInfo.cs
+++ Havit.Services.Azure/Properties/AssemblyInfo.cs
@@ -3,6 +3,3 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("HAVIT Azure Services Class Library")]
 [assembly: AssemblyDescription("HAVIT .NET Framework Extensions - Azure Services")]
-
-// TODO šifrování storage:  odstranit
-[assembly: InternalsVisibleTo("Havit.Services.Azure.Tests")]
--- Havit.Services/Properties/AssemblyInfo.cs
+++ Havit.Services/Properties/AssemblyInfo.cs
@@ -3,8 +3,3 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("HAVIT Services Class Library")]
 [assembly: AssemblyDescription("HAVIT .NET Framework Extensions - Services")]
-
-// TODO šifrování storage: odstranit
-[assembly: InternalsVisibleTo("Havit.Services.Azure")]
-[assembly: InternalsVisibleTo("Havit.Services.Tests")]
-[assembly: InternalsVisibleTo("Havit.Services.Azure.Tests")]

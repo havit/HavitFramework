56e3679face366a191819de99668296f97531c05
Haken, Robert <haken@havit.cz>
2016-10-26 21:18:48 +0000

    29348 IServiceFactory<TService> interface pro IoC/DI Resolve

---
+++ Havit.Services/IServiceFactory.cs
@@ -0,0 +1,27 @@
+﻿namespace Havit.Services
+{
+	/// <summary>
+	/// Generic interface for service factories.
+	/// Intended to be used with IoC/DI containers, e.g. Castle Windsor Typed Factories facility:
+	/// <code>
+	/// 	container.AddFacility&lt;TypedFactoryFacility&gt;();
+	///		container.Register(Component.For(typeof(IServiceFactory&lt;&gt;)).AsFactory());
+	/// </code>
+	/// </summary>
+	/// <typeparam name="T">type of service to be managed by the factory</typeparam>
+	public interface IServiceFactory<T>
+		where T : class
+	{
+		/// <summary>
+		/// Creates/resolves the service.
+		/// </summary>
+		/// <returns>Service created/resolved.</returns>
+		T CreateService();
+
+		/// <summary>
+		/// Releases the service.
+		/// </summary>
+		/// <param name="service">Service to be released.</param>
+		void ReleaseService(T service);
+	}
+}

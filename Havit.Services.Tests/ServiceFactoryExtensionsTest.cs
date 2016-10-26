969a39470ce3b582dce0f8089607eaed44b15e17
Haken, Robert <haken@havit.cz>
2016-10-26 21:38:41 +0000

    29348 IServiceFactory<TService> interface pro IoC/DI - ServiceFactoryExtensions.ExecuteAction()

---
+++ Havit.Services.Tests/ServiceFactoryExtensionsTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Havit.Services.Tests
+{
+	[TestClass]
+	public class ServiceFactoryExtensionsTests
+	{
+		[TestMethod]
+		public void ServiceFactoryExtensions_ExecuteAction_CreatesService()
+		{
+			// arrange
+			var fakeService = new object();
+			var serviceFactory = new Mock<IServiceFactory<object>>();
+			serviceFactory.Setup(f => f.CreateService()).Returns(fakeService);
+
+			// act
+			serviceFactory.Object.ExecuteAction(service =>
+			{
+				// NOOP
+			});
+
+			// assert
+			serviceFactory.Verify(f => f.CreateService(), Times.Once);
+		}
+
+		[TestMethod]
+		public void ServiceFactoryExtensions_ExecuteAction_ExecutesAction()
+		{
+			// arrange
+			var fakeService = new object();
+			var serviceFactory = new Mock<IServiceFactory<object>>();
+			serviceFactory.Setup(f => f.CreateService()).Returns(fakeService);
+
+			// act
+			object result = null;
+			serviceFactory.Object.ExecuteAction(service =>
+			{
+				result = service;
+			});
+
+			// assert
+			Assert.AreSame(fakeService, result);
+		}
+
+		[TestMethod]
+		public void ServiceFactoryExtensions_ExecuteAction_ReleasesService()
+		{
+			// arrange
+			var fakeService = new object();
+			var serviceFactory = new Mock<IServiceFactory<object>>();
+			serviceFactory.Setup(f => f.CreateService()).Returns(fakeService);
+
+			// act
+			serviceFactory.Object.ExecuteAction(service =>
+			{
+				// NOOP
+			});
+
+			// assert
+			serviceFactory.Verify(f => f.ReleaseService(fakeService), Times.Once);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(Exception), "FAKE EXCEPTION")]
+		public void ServiceFactoryExtensions_ExecuteAction_ActionThrowsException_ReleasesService()
+		{
+			// arrange
+			var fakeService = new object();
+			var serviceFactory = new Mock<IServiceFactory<object>>();
+			serviceFactory.Setup(f => f.CreateService()).Returns(fakeService);
+
+			// act
+			try
+			{
+				serviceFactory.Object.ExecuteAction(service =>
+				{
+					throw new Exception("FAKE EXCEPTION");
+				});
+			}
+			finally
+			{
+				// assert
+				serviceFactory.Verify(f => f.ReleaseService(fakeService), Times.Once);
+			}
+		}
+	}
+}
---
+++ Havit.Services/ServiceFactoryExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Havit.Services
+{
+	/// <summary>
+	/// Extension methods for <c>IServiceFactory&lt;TService&gt;</c>
+	/// </summary>
+	public static class ServiceFactoryExtensions
+	{
+		/// <summary>
+		/// Executes action using service from service factory. Releases the service after executing the action.
+		/// </summary>
+		/// <param name="serviceFactory">Service factory providing the service to be used.</param>
+		/// <param name="action">Action to be executed.</param>
+		/// <typeparam name="TService">type of service managed by the service factory</typeparam>
+		public static void ExecuteAction<TService>(this IServiceFactory<TService> serviceFactory, Action<TService> action)
+			where TService : class
+		{
+			TService service = null;
+
+			try
+			{
+				service = serviceFactory.CreateService();
+				action(service);
+			}
+			finally
+			{
+				serviceFactory.ReleaseService(service);
+			}
+		}
+	}
+}

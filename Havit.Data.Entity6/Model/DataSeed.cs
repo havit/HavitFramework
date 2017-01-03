65a81e05ee481545a231a0c9483e8c2b9cffae16
Kanda, Jiří <kanda@havit.cz>
2017-01-03 11:57:35 +0000

    Kosmetika.

--- Havit.Data.Entity6/Model/DataSeed.cs
+++ Havit.Data.Entity6/Model/DataSeed.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace Havit.Data.Entity.Model
 {
+	/// <summary>
+	/// Informace o naposledy spuštěné verzi seedování dat.
+	/// </summary>
 	public class DataSeed
 	{
+		/// <summary>
+		/// Identifikátor. Používá se jen s hodnotou 1.
+		/// </summary>
 		public int Id { get; set; }		
+
+		/// <summary>
+		/// Naposledy spuštěná verze seedování dat.
+		/// </summary>
 		public string Version { get; set; }
 	}
 }

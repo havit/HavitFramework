d09a970d2b0bfbfeb4cacdaf0e54ce96183d401e
Kanda, Jiří <kanda@havit.cz>
2017-08-30 10:44:51 +0000

    35192 FakeDataLoader - pro použití v unit testech

---
+++ Havit.Data.Patterns/DataLoaders/Fakes/FakeDataLoader.cs
@@ -0,0 +1,55 @@
+﻿using Havit.Data.Patterns.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace Havit.Data.Patterns.DataLoaders.Fakes
+{
+    /// <summary>
+    /// Explicity data loader, který nic nedělá.
+    /// Určeno pro použití v unit testech pro mock IDataLoaderu.
+    /// </summary>
+    [Fake]    
+    public class FakeDataLoader : IDataLoader, IDataLoaderAsync
+    {
+        /// <summary>
+        /// Contract: Načte vlastnosti objektů, pokud ještě nejsou načteny.        
+        /// Implementace: Nic nedělá.
+        /// </summary>
+        public void Load<TEntity>(TEntity entity, params Expression<Func<TEntity, object>>[] propertyPaths)
+            where TEntity : class
+        {
+            // NOOP
+        }
+
+        /// <summary>
+        /// Contract: Načte vlastnosti objektů, pokud ještě nejsou načteny.        
+        /// Implementace: Nic nedělá.
+        public void LoadAll<TEntity>(IEnumerable<TEntity> entities, params Expression<Func<TEntity, object>>[] propertyPaths)
+            where TEntity : class
+        {
+            // NOOP
+        }
+
+        /// <summary>
+        /// Contract: Načte vlastnosti objektů, pokud ještě nejsou načteny.        
+        /// Implementace: Nic nedělá.
+        public Task LoadAllAsync<TEntity>(IEnumerable<TEntity> entities, params Expression<Func<TEntity, object>>[] propertyPaths)
+            where TEntity : class
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Contract: Načte vlastnosti objektů, pokud ještě nejsou načteny.        
+        /// Implementace: Nic nedělá.
+        public Task LoadAsync<TEntity>(TEntity entity, params Expression<Func<TEntity, object>>[] propertyPaths)
+            where TEntity : class
+        {
+            return Task.CompletedTask;            
+        }
+    }
+}

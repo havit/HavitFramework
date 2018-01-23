0bb860a8ad1707de98f30925bf9ffc91025ef9c8
Haken, Robert <haken@havit.cz>
2018-01-23 00:09:43 +0000

    37291 ICollection<T>.UpdateFrom() - synchronizace kolekcí

---
+++ Havit.Tests/Linq/CollectionExtTests.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Havit.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Havit.Tests.Linq
+{
+	[TestClass]
+	public class CollectionExtTests
+	{
+		[TestMethod]
+		public void CollectionExt_UpdateFrom_Demo()
+		{
+			// arrange
+			TargetClass targetItem1 = new TargetClass() { Id = 1, StringProperty = "FAKE_TARGET_1" };
+			TargetClass targetItem3 = new TargetClass() { Id = 3, StringProperty = "FAKE_TARGET_3" };
+			var targetList = new List<TargetClass>()
+			{
+				targetItem1,
+				targetItem3
+			};
+			var sourceList = new List<SourceClass>()
+			{
+				new SourceClass() { Id = 1, StringProperty = "FAKE_SOURCE_1" },
+				new SourceClass() { Id = 2, StringProperty = "FAKE_SOURCE_2" }
+			};
+
+			// act
+			targetList.UpdateFrom(sourceList,
+				targetKeySelector: target => target.Id,
+				sourceKeySelector: source => source.Id,
+				newItemCreateFunc:
+					source =>
+					{
+						var newItem = new TargetClass()
+						{
+							Id = source.Id,
+							StringProperty = source.StringProperty
+						};
+						// itemsAdded.Add(newItem);
+						// unitOfWork.AddForInsert(newItem);
+						return newItem;
+					},
+				updateItemAction:
+					(source, target) =>
+					{
+						target.StringProperty = source.StringProperty;
+						// itemsUpdated.Add(target);
+						// unitOfWork.AddForUpdate(target);
+					},
+				removeItemAction:
+					(target) =>
+					{
+						// itemsRemoved.Add(target);
+						// unitOfWork.AddForDelete(target);
+					}
+			);
+
+			// assert
+			// Item 1 updated
+			CollectionAssert.Contains(targetList, targetItem1);
+			var item1 = targetList.Single(i => i.Id == 1);
+			Assert.AreEqual("FAKE_SOURCE_1", item1.StringProperty);
+
+			// Item 3 removed
+			CollectionAssert.DoesNotContain(targetList, targetItem3);
+
+			// Item 2 added
+			Assert.AreEqual(2, targetList.Count);
+			var item2 = targetList.Single(i => i.Id == 2);
+			Assert.AreEqual("FAKE_SOURCE_2", item2.StringProperty);
+		}
+
+		[TestMethod]
+		public void CollectionExt_UpdateFrom_NullKey_ShouldMatchNullToNull()
+		{
+			// arrange
+			TargetClass targetItem1 = new TargetClass() { StringProperty = null, OtherProperty = "TARGET_OTHER_1" };
+			TargetClass targetItem3 = new TargetClass() { StringProperty = "TARGET_KEY_3", OtherProperty = "TARGET_OTHER_3" };
+			var targetList = new List<TargetClass>()
+			{
+				targetItem1,
+				targetItem3
+			};
+			var sourceList = new List<SourceClass>()
+			{
+				new SourceClass() { StringProperty = null, OtherProperty = 0 },
+				new SourceClass() { StringProperty = "SOURCE_KEY_2", OtherProperty = 2 }
+			};
+
+			// act
+			targetList.UpdateFrom(sourceList,
+				targetKeySelector: target => target.StringProperty,
+				sourceKeySelector: source => source.StringProperty,
+				newItemCreateFunc:
+					source =>
+					{
+						var newItem = new TargetClass()
+						{
+							StringProperty = source.StringProperty,
+							OtherProperty = source.OtherProperty.ToString()
+						};
+						return newItem;
+					},
+				updateItemAction:
+					(source, target) =>
+					{
+						target.StringProperty = source.StringProperty;
+						target.OtherProperty = source.OtherProperty.ToString();
+					},
+				removeItemAction:
+					(target) => {}
+			);
+
+			// assert
+			// Item 1 updated
+			CollectionAssert.Contains(targetList, targetItem1);
+			var item1 = targetList.Single(i => i.StringProperty == null);
+			Assert.AreEqual("0", item1.OtherProperty);
+
+			// Item 3 removed
+			CollectionAssert.DoesNotContain(targetList, targetItem3);
+
+			// Item 2 added
+			Assert.AreEqual(2, targetList.Count);
+			var item2 = targetList.Single(i => i.StringProperty == "SOURCE_KEY_2");
+			Assert.AreEqual("2", item2.OtherProperty);
+		}
+
+		[TestMethod]
+		public void CollectionExt_UpdateFrom_DuplicateSourceKey_DoubleUpdate()
+		{
+			// arrange
+			TargetClass targetItem1 = new TargetClass() { Id = 1, StringProperty = "FAKE_TARGET_1" };
+			var targetList = new List<TargetClass>()
+			{
+				targetItem1,
+			};
+			var sourceList = new List<SourceClass>()
+			{
+				new SourceClass() { Id = 1, StringProperty = "FAKE_SOURCE_11" },
+				new SourceClass() { Id = 1, StringProperty = "FAKE_SOURCE_12" }
+			};
+
+			var itemsAdded = new List<TargetClass>();
+			var itemsUpdated = new List<TargetClass>();
+			var itemsRemoved = new List<TargetClass>();
+
+			// act
+			targetList.UpdateFrom(sourceList,
+				targetKeySelector: target => target.Id,
+				sourceKeySelector: source => source.Id,
+				newItemCreateFunc:
+					source =>
+					{
+						var newItem = new TargetClass()
+						{
+							Id = source.Id,
+							StringProperty = source.StringProperty
+						};
+						itemsAdded.Add(newItem);
+						return newItem;
+					},
+				updateItemAction:
+					(source, target) =>
+					{
+						target.StringProperty = source.StringProperty;
+						itemsUpdated.Add(target);
+					},
+				removeItemAction:
+					(target) =>
+					{
+						itemsRemoved.Add(target);
+					}
+			);
+
+			// assert
+			// Item 1 updated
+			CollectionAssert.Contains(targetList, targetItem1);
+			var item1 = targetList.Single(i => i.Id == 1);
+			Assert.AreEqual("FAKE_SOURCE_12", item1.StringProperty);
+			Assert.AreEqual(0, itemsAdded.Count);
+			Assert.AreEqual(2, itemsUpdated.Count);
+			Assert.AreEqual(0, itemsRemoved.Count);
+		}
+
+		[TestMethod]
+		public void CollectionExt_UpdateFrom_DuplicateTargetKey_BothUpdated()
+		{
+			// arrange
+			TargetClass targetItem1 = new TargetClass() { Id = 1, StringProperty = "FAKE_TARGET_1" };
+			TargetClass targetItem2 = new TargetClass() { Id = 1, StringProperty = "FAKE_TARGET_2" };
+			var targetList = new List<TargetClass>()
+			{
+				targetItem1,
+				targetItem2,
+			};
+			var sourceList = new List<SourceClass>()
+			{
+				new SourceClass() { Id = 1, StringProperty = "FAKE_SOURCE_1" },
+			};
+
+			var itemsAdded = new List<TargetClass>();
+			var itemsUpdated = new List<TargetClass>();
+			var itemsRemoved = new List<TargetClass>();
+
+			// act
+			targetList.UpdateFrom(sourceList,
+				targetKeySelector: target => target.Id,
+				sourceKeySelector: source => source.Id,
+				newItemCreateFunc:
+					source =>
+					{
+						var newItem = new TargetClass()
+						{
+							Id = source.Id,
+							StringProperty = source.StringProperty
+						};
+						itemsAdded.Add(newItem);
+						return newItem;
+					},
+				updateItemAction:
+					(source, target) =>
+					{
+						target.StringProperty = source.StringProperty;
+						itemsUpdated.Add(target);
+					},
+				removeItemAction:
+					(target) =>
+					{
+						itemsRemoved.Add(target);
+					}
+			);
+
+			// assert
+			// Item 1 updated
+			CollectionAssert.Contains(targetList, targetItem1);
+			CollectionAssert.Contains(targetList, targetItem2);
+			Assert.AreEqual(2, targetList.Count);
+			Assert.AreEqual("FAKE_SOURCE_1", targetList[0].StringProperty);
+			Assert.AreEqual("FAKE_SOURCE_1", targetList[1].StringProperty);
+			Assert.AreEqual(0, itemsAdded.Count);
+			Assert.AreEqual(2, itemsUpdated.Count);
+			Assert.AreEqual(0, itemsRemoved.Count);
+		}
+
+		[TestMethod]
+		public void CollectionExt_UpdateFrom_NullNewItemFunc_DoesNotAddNewItems()
+		{
+			// arrange
+			var targetList = new List<TargetClass>()
+			{
+				new TargetClass() { Id = 1, StringProperty = "ITEM_TO_UPDATE" },
+				new TargetClass() { Id = 3, StringProperty = "ITEM_TO_REMOVE" },
+			};
+			var sourceList = new List<SourceClass>()
+			{
+				new SourceClass() { Id = 1, StringProperty = "FAKE_SOURCE_1" },
+				new SourceClass() { Id = 2, StringProperty = "FAKE_SOURCE_2" }
+			};
+
+			// act
+			targetList.UpdateFrom(sourceList,
+				targetKeySelector: target => target.Id,
+				sourceKeySelector: source => source.Id,
+				newItemCreateFunc: null,
+				updateItemAction:
+					(source, target) =>
+					{
+						target.StringProperty = source.StringProperty;
+					},
+				removeItemAction:
+					(target) =>
+					{
+					}
+			);
+
+			// assert
+			Assert.AreEqual(1, targetList.Count);
+			Assert.AreEqual("FAKE_SOURCE_1", targetList.Single().StringProperty);
+		}
+
+		[TestMethod]
+		public void CollectionExt_UpdateFrom_NullUpdateAction_ShouldNotUpdateItems()
+		{
+			// arrange
+			var targetList = new List<TargetClass>()
+			{
+				new TargetClass() { Id = 1, StringProperty = "ITEM_TO_UPDATE" },
+				new TargetClass() { Id = 3, StringProperty = "ITEM_TO_REMOVE" },
+			};
+			var sourceList = new List<SourceClass>()
+			{
+				new SourceClass() { Id = 1, StringProperty = "FAKE_SOURCE_1" },
+				new SourceClass() { Id = 2, StringProperty = "FAKE_SOURCE_2" }
+			};
+
+			// act
+			targetList.UpdateFrom(sourceList,
+				targetKeySelector: target => target.Id,
+				sourceKeySelector: source => source.Id,
+				newItemCreateFunc:
+					source =>
+					{
+						var newItem = new TargetClass()
+						{
+							Id = source.Id,
+							StringProperty = source.StringProperty
+						};
+						return newItem;
+					},
+				updateItemAction: null,
+				removeItemAction:
+					(target) =>
+					{
+					}
+			);
+
+			// assert
+			Assert.AreEqual(2, targetList.Count);
+			Assert.AreEqual("ITEM_TO_UPDATE", targetList.Single(i => i.Id == 1).StringProperty);
+			Assert.AreEqual(1, targetList.Count(i => i.Id == 2));
+		}
+
+		[TestMethod]
+		public void CollectionExt_UpdateFrom_NullRemoveAction_ShouldNotRemoveItems()
+		{
+			// arrange
+			var targetList = new List<TargetClass>()
+			{
+				new TargetClass() { Id = 1, StringProperty = "ITEM_TO_UPDATE" },
+				new TargetClass() { Id = 3, StringProperty = "ITEM_TO_REMOVE" },
+			};
+			var sourceList = new List<SourceClass>()
+			{
+				new SourceClass() { Id = 1, StringProperty = "FAKE_SOURCE_1" },
+				new SourceClass() { Id = 2, StringProperty = "FAKE_SOURCE_2" }
+			};
+
+			// act
+			targetList.UpdateFrom(sourceList,
+				targetKeySelector: target => target.Id,
+				sourceKeySelector: source => source.Id,
+				newItemCreateFunc:
+					source =>
+					{
+						var newItem = new TargetClass()
+						{
+							Id = source.Id,
+							StringProperty = source.StringProperty
+						};
+						return newItem;
+					},
+				updateItemAction:
+					(source, target) =>
+					{
+						target.StringProperty = source.StringProperty;
+					},
+				removeItemAction: null
+			);
+
+			// assert
+			Assert.AreEqual(3, targetList.Count);
+			Assert.AreEqual(1, targetList.Count(i => i.Id == 1));
+			Assert.AreEqual(1, targetList.Count(i => i.Id == 2));
+			Assert.AreEqual(1, targetList.Count(i => i.Id == 3));
+		}
+
+		private class SourceClass
+		{
+			public int Id { get; set; }
+			public string StringProperty { get; set; }
+			public int OtherProperty { get; set; }
+		}
+
+		private class TargetClass
+		{
+			public int Id { get; set; }
+			public string StringProperty { get; set; }
+			public string OtherProperty { get; set; }
+		}
+	}
+}
---
+++ Havit/Linq/CollectionExt.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Havit.Diagnostics.Contracts;
+
+namespace Havit.Linq
+{
+	/// <summary>
+	/// Extension metody pro IColllection&lt;T&gt;.
+	/// </summary>
+	public static class CollectionExt
+    {
+
+		/// <summary>
+		/// Sloučení kolekcí. Do existující kolekce aplikuje změny z kolekce druhé.
+		/// Použitelné pro aplikaci změn z ViewModelu do DataLayer-entities (a naopak), pro importy, atp.
+		/// </summary>
+		/// <remarks>
+		/// Interně používá FullOuterJoin pro výkonovou optimalizaci.
+		/// Mezní situace (logika vyplývá z použití FullOuterJoin):
+		/// - Pokud je duplicita ve zdrojové kolekci, aplikují se záznamy opakovaně (v nedefinovaném pořadí).
+		/// - Pokud je duplicita v cílové kolekci, aktualizují se všechny záznamy.
+		/// - Klíče <c>null</c> se na sebe spárují.
+		/// </remarks>
+		/// <typeparam name="TSource">typ prvků kolekce, kterou chceme aplikovat</typeparam>
+		/// <typeparam name="TTarget">typ prvků cílové kolekce, kterou aktualizujeme</typeparam>
+		/// <typeparam name="TKey">typ párovacího klíče</typeparam>
+		/// <param name="target">aktualizovaná kolekce</param>
+		/// <param name="source">kolekece s hodnotami, které chceme aplikovat</param>
+		/// <param name="targetKeySelector">selektor pro párovací klíč prvků cílové kolekce</param>
+		/// <param name="sourceKeySelector">selektor pro párovací klíč prvků zdrojové (aplikované) kolekce</param>
+		/// <param name="newItemCreateFunc">funkce pro založení nového prvku cílové kolekce (pokud bude funkce <c>null</c>, chybějící prvky ignorujeme)</param>
+		/// <param name="updateItemAction">akce pro aktualizaci spárovaného prvku cílové kolekce z hodnot prvku aplikované kolekce (pokud bude akce <c>null</c>, spárované prvky neaktualizujeme)</param>
+		/// <param name="removeItemAction">akce pro odebrání prvku z aktualizované kolekce, který nebyl nalezen v aplikované kolekci (pokud bude akce <c>null</c>, přebývající prvky neodebíráme) </param>
+		public static void UpdateFrom<TSource, TTarget, TKey>(
+			this ICollection<TTarget> target,
+			IEnumerable<TSource> source,
+			Func<TTarget, TKey> targetKeySelector,
+			Func<TSource, TKey> sourceKeySelector,
+			Func<TSource, TTarget> newItemCreateFunc,
+			Action<TSource, TTarget> updateItemAction,
+			Action<TTarget> removeItemAction)
+		{
+			Contract.Requires<ArgumentNullException>(target != null);
+			Contract.Requires<ArgumentNullException>(source != null);
+			Contract.Requires<ArgumentNullException>(sourceKeySelector != null);
+			Contract.Requires<ArgumentNullException>(targetKeySelector != null);
+
+			var joinedCollections = target.FullOuterJoin(
+				rightSource: source,
+				leftKeySelector: targetKeySelector,
+				rightKeySelector: sourceKeySelector,
+				resultSelector: (targetItem, sourceItem) => new { Target = targetItem, Source = sourceItem });
+
+			foreach (var joinedItem in joinedCollections)
+			{
+				if (joinedItem.Target == null) // && (Source != null)
+				{
+					// new item
+					if (newItemCreateFunc != null)
+					{
+						var newTargetItem = newItemCreateFunc(joinedItem.Source);
+						target.Add(newTargetItem);
+					}
+				}
+				else if (joinedItem.Source != null) // && (Target != null)
+				{
+					// existing item
+					if (updateItemAction != null)
+					{
+						updateItemAction(joinedItem.Source, joinedItem.Target);
+					}
+				}
+				else // (Source == null) && (Target != null)
+				{
+					if (removeItemAction != null)
+					{
+						removeItemAction(joinedItem.Target);
+						target.Remove(joinedItem.Target);
+					}
+
+				}
+			}
+		}
+	}
+}

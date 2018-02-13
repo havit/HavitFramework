7a545d5f1129d76dc94d65c3f275f12d52838c93
Haken, Robert <haken@havit.cz>
2018-02-13 11:57:33 +0000

    37291 ICollection<T>.UpdateFrom() - synchronizace kolekcí - IUnitOfWork[Async].AddUpdateFromResult()

---
+++ Havit.Data.Patterns/UnitOfWorks/UnitOfWorkExt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Havit.Diagnostics.Contracts;
+using Havit.Linq;
+
+namespace Havit.Data.Patterns.UnitOfWorks
+{
+	/// <summary>
+	/// Extension metody pro <c>IUnitOfWork</c>.
+	/// </summary>
+	public static class UnitOfWorkExt
+	{
+		/// <summary>
+		/// Zkrácený zápis pro přesypání výstupu metody UpdateFrom() do UoW.
+		/// Přeleje ItemsAdding do AddRangeForInsert(), ItemsUpdated do AddRangeForUpdate() a ItemsRemoved do AddRangeForDelete().
+		/// </summary>
+		/// <typeparam name="TTarget">typ prvků v cílové kolekci UpdateFrom()</typeparam>
+		public static void AddUpdateFromResult<TTarget>(this IUnitOfWork unitOfWork, UpdateFromResult<TTarget> updateFromResult)
+			where TTarget : class
+		{
+			Contract.Requires<ArgumentNullException>(unitOfWork != null);
+			Contract.Requires<ArgumentNullException>(updateFromResult != null);
+
+			unitOfWork.AddRangeForInsert(updateFromResult.ItemsAdding);
+			unitOfWork.AddRangeForUpdate(updateFromResult.ItemsUpdating);
+			unitOfWork.AddRangeForDelete(updateFromResult.ItemsRemoving);
+		}
+
+		/// <summary>
+		/// Zkrácený zápis pro přesypání výstupu metody UpdateFrom() do UoW.
+		/// Přeleje ItemsAdding do AddRangeForInsert(), ItemsUpdated do AddRangeForUpdate() a ItemsRemoved do AddRangeForDelete().
+		/// </summary>
+		/// <typeparam name="TTarget">typ prvků v cílové kolekci UpdateFrom()</typeparam>
+		public static void AddUpdateFromResult<TTarget>(this IUnitOfWorkAsync unitOfWork, UpdateFromResult<TTarget> updateFromResult)
+			where TTarget : class
+		{
+			Contract.Requires<ArgumentNullException>(unitOfWork != null);
+			Contract.Requires<ArgumentNullException>(updateFromResult != null);
+
+			unitOfWork.AddRangeForInsert(updateFromResult.ItemsAdding);
+			unitOfWork.AddRangeForUpdate(updateFromResult.ItemsUpdating);
+			unitOfWork.AddRangeForDelete(updateFromResult.ItemsRemoving);
+		}
+	}
+}

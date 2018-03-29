9b507feda1d9b27a4a8591e5f7201a5c0b8afc93
Kanda, Jiří <kanda@havit.cz>
2018-03-29 14:48:26 +0000

    Čistka HavitTestConsoleApplication, HavitFramework.sln

--- HavitTestConsoleApplication/Program.cs
+++ HavitTestConsoleApplication/Program.cs
@@ -1,37 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.DirectoryServices.ActiveDirectory;
-using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Data.SqlTypes;
-using System.Diagnostics;
-using System.IO;
-
-using Havit.Services.Ares;
-using System.ComponentModel;
-using Havit;
-using System.Threading.Tasks;
-using System.Runtime.Remoting.Messaging;
-
-using Havit.Services.DirectoryServices.ActiveDirectory;
-using System.DirectoryServices;
-using HavitTestConsoleApplication.ServiceReference1;
 
 namespace HavitTestConsoleApplication
 {
-
 	internal static class Program
 	{
-		#region Main
 		private static void Main(string[] args)
 		{
-			ServiceReference1.WebServiceExceptionTestSoapClient client = new WebServiceExceptionTestSoapClient();
-			client.DoException();
+			// NOOP
 		}
-		#endregion
-
 	}
 }

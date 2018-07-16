96f62e211caab494b3435143244221a0d417dc7d
Činčura, Jiří <cincura@havit.cz>
2018-07-16 14:44:11 +0000

    Added base class for extended attributes.

---
+++ Havit.Business.CodeMigrations/ExtendedPropertyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Havit.Business.CodeMigrations
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+	public abstract class ExtendedPropertyAttribute : Attribute
+	{ }
+}

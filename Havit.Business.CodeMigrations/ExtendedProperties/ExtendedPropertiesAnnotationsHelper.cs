ba30557f6729352497a0f5ad9245c9c687f02afd
Činčura, Jiří <cincura@havit.cz>
2018-07-16 15:31:56 +0000

    Implementation of ExtendedPropertiesMigrationsAnnotationProvider.

--- Havit.Business.CodeMigrations/ExtendedProperties/ExtendedPropertiesAnnotationsHelper.cs
+++ Havit.Business.CodeMigrations/ExtendedProperties/ExtendedPropertiesAnnotationsHelper.cs
@@ -1,9 +1,14 @@
-﻿namespace Havit.Business.CodeMigrations.ExtendedProperties
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Havit.Business.CodeMigrations.ExtendedProperties
 {
 	internal static class ExtendedPropertiesAnnotationsHelper
 	{
-		public static string AnnotationPrefix => "ExtendedProperty";
+		private const string Separator = ":";
+		private const string AnnotationPrefix = "ExtendedProperty";
 
-		public static string BuildAnnotationName(ExtendedPropertyAttribute attribute) => $"{AnnotationPrefix}:{attribute.Name}";
+		public static string BuildAnnotationName(ExtendedPropertyAttribute attribute) => $"{AnnotationPrefix}{Separator}{attribute.Name}";
+
+		public static bool AnnotationsFilter(IAnnotation annotation) => annotation.Name.StartsWith($"{AnnotationPrefix}{Separator}");
 	}
 }
--- Havit.Business.CodeMigrations/ExtendedProperties/ExtendedPropertiesMigrationsAnnotationProvider.cs
+++ Havit.Business.CodeMigrations/ExtendedProperties/ExtendedPropertiesMigrationsAnnotationProvider.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.SqlServer.Migrations.Internal;
 
 namespace Havit.Business.CodeMigrations.ExtendedProperties
@@ -8,5 +12,17 @@ namespace Havit.Business.CodeMigrations.ExtendedProperties
 		public ExtendedPropertiesMigrationsAnnotationProvider(MigrationsAnnotationProviderDependencies dependencies)
 			: base(dependencies)
 		{ }
+
+		public override IEnumerable<IAnnotation> For(IProperty property)
+		{
+			return base.For(property)
+				.Concat(property.GetAnnotations().Where(ExtendedPropertiesAnnotationsHelper.AnnotationsFilter));
+		}
+
+		public override IEnumerable<IAnnotation> ForRemove(IEntityType entityType)
+		{
+			return base.ForRemove(entityType)
+				.Concat(entityType.GetAnnotations().Where(ExtendedPropertiesAnnotationsHelper.AnnotationsFilter));
+		}
 	}
 }

bd506825166053f9abad28b1fa77156a15c05a36
Louma, Miroslav <louma@havit.cz>
2018-07-16 11:15:18 +0000

    BL Code Migrations: public constructor for BusinessLayerDbContext

--- Havit.Business.CodeMigrations/BusinessLayerDbContext.cs
+++ Havit.Business.CodeMigrations/BusinessLayerDbContext.cs
@@ -8,6 +8,15 @@ namespace Havit.Business.CodeMigrations
 {
 	public class BusinessLayerDbContext : DbContext
 	{
+		public BusinessLayerDbContext()
+		{
+		}
+
+		public BusinessLayerDbContext(DbContextOptions options)
+			: base(options)
+		{
+		}
+
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			base.OnModelCreating(modelBuilder);

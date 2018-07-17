4bc8038b38636bc26c46508a15e04817e6cd5a79
Činčura, Jiří <cincura@havit.cz>
2018-07-17 09:48:09 +0000

    This might actually fix the build.

---
+++ Havit.Business.CodeMigrations.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("HAVIT Code Migrations Tests")]
+[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - Entity Framework Patterns Extensions Tests")]
+
+namespace Havit.Business.CodeMigrations.Tests
+{
+	internal static class AssemblyInfo
+	{
+
+	}
+}

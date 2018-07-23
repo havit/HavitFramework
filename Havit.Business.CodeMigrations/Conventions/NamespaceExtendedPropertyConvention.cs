c5913789a7dd48d2fed96ef5216d7efd3649782c
Louma, Miroslav <louma@havit.cz>
2018-07-23 11:18:55 +0000

    CodeMigrations: namespace extended property convention

---
+++ Havit.Business.CodeMigrations/Conventions/NamespaceExtendedPropertyConvention.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Reflection;
+using Havit.Business.CodeMigrations.ExtendedProperties;
+using Havit.Business.CodeMigrations.ExtendedProperties.Attributes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Havit.Business.CodeMigrations.Conventions
+{
+	public static class NamespaceExtendedPropertyConvention
+	{
+		public static void Apply(ModelBuilder modelBuilder)
+		{
+			var tables = modelBuilder.Model.GetEntityTypes();
+			foreach (IMutableEntityType table in tables)
+			{
+				if (table.ClrType.GetCustomAttributes<NamespaceAttribute>().Any())
+				{
+					continue;
+				}
+
+				//table.AddAnnotation(ExtendedPropertiesAnnotationsHelper.BuildAnnotationName(NamespaceAttribute.PropertyName), table.ClrType.Namespace);
+			}
+		}
+	}
+}
--- Havit.Business.CodeMigrations/ExtendedProperties/Attributes/NamespaceAttribute.cs
+++ Havit.Business.CodeMigrations/ExtendedProperties/Attributes/NamespaceAttribute.cs
@@ -4,16 +4,16 @@ namespace Havit.Business.CodeMigrations.ExtendedProperties.Attributes
 {
 	public class NamespaceAttribute : ExtendedPropertyAttribute
 	{
-		private readonly string _name;
+		private readonly string name;
 
 		public NamespaceAttribute(string name)
 		{
-			_name = name;
+			this.name = name;
 		}
 
 		public override IDictionary<string, string> ExtendedProperties => new Dictionary<string, string>()
 		{
-			{ "Namespace", _name },
+			{ "Namespace", name },
 		};
 	}
 }

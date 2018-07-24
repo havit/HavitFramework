4622b30f6b5661dbc24f31a2f48af9e89ba71340
Louma, Miroslav <louma@havit.cz>
2018-07-24 10:45:28 +0000

    CodeMigrations: full support for all Cache-related extended properties using Cache attribute

---
+++ Havit.Business.CodeMigrations/DictionaryExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Havit.Diagnostics.Contracts;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace Havit.Business.CodeMigrations
+{
+	internal static class DictionaryExtensions
+	{
+		public static Dictionary<TKey, TValue> AddIfNotDefault<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value)
+		{
+			Contract.Requires<ArgumentNullException>(dictionary != null);
+			Contract.Requires<ArgumentNullException>(key != null);
+
+			if (value != null)
+			{
+				dictionary.Add(key, value);
+			}
+
+			return dictionary;
+		}
+
+		public static Dictionary<string, string> AddIfNotDefault(this Dictionary<string, string> dictionary, string key, object value)
+		{
+			Contract.Requires<ArgumentNullException>(dictionary != null);
+			Contract.Requires<ArgumentNullException>(key != null);
+
+			if (value != null && !value.GetType().IsDefaultValue(value))
+			{
+				dictionary.Add(key, value.ToString());
+			}
+
+			return dictionary;
+		}
+	}
+}
--- Havit.Business.CodeMigrations/ExtendedProperties/Attributes/CacheAttribute.cs
+++ Havit.Business.CodeMigrations/ExtendedProperties/Attributes/CacheAttribute.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using Havit.Services.Caching;
 
 namespace Havit.Business.CodeMigrations.ExtendedProperties.Attributes
 {
 	[AttributeUsage(AttributeTargets.Class)]
 	public class CacheAttribute : ExtendedPropertiesAttribute
 	{
+		public bool SuppressPreload { get; set; }
+
+		public CacheItemPriority Priority { get; set; }
+
+		public int AbsoluteExpiration { get; set; }
+
+		public int SlidingExpiration { get; set; }
+
+
 		public override IDictionary<string, string> ExtendedProperties => new Dictionary<string, string>
-		{
-			{ "Cache", "true" }
-		};
+			{
+				{ "Cache", "true" }
+			}.AddIfNotDefault("Cache_Priority", Priority)
+			.AddIfNotDefault("Cache_AbsoluteExpiration", AbsoluteExpiration)
+			.AddIfNotDefault("Cache_SlidingExpiration", SlidingExpiration);
 	}
 }

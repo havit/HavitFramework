e2b959ef9755fd0b0fd5022451603eae5f5863b0
Louma, Miroslav <louma@havit.cz>
2018-07-24 15:44:21 +0000

    CodeMigrations: attribute for generating Collection-related extended properties

---
+++ Havit.Business.CodeMigrations/ExtendedProperties/Attributes/CollectionAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Havit.Business.CodeMigrations.ExtendedProperties.Attributes
+{
+	public class CollectionAttribute : ExtendedPropertiesAttribute
+	{
+		/// <summary>
+		/// Je-li nastaveno na true, načítají se do kolekce i (příznakem) smazané záznamy.
+		/// </summary>
+		public bool? IncludeDeleted { get; set; }
+
+		/// <summary>
+		/// Vlastnost říká, že se automaticky načtou všechny prvky kolekce v okamžiku prvního přístupu ke kolekci (zavolá se na kolekci metoda LoadAll).
+		/// </summary>
+		public bool? LoadAll { get; set; }
+
+		/// <summary>
+		/// Pořadí, v jakém jsou položky kolekce načteny z databáze do paměti. Obsahuje SQL výraz, který se vloží do části ORDER BY. Názvy sloupců musí být vloženy do složených závorek! Příklady: "{Order}", "COALESCE({Order}, {TabulkaID})
+		/// </summary>
+		public string Sorting { get; set; }
+
+		/// <summary>
+		/// Určuje režim klonování prvků kolekce při klonování objektu. Kolekce typu 1:N nepodporují klonování typu Shallow.
+		/// - No - prvky se neklonují
+		/// - Shallow - připojí se prvky originálu(budou tak sdílené, vhodné jen pro M:N kolekce)
+		/// - Deep - připojí se klony prvků originálu.
+		/// </summary>
+		public string CloneMode { get; set; }
+
+		public override IDictionary<string, string> ExtendedProperties => new Dictionary<string, string>()
+			.AddIfNotDefault("Collection_IncludeDeleted", IncludeDeleted)
+			.AddIfNotDefault("Collection_LoadAll", LoadAll)
+			.AddIfNotDefault("Collection_Sorting", Sorting)
+			.AddIfNotDefault("Collection_CloneMode", CloneMode);
+	}
+}

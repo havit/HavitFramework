0c29b5ad18d6fad290b2d79b236735c43d8c7e9d
Louma, Miroslav <louma@havit.cz>
2018-07-25 13:44:42 +0000

    CodeMigrations: support for deleting stored procedure

--- Havit.Business.CodeMigrations/StoredProcedures/StoredProceduresMigrationsGenerator.cs
+++ Havit.Business.CodeMigrations/StoredProcedures/StoredProceduresMigrationsGenerator.cs
@@ -1,24 +1,24 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Havit.Business.CodeMigrations.StoredProcedures
 {
-	public class StoredProceduresMigrationsGenerator : IMigrationsSqlGenerator
+    public class StoredProceduresMigrationsGenerator : IMigrationsSqlGenerator
 	{
-	    private readonly IRelationalCommandBuilderFactory relationalCommandBuilderFactory;
+		public MigrationsSqlGeneratorDependencies Dependencies { get; }
 
-	    public StoredProceduresMigrationsGenerator(IRelationalCommandBuilderFactory relationalCommandBuilderFactory)
-	    {
-	        this.relationalCommandBuilderFactory = relationalCommandBuilderFactory;
-	    }
+		public StoredProceduresMigrationsGenerator(MigrationsSqlGeneratorDependencies dependencies)
+		{
+			Dependencies = dependencies;
+		}
 
 	    public IReadOnlyList<MigrationCommand> Generate(IReadOnlyList<MigrationOperation> operations, IModel model = null)
 	    {
-	        MigrationCommandListBuilder builder = new MigrationCommandListBuilder(relationalCommandBuilderFactory);
+	        MigrationCommandListBuilder builder = new MigrationCommandListBuilder(Dependencies.CommandBuilderFactory);
 	        AlterDatabaseOperation alterDatabaseOperation = operations.OfType<AlterDatabaseOperation>().FirstOrDefault();
 	        if (alterDatabaseOperation != null)
 	        {
@@ -28,16 +28,31 @@ namespace Havit.Business.CodeMigrations.StoredProcedures
 	        return builder.GetCommandList();
         }
 
-	    private static void Generate(AlterDatabaseOperation operation, IModel model, MigrationCommandListBuilder builder)
-	    {
-	        var newAnnotations = operation.GetAnnotations().Where(StoredProceduresAnnotationsHelper.IsStoredProcedureAnnotation).ToDictionary(x => x.Name, StoredProceduresAnnotationsHelper.Comparer);
+		protected void Generate(AlterDatabaseOperation operation, IModel model, MigrationCommandListBuilder builder)
+		{
+			var oldAnnotations = operation.OldDatabase.GetAnnotations().Where(StoredProceduresAnnotationsHelper.IsStoredProcedureAnnotation).ToDictionary(x => x.Name, StoredProceduresAnnotationsHelper.Comparer);
+			var newAnnotations = operation.GetAnnotations().Where(StoredProceduresAnnotationsHelper.IsStoredProcedureAnnotation).ToDictionary(x => x.Name, StoredProceduresAnnotationsHelper.Comparer);
 
-	        foreach (var annotation in newAnnotations.Select(p => p.Value))
+		    foreach (var annotation in oldAnnotations.Where(x => !newAnnotations.ContainsKey(x.Key)).Select(x => x.Value))
+		    {
+			    var name = StoredProceduresAnnotationsHelper.ParseAnnotationName(annotation);
+			    DropStoredProcedure(model.Relational().DefaultSchema ?? "dbo", name, builder);
+		    }
+
+			// Both for create and update
+			foreach (var annotation in newAnnotations.Select(p => p.Value))
 	        {
 	            var sql = (string)annotation.Value;
 
 	            builder.Append(sql).EndCommand();
 	        }
 	    }
+
+		private void DropStoredProcedure(string schema, string name, MigrationCommandListBuilder builder)
+		{
+			builder
+				.Append($"DROP PROCEDURE [{schema}].[{name}]")
+				.EndCommand();
+		}
 	}
 }

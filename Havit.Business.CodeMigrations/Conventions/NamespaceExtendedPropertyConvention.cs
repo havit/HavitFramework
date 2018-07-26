b959dbd51fc007a66a3167dc1fa74db0203155e5
Louma, Miroslav <louma@havit.cz>
2018-07-26 11:11:34 +0000

    CodeMigrations: removed Namespace attribute - currently broken and not necessary (just move class to different namespace)

--- Havit.Business.CodeMigrations/Conventions/NamespaceExtendedPropertyConvention.cs
+++ Havit.Business.CodeMigrations/Conventions/NamespaceExtendedPropertyConvention.cs
@@ -1,29 +1,21 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using Havit.Business.CodeMigrations.ExtendedProperties;
-using Havit.Business.CodeMigrations.ExtendedProperties.Attributes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Havit.Business.CodeMigrations.Conventions
 {
-	public static class NamespaceExtendedPropertyConvention
+    public static class NamespaceExtendedPropertyConvention
 	{
 		public static void ApplyDefaultNamespaces(this ModelBuilder modelBuilder)
 		{
 			var tables = modelBuilder.Model.GetEntityTypes();
 			foreach (IMutableEntityType table in tables)
 			{
-				if (table.ClrType.GetCustomAttributes<NamespaceAttribute>().Any())
-				{
-					continue;
-				}
-
 				string entityNamespace = table.ClrType.Namespace?.Replace(table.ClrType.Assembly.GetName().Name, "").Trim('.');
 				table.AddExtendedProperties(new Dictionary<string, string>()
 				{
-					{ NamespaceAttribute.PropertyName, entityNamespace },
+					{ "Namespace", entityNamespace },
 				});
 			}
 		}

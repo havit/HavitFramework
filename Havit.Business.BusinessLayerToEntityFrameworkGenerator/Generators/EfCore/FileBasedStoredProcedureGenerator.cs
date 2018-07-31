45b13cc2c98f42f497bb6c9b27c95d5dc418fbd9
Louma, Miroslav <louma@havit.cz>
2018-07-31 12:51:32 +0000

    BL2EF generator: fixed generating SQL files with stored procedures (generate only CREATE statement)

--- Havit.Business.BusinessLayerToEntityFrameworkGenerator/Generators/EfCore/FileBasedStoredProcedureGenerator.cs
+++ Havit.Business.BusinessLayerToEntityFrameworkGenerator/Generators/EfCore/FileBasedStoredProcedureGenerator.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Havit.Business.BusinessLayerGenerator.Csproj;
 using Havit.Business.BusinessLayerToEntityFrameworkGenerator.Metadata;
 using Microsoft.SqlServer.Management.Smo;
@@ -14,7 +16,15 @@ namespace Havit.Business.BusinessLayerToEntityFrameworkGenerator.Generators.EfCo
 			{
 				string fileName = Path.Combine(Path.GetDirectoryName(modelCsprojFile.Path), dbStoredProcedure.GeneratedFile);
 				Directory.CreateDirectory(Path.GetDirectoryName(fileName));
-                dbStoredProcedure.StoredProcedure.Script(new ScriptingOptions() { FileName = fileName });
+
+				string[] lines = dbStoredProcedure.StoredProcedure.Script(new ScriptingOptions())
+					.Cast<string>()
+					.SelectMany(l => l.Split(new[] { Environment.NewLine }, StringSplitOptions.None))
+					.SkipWhile(s => !s.Trim().StartsWith("CREATE"))
+					.Where(s => s.Trim() != "GO")
+					.ToArray();
+
+				File.WriteAllLines(fileName, lines);
 			}
 		}
 	}

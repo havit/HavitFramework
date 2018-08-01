9dc771b7afb3444cacffd9ba3f31a93170823627
Činčura, Jiří <cincura@havit.cz>
2018-08-01 08:00:23 +0000

    Cleanup.

--- Havit.Business.CodeMigrations/ExtendedProperties/ExtendedPropertiesMigrationsSqlGenerator.cs
+++ Havit.Business.CodeMigrations/ExtendedProperties/ExtendedPropertiesMigrationsSqlGenerator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;

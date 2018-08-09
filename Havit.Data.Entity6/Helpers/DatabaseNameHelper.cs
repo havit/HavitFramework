0f68e1d7ea9d0b8c00d5129361a7ba387159c0ee
Kanda, Jiří <kanda@havit.cz>
2018-08-09 12:58:35 +0000

    Kosmetika.

--- Havit.Data.Entity6/Helpers/DatabaseNameHelper.cs
+++ Havit.Data.Entity6/Helpers/DatabaseNameHelper.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace Havit.Data.Entity.Helpers
 {
+	/// <summary>
+	/// Pomocné metody pro práci s názvem databáze.
+	/// </summary>
 	public static class DatabaseNameHelper
 	{
+		/// <summary>
+		/// Vrátí název databáze pro účely použití unit testů.
+		/// Pokud existuje proměná prostředí BUILD_BUILDNUMBER, pak se použije databaseNameBase-BUILD_BUILDNUMBER, jinak jen databaseNameBase.
+		/// </summary>
 		public static string GetDatabaseNameForUnitTest(string databaseNameBase)
 		{
 			string buildNumber = Environment.GetEnvironmentVariable("BUILD_BUILDNUMBER");

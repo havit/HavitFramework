c735a4271cabb9af7a1e62ec2469116a3e937100
Kanda, Jiří <kanda@havit.cz>
2018-08-16 15:42:30 +0000

    Převod Havit.Data.Entity6.*.Tests.csproj na nové CSPROJ

--- Havit.Data.Entity6.Tests/Properties/AssemblyInfo.cs
+++ Havit.Data.Entity6.Tests/Properties/AssemblyInfo.cs
@@ -1,36 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Havit.Data.Entity6.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Havit.Data.Entity6.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("09db04ec-99a1-4341-a137-eb724d2b6b37")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle("HAVIT Entity Framework 6 Extensions - Tests")]
+[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - Entity Framework 6 Extensions - Tests")]

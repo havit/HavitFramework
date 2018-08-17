986b4a837ad11faa95e539bbae26e84e35d66498
Louma, Miroslav <louma@havit.cz>
2018-08-17 16:13:53 +0000

    BL2EF generator/EFCore.BL: generate DataLoadPower attribute into SP DbInjectors

---
+++ Havit.Data.EntityFrameworkCore.BusinessLayer/DbInjections/ExtendedProperties/Attributes/DataLoadPowerType.cs
@@ -0,0 +1,9 @@
+﻿namespace Havit.Data.EntityFrameworkCore.BusinessLayer.DbInjections.ExtendedProperties.Attributes
+{
+	public enum DataLoadPowerType
+	{
+		Ghost,
+		PartialLoad,
+		FullLoad
+	}
+}

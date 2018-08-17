0244558cabe0fe742d3db60b31ed972b3b687574
Louma, Miroslav <louma@havit.cz>
2018-08-17 10:51:15 +0000

    EFCore.BL: readded BusinessObjectBaseTypeAttribute

---
+++ Havit.Data.EntityFrameworkCore.BusinessLayer.Abstractions/Attributes/ExtendedProperties/BusinessObjectBaseTypeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Havit.Data.EntityFrameworkCore.BusinessLayer.Attributes.ExtendedProperties
+{
+	public class BusinessObjectBaseTypeAttribute : ExtendedPropertiesAttribute
+	{
+		public string TypeName { get; }
+
+		public BusinessObjectBaseTypeAttribute(string typeName)
+		{
+			TypeName = typeName;
+		}
+
+		public override IDictionary<string, string> GetExtendedProperties(MemberInfo memberInfo) => new Dictionary<string, string>
+		{
+			{ "BusinessObjectBaseType", TypeName }
+		};
+	}
+}

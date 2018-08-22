6a63be600d949d0d1fc3fa66048892ca0bcc9fb1
Louma, Miroslav <louma@havit.cz>
2018-08-22 08:34:21 +0000

    EFCore.BL: extension method for creating migration command that reads SQL statements from resource file.

---
+++ Havit.Data.EntityFrameworkCore.BusinessLayer/MigrationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Havit.Diagnostics.Contracts;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Havit.Data.EntityFrameworkCore.BusinessLayer
+{
+    public static class MigrationExtensions
+    {
+        public static void SqlResource(this MigrationBuilder migrationBuilder, string resourceName, bool suppressTransaction = false)
+        {
+			Contract.Requires<ArgumentNullException>(resourceName != null);
+
+            using (var textStream = new StreamReader(Assembly.GetCallingAssembly().GetManifestResourceStream(resourceName)))
+            {
+                string sql = textStream.ReadToEnd();
+	            migrationBuilder.Sql(sql, suppressTransaction);
+            }
+        }
+    }
+}

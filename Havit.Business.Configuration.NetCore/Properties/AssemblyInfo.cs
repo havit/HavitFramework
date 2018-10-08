e1aaa7c9a9e8e11b75bde6acfeb47577a3e4fb7d
Louma, Miroslav <louma@havit.cz>
2018-10-08 15:30:22 +0200

    Havit.Business.Configuration: AssemblyInfo for .NetCore, cleanup for Configuration

---
+++ Havit.Business.Configuration.NetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("HAVIT Framework Library for configuration-related functionality (.NET Core).")]
+[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - HAVIT Business Layer Library to manage configuration (connection strings) in Business Layer projects")]
+

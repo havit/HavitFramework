1c443d0504e263ac1cec456323fa2d5fbdfd33c9
Kanda, Jiří <kanda@havit.cz>
2018-10-16 10:33:46 +0200

    CacheOptions - možnost uzamkrnout do readonly režimu (freeze)

--- Havit.Services/Caching/CacheOptions.cs
+++ Havit.Services/Caching/CacheOptions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Havit.Services.Caching
 {
@@ -10,21 +11,92 @@ namespace Havit.Services.Caching
 		/// <summary>
 		/// Doba, po které má být položka vyhozena z cache (od okamžiku přidání).
 		/// </summary>
-		public TimeSpan? AbsoluteExpiration { get; set; }
+		public TimeSpan? AbsoluteExpiration
+		{
+			get
+			{
+				return absoluteExpiration;
+			}
+			set
+			{
+				ThrowIfFrozen();
+				absoluteExpiration = value;
+			}
+		}
+		private TimeSpan? absoluteExpiration;
 
 		/// <summary>
 		/// Doba, po které má být položka vyhozena z cache (od posledního přístupu k položce).
 		/// </summary>
-		public TimeSpan? SlidingExpiration { get; set; }
+		public TimeSpan? SlidingExpiration
+		{
+			get
+			{
+				return slidingExpiration;
+			}
+			set
+			{
+				ThrowIfFrozen();
+				slidingExpiration = value;
+			}
+		}
+		private TimeSpan? slidingExpiration;
 
 		/// <summary>
 		/// Priorita položky v cache.
 		/// </summary>
-		public CacheItemPriority Priority { get; set; } = CacheItemPriority.Normal;
+		public CacheItemPriority Priority
+		{
+			get
+			{
+				return priority;
+			}
+			set
+			{
+				ThrowIfFrozen();
+				priority = value;
+			}
+		}
+		private CacheItemPriority priority = CacheItemPriority.Normal;
 
 		/// <summary>
 		/// Cache dependencies - Klíče, na kterých je položka závislá.
 		/// </summary>
-		public string[] CacheDependencyKeys { get; set; }
+		public string[] CacheDependencyKeys
+		{
+			get
+			{
+				return cacheDependencyKeys;
+			}
+			set
+			{
+				ThrowIfFrozen();
+				cacheDependencyKeys = value;
+			}
+		}
+		private string[] cacheDependencyKeys;
+
+		#region Freeze, ThrowIfFrozen
+		/// <summary>
+		/// Zamkne kolekci vůči změnám. Od toho okamžiku není možné změnit položky v kolekci.
+		/// </summary>
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		public void Freeze()
+		{
+			isFrozen = true;
+		}
+		private bool isFrozen = false;
+
+		/// <summary>
+		/// Pokud je nastaven příznak isFrozen, vyhodí výjimku InvalidOperationException.
+		/// </summary>
+		private void ThrowIfFrozen()
+		{
+			if (this.isFrozen)
+			{
+				throw new InvalidOperationException("CacheOptions are frozen, cannot be modified.");
+			}
+		}
+		#endregion
 	}
 }

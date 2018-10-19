4a9a0d02dc92e03020f3bfc77d1305bfc64922e0
Kanda, Jiří <kanda@havit.cz>
2018-10-19 08:40:01 +0200

    NuGet balíčky pro Havit.ApplicationInsights.DependencyCollector, Havit.CastleWindsor.WebForms

--- Havit.CastleWindsor.WebForms/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("HAVIT Castle.Windsor Extension for WebForms")]
-[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - Castle.Windsor Extension for WebForms")]

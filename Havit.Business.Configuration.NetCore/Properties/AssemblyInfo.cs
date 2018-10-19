d2b440c3590ed346bafc6e59eee0a9c720c92940
Kanda, Jiří <kanda@havit.cz>
2018-10-19 11:07:40 +0200

    NuGet balíčky pro Havit.Business.Configuration + Havit.Business.Configuration.NetCore

--- Havit.Business.Configuration.NetCore/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("HAVIT Framework Library for configuration-related functionality (.NET Core).")]
-[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - HAVIT Business Layer Library to manage configuration (connection strings) in Business Layer projects")]
-
--- Havit.Business.Configuration/Properties/AssemblyInfo.cs
+++ Havit.Business.Configuration/Properties/AssemblyInfo.cs
@@ -1,8 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("HAVIT Business Layer Library to seed database schema for configuration")]
-[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - HAVIT Business Layer Library to manage configuration (connection strings) in Business Layer projects")]
-
 [assembly: InternalsVisibleTo("Havit.Business.Configuration.Tests")]
 

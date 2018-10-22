f04b622ec8f60c2c2d7345b6260d24d62b2d37d5
Kanda, Jiří <kanda@havit.cz>
2018-10-22 15:08:10 +0200

    Kosmetika *.csproj

--- Havit.Data.EntityFrameworkCore.BusinessLayer.Tests/Properties/AssemblyInfo.cs
+++ Havit.Data.EntityFrameworkCore.BusinessLayer.Tests/Properties/AssemblyInfo.cs
@@ -1,8 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("HAVIT Code Migrations Tests")]
-[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - Entity Framework Patterns Extensions Tests")]
-
 namespace Havit.Data.EntityFrameworkCore.BusinessLayer.Tests.Properties
 {
 	internal static class AssemblyInfo

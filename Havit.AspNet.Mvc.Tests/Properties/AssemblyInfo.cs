5aa6c8c32008148f6da755705c6a2569b6c34deb
Kanda, Jiří <kanda@havit.cz>
2018-10-22 15:09:26 +0200

    Generování NuGet balíčků pro Havit.AspNet.Mvc.

--- Havit.AspNet.Mvc.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("HAVIT AspNet MVC Extensions Tests")]
-[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - AspNet MVC Extensions Tests")]
--- Havit.AspNet.Mvc/Properties/AssemblyInfo.cs
+++ Havit.AspNet.Mvc/Properties/AssemblyInfo.cs
@@ -1,7 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("HAVIT AspNet MVC Extensions")]
-[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - AspNet MVC Extensions")]
-
 [assembly: InternalsVisibleTo("Havit.AspNet.Mvc.Tests")]

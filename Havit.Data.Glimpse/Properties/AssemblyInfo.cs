810c196dbc70e8f0d5b8f0fc9b9f816242f88dc1
Kanda, Jiří <kanda@havit.cz>
2018-10-22 15:22:53 +0200

    Generování NuGet balíčku pro Havit.Data.Glimpse

--- Havit.Data.Glimpse/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("HAVIT Glimpse Extension for HAVIT Data Class Library")]
-[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - HAVIT Glimpse Extension for HAVIT Data Class Library")]

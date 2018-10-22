1fae3786f3b354709ce61c5d5f0539670c6de9ca
Kanda, Jiří <kanda@havit.cz>
2018-10-22 14:36:59 +0200

    Tvorba NuGet balíčků pro Havit.Business, Havit.Data.EntityFrameworkCore.BusinessLayer+Abstractions, Havit.Enterprise.Web

--- Havit.Business/Properties/AssemblyInfo.cs
+++ Havit.Business/Properties/AssemblyInfo.cs
@@ -1,7 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("HAVIT Business Class Library")]
-[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - Business Class Library")]
-
 [assembly: InternalsVisibleTo("Havit.BusinessTest")]
--- Havit.Data.EntityFrameworkCore.BusinessLayer.Abstractions/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("HAVIT Entity Framework Core Library to seed database schema for Havit.Business & Havit.Business.BusinessLayerGenerator - Abstractions")]
-[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - HAVIT Entity Framework Core Library to seed database schema for Havit.Business & Havit.Business.BusinessLayerGenerator - Abstractions")]
-
-

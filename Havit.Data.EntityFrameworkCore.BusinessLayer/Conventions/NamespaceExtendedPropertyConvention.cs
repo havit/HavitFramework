76416887f3c726b8c65dc7afe40e86fecd7e4f59
Kanda, Jiří <kanda@havit.cz>
2018-11-15 10:58:02 +0100

    Issue 41555: AttendanceActivityLocalization, AttendanceAllowanceLocalization a další lokalizační tabulky - chybí extended property zdroje, přebývá Namespace
    Related Work Items: #41555

--- Havit.Data.EntityFrameworkCore.BusinessLayer/Conventions/NamespaceExtendedPropertyConvention.cs
+++ Havit.Data.EntityFrameworkCore.BusinessLayer/Conventions/NamespaceExtendedPropertyConvention.cs
@@ -19,6 +19,17 @@ namespace Havit.Data.EntityFrameworkCore.BusinessLayer.Conventions
 		{
 			foreach (IMutableEntityType entityType in modelBuilder.Model.GetApplicationEntityTypes(includeManyToManyEntities: false))
 			{
+				// pokud jde o lokalizační tabulku, která je ve stejném namespaces s lokalizovanou tabulkou, nebudeme extended property generovat
+				var localizationInterfaceType = entityType.ClrType.GetInterfaces().FirstOrDefault(iType => iType.IsGenericType && (iType.GetGenericTypeDefinition() == typeof(Havit.Model.Localizations.ILocalization<,>)));
+				if (localizationInterfaceType != null)
+				{
+					Type localizedType = localizationInterfaceType.GetGenericArguments().First(); // TLocalizedEntity
+					if (localizedType.Namespace == entityType.ClrType.Namespace) // namespace lokalizované a této lokalizační třídy je stejný
+					{
+						continue; // nebudeme extended property generovat
+					}
+				}
+
 				string entityNamespace = entityType.ClrType.Namespace?.Replace(entityType.ClrType.Assembly.GetName().Name, "").Trim('.');
 				if (!String.IsNullOrEmpty(entityNamespace))
 				{

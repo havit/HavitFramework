a51c6ba2d96786be972c72d80b6b44fe741903b5
Kanda, Jiří <kanda@havit.cz>
2018-11-19 11:04:21 +0100

    Issue 41575: AttendanceRequest_File - generuje namespace, ačkoliv jde o M:N tabulku (resp. hybrid se spoustou Ignored)

--- Havit.Data.EntityFrameworkCore.BusinessLayer/Conventions/NamespaceExtendedPropertyConvention.cs
+++ Havit.Data.EntityFrameworkCore.BusinessLayer/Conventions/NamespaceExtendedPropertyConvention.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Havit.Data.EntityFrameworkCore.BusinessLayer.Attributes.ExtendedProperties;
 using Havit.Data.EntityFrameworkCore.BusinessLayer.ExtendedProperties;
 using Havit.Data.EntityFrameworkCore.Conventions;
 using Havit.Data.EntityFrameworkCore.Metadata;
@@ -19,8 +20,15 @@ namespace Havit.Data.EntityFrameworkCore.BusinessLayer.Conventions
 		{
 			foreach (IMutableEntityType entityType in modelBuilder.Model.GetApplicationEntityTypes(includeManyToManyEntities: false))
 			{
+				// pokud jde o many to many tabulku, která má další ignorované sloupce, nebudeme extended property generovat
+				var notIgnoredProperties = entityType.GetProperties().Where(property => !property.IsShadowProperty && !property.PropertyInfo.GetCustomAttributes(typeof(IgnoredAttribute), false).Any()).ToList();
+				if ((notIgnoredProperties.Count == 2) && notIgnoredProperties.All(property => property.IsPrimaryKey()) && notIgnoredProperties.All(property => property.IsForeignKey()))
+				{
+					continue;
+				}
+
 				// pokud jde o lokalizační tabulku, která je ve stejném namespaces s lokalizovanou tabulkou, nebudeme extended property generovat
-				var localizationInterfaceType = entityType.ClrType.GetInterfaces().FirstOrDefault(iType => iType.IsGenericType && (iType.GetGenericTypeDefinition() == typeof(Havit.Model.Localizations.ILocalization<,>)));
+					var localizationInterfaceType = entityType.ClrType.GetInterfaces().FirstOrDefault(iType => iType.IsGenericType && (iType.GetGenericTypeDefinition() == typeof(Havit.Model.Localizations.ILocalization<,>)));
 				if (localizationInterfaceType != null)
 				{
 					Type localizedType = localizationInterfaceType.GetGenericArguments().First(); // TLocalizedEntity

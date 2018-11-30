b83d3f86d6a580ec95b8071f3f1778948ee27244
Louma, Miroslav <louma@havit.cz>
2018-11-30 14:31:08 +0100

    Formatting in NamespaceExtendedPropertyConvention

--- Havit.Data.EntityFrameworkCore.BusinessLayer/Conventions/NamespaceExtendedPropertyConvention.cs
+++ Havit.Data.EntityFrameworkCore.BusinessLayer/Conventions/NamespaceExtendedPropertyConvention.cs
@@ -13,7 +13,7 @@ namespace Havit.Data.EntityFrameworkCore.BusinessLayer.Conventions
 	/// <summary>
 	/// Konvencia pre nastavenie Namespace extended property na všetky entity v modeli. Z namespace triedy sa odstráni názov assembly (Havit.{Projekt}.Model.Common -> Common).
 	/// </summary>
-    public class NamespaceExtendedPropertyConvention : IModelConvention
+	public class NamespaceExtendedPropertyConvention : IModelConvention
 	{
 		/// <inheritdoc />
 		public void Apply(ModelBuilder modelBuilder)
@@ -21,14 +21,22 @@ namespace Havit.Data.EntityFrameworkCore.BusinessLayer.Conventions
 			foreach (IMutableEntityType entityType in modelBuilder.Model.GetApplicationEntityTypes(includeManyToManyEntities: false))
 			{
 				// pokud jde o many to many tabulku, která má další ignorované sloupce, nebudeme extended property generovat
-				var notIgnoredProperties = entityType.GetProperties().Where(property => !property.IsShadowProperty && !property.PropertyInfo.GetCustomAttributes(typeof(IgnoredAttribute), false).Any()).ToList();
-				if ((notIgnoredProperties.Count == 2) && notIgnoredProperties.All(property => property.IsPrimaryKey()) && notIgnoredProperties.All(property => property.IsForeignKey()))
+				var notIgnoredProperties = entityType.GetProperties()
+					.Where(property => !property.IsShadowProperty &&
+					                   !property.PropertyInfo.GetCustomAttributes(typeof(IgnoredAttribute), false).Any())
+					.ToList();
+				if ((notIgnoredProperties.Count == 2) &&
+				    notIgnoredProperties.All(property => property.IsPrimaryKey()) &&
+				    notIgnoredProperties.All(property => property.IsForeignKey()))
 				{
 					continue;
 				}
 
 				// pokud jde o lokalizační tabulku, která je ve stejném namespaces s lokalizovanou tabulkou, nebudeme extended property generovat
-					var localizationInterfaceType = entityType.ClrType.GetInterfaces().FirstOrDefault(iType => iType.IsGenericType && (iType.GetGenericTypeDefinition() == typeof(Havit.Model.Localizations.ILocalization<,>)));
+				var localizationInterfaceType = entityType.ClrType.GetInterfaces()
+					.FirstOrDefault(iType =>
+						iType.IsGenericType &&
+						(iType.GetGenericTypeDefinition() == typeof(Havit.Model.Localizations.ILocalization<,>)));
 				if (localizationInterfaceType != null)
 				{
 					Type localizedType = localizationInterfaceType.GetGenericArguments().First(); // TLocalizedEntity

4c8efb501c8cca5f1a212f8a890828c208215d63
Kanda, Jiří <kanda@havit.cz>
2018-12-03 14:04:06 +0100

    Issue 41568: Doplnit FilteringCollection

---
+++ Havit.Model.Tests/Collections/Generic/FilteringCollectionTests.cs
@@ -0,0 +1,128 @@
+﻿using Havit.Model.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Havit.Model.Tests.Collections.Generic
+{
+	[TestClass]
+	public class FilteringCollectionTests
+	{
+		[TestMethod]
+		public void FilteringCollection_Add()
+		{
+			// Arrange
+			var list = new List<int>();
+			var filteringCollection = new FilteringCollection<int>(list, i => i % 2 == 0);
+
+			// Act
+			filteringCollection.Add(1);
+			filteringCollection.Add(2);
+
+			// Assert
+			Assert.IsTrue(list.Contains(1));
+			Assert.IsTrue(list.Contains(2));
+		}
+
+		[TestMethod]
+		public void FilteringCollection_AddRange()
+		{
+			// Arrange
+			var list = new List<int>();
+			var filteringCollection = new FilteringCollection<int>(list, i => i % 2 == 0);
+
+			// Act
+			filteringCollection.AddRange(new List<int> { 1, 2 });
+
+			// Assert
+			Assert.IsTrue(list.Contains(1));
+			Assert.IsTrue(list.Contains(2));
+		}
+
+		[TestMethod]
+		public void FilteringCollection_Clear()
+		{
+			// Arrange
+			var list = new List<int>() { 1, 2, 3, 4 };
+			var filteringCollection = new FilteringCollection<int>(list, i => i % 2 == 0);
+
+			// Act
+			filteringCollection.Clear();
+
+			// Assert
+			Assert.AreEqual(0, list.Count);
+		}
+		[TestMethod]
+		public void FilteringCollection_Contains()
+		{
+			// Arrange
+			var filteringCollection = new FilteringCollection<int>(new List<int> { 1, 2, 3, 4 }, i => i % 2 == 0);
+
+			// Act + Assert
+			Assert.IsFalse(filteringCollection.Contains(1));
+			Assert.IsTrue(filteringCollection.Contains(2));
+		}
+
+		[TestMethod]
+		public void FilteringCollection_Count()
+		{
+			// Arrange
+			var filteringCollection = new FilteringCollection<int>(new List<int> { 1, 2, 3, 4 }, i => i % 2 == 0);
+
+			// Act + Assert
+			Assert.AreEqual(2, filteringCollection.Count);
+		}
+
+
+		[TestMethod]
+		public void FilteringCollection_ForEach()
+		{
+			// Arrange
+			var filteringCollection = new FilteringCollection<int>(new List<int> { 1, 2, 3, 4 }, i => i % 2 == 0);
+
+			// Act
+			int sum = 0;
+			filteringCollection.ForEach(i => sum += i);
+
+			// Assert
+			Assert.AreEqual(6, sum);
+		}
+
+		[TestMethod]
+		public void FilteringCollection_Remove()
+		{
+			// Arrange
+			var list = new List<int>() { 1, 2, 3, 4 };
+			var filteringCollection = new FilteringCollection<int>(list, i => i % 2 == 0);
+
+			// Act
+			filteringCollection.Remove(1);
+			filteringCollection.Remove(2);
+
+			// Assert
+			Assert.IsFalse(list.Contains(1));
+			Assert.IsFalse(list.Contains(2));
+			Assert.IsTrue(list.Contains(3));
+			Assert.IsTrue(list.Contains(4));
+		}
+
+		[TestMethod]
+		public void FilteringCollection_RemoveAll()
+		{
+			// Arrange
+			var list = new List<int>() { 1, 2, 3, 4 };
+			var filteringCollection = new FilteringCollection<int>(list, i => i % 2 == 0);
+
+			// Act
+			filteringCollection.RemoveAll(i => i < 3);
+
+			// Assert
+			Assert.IsFalse(list.Contains(1));
+			Assert.IsFalse(list.Contains(2));
+			Assert.IsTrue(list.Contains(3));
+			Assert.IsTrue(list.Contains(4));
+		}
+
+	}
+}
---
+++ Havit.Model/Collections/Generic/FilteringCollection.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Havit.Model.Collections.Generic
+{
+	/// <summary>
+	/// Kolekce filtrující data z jiné kolekce (resp. Listu).
+	/// </summary>
+	public class FilteringCollection<T> : ICollection<T>
+	{
+		private readonly List<T> source;
+		private readonly Func<T, bool> filter;
+
+		/// <summary>
+		/// Konstruktor.
+		/// </summary>
+		/// <param name="source">Podkladová kolekce, ve které jsou držena data.</param>
+		/// <param name="filter">Filtr, kterým se podkladová kolekce filtruje.</param>
+		public FilteringCollection(List<T> source, Func<T, bool> filter)
+		{
+			this.source = source;
+			this.filter = filter;
+		}
+
+		/// <inheritdoc />
+		public IEnumerator<T> GetEnumerator()
+		{
+			return source.Where(filter).GetEnumerator();
+		}
+
+		/// <inheritdoc />
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		/// <summary>
+		/// Přidá objekt do podkladové kolekce (bez ohledu na filtr).
+		/// </summary>
+		public void Add(T item)
+		{
+			source.Add(item);
+		}
+
+		/// <summary>
+		/// Přidá objekty do podkladové kolekce (bez ohledu na filtr).
+		/// </summary>
+		public void AddRange(IEnumerable<T> collection)
+		{
+			source.AddRange(collection);
+		}
+
+		/// <summary>
+		/// Odstraní všechny objekty z podkladové kolekce (bez ohledu na filtr).
+		/// </summary>
+		public void Clear()
+		{
+			source.Clear();
+		}
+
+		/// <summary>
+		/// Vrací true, pokud podkladová kolekce po aplikování filtru obsahuje daný objekt.
+		/// </summary>
+		public bool Contains(T item)
+		{
+			return source.Where(filter).Contains(item);
+		}
+
+		/// <summary>
+		/// Kopíruje podkladovou kolekci po aplikování filtru do cílového pole.
+		/// </summary>
+		public void CopyTo(T[] array, int arrayIndex)
+		{
+			source.Where(filter).ToList().CopyTo(array, arrayIndex);
+		}
+
+		/// <summary>
+		/// Odebere objekt z podkladové kolekce (bez ohledu na filtr).
+		/// </summary>
+		public bool Remove(T item)
+		{
+			return source.Remove(item);
+		}
+
+		/// <summary>
+		/// Odebere objekty z podkladové kolekce (bez ohledu na filtr), tj. predicate se bude volat na všechny objekty podkladové kolekce, bez ohledu na filtr.
+		/// </summary>
+		public int RemoveAll(Predicate<T> predicate)
+		{
+			return source.RemoveAll(predicate);
+		}
+
+		/// <summary>
+		/// Spustí action nad každým objektem v podkladové kolekci po aplikování filtru.
+		/// </summary>
+		public void ForEach(Action<T> action)
+		{
+			source.Where(filter).ToList().ForEach(action);
+		}
+
+		/// <summary>
+		/// Vrací počet objektů z podkladové kolekci po aplikování filtru.
+		/// Nemá konstantní, ale lineární (vzhledem k velikosti kolekce) časovou složitost!
+		/// </summary>
+		public int Count => source.Where(filter).Count();
+
+		/// <inheritdoc />
+		public bool IsReadOnly => false;
+	}
+}

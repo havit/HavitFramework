d001e84cc9e024c75df61fea5ffc6c02b3459b33
Louma, Miroslav <louma@havit.cz>
2018-12-11 12:04:58 +0100

    EFCore.BL: fixed crash in tests for SP DbInjections:
    fixed incorrect implementation of base class DbInjectionAnnotationProvider<T>

--- Havit.Data.EntityFrameworkCore.BusinessLayer/DbInjections/IDbInjectionAnnotationProvider.cs
+++ Havit.Data.EntityFrameworkCore.BusinessLayer/DbInjections/IDbInjectionAnnotationProvider.cs
@@ -16,7 +16,7 @@ namespace Havit.Data.EntityFrameworkCore.BusinessLayer.DbInjections
         where T : IDbInjection
     {
         List<IAnnotation> IDbInjectionAnnotationProvider.GetAnnotations(IDbInjection dbAnnotation, MemberInfo memberInfo) => 
-            dbAnnotation is T dbAnnotationT ? GetAnnotations(dbAnnotationT, memberInfo) : null;
+            dbAnnotation is T dbAnnotationT ? GetAnnotations(dbAnnotationT, memberInfo) : new List<IAnnotation>();
 
         List<IDbInjection> IDbInjectionAnnotationProvider.GetDbInjections(List<IAnnotation> annotations) => 
             GetDbInjections(annotations).Cast<IDbInjection>().ToList();

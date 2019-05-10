d3b7933f551bcc4eea574c3a17d57a0f4b883c00
Kanda, Jiří <kanda@havit.cz>
2019-05-10 10:20:08 +0200

    Odstranění zbytečných AssemblyInfo.cs

--- Havit.Data.Entity6.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("HAVIT Entity Framework 6 Extensions - Tests")]
-[assembly: AssemblyDescription("HAVIT .NET Framework Extensions - Entity Framework 6 Extensions - Tests")]

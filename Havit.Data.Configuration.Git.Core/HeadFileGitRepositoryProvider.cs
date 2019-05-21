becc22b122a0a65344c7529bcff09d00ec6d9098
Louma, Miroslav <louma@havit.cz>
2019-05-21 13:06:48 +0200

    Havit.Data.Configuration.Git.Core: support transforming branches with slash characters (e.g. feature/<branch>).
    Added tests for HeadFileGitRepositoryProvider.
    
    #44361

--- Havit.Data.Configuration.Git.Core/HeadFileGitRepositoryProvider.cs
+++ Havit.Data.Configuration.Git.Core/HeadFileGitRepositoryProvider.cs
@@ -5,11 +5,12 @@ using System.Text.RegularExpressions;
 namespace Havit.Data.Configuration.Git.Core
 {
 	/// <summary>
-	/// Implementation of <see cref="IGitRepositoryProvider"/> that reads current branch from .git/HEAD.
+	/// Simple implementation of <see cref="IGitRepositoryProvider"/> that reads current branch from .git/HEAD.
+	/// <remarks>Useful for contexts, where using full-blown Git library like libgit2sharp is not appropriate.</remarks>
 	/// </summary>
 	public class HeadFileGitRepositoryProvider : IGitRepositoryProvider
 	{
-		private static readonly Regex HeadRefRegex = new Regex("^ref: refs/heads/(?<branch_name>[\\w\\d]+)$");
+		private static readonly Regex HeadRefRegex = new Regex("^ref: refs/heads/(?<branch_name>[^\\s]+)$");
 
 		/// <inheritdoc />
 		public string GetBranch(string path)
@@ -40,13 +41,18 @@ namespace Havit.Data.Configuration.Git.Core
 			return null;
 		}
 
-		private string ReadHeadRef(DirectoryInfo gitRepository)
+		private static string ReadHeadRef(DirectoryInfo gitRepository)
 		{
 			FileInfo headFile = gitRepository.GetDirectories(".git").Single().GetFiles("HEAD").Single();
 
 			string line = File.ReadAllLines(headFile.FullName)[0];
-			Match match = HeadRefRegex.Match(line);
-			return match.Success ? match.Groups["branch_name"].Value : null;
-		}
-	}
+			return ParseBranchName(line);
+        }
+
+        public static string ParseBranchName(string refFromHeadFile)
+        {
+            Match match = HeadRefRegex.Match(refFromHeadFile);
+            return match.Success ? match.Groups["branch_name"].Value : null;
+        }
+    }
 }
---
+++ Havit.Data.Configuration.Git.Tests/HeadFileGitRepositoryProviderTests.cs
@@ -0,0 +1,42 @@
+﻿using Havit.Data.Configuration.Git.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Havit.Data.Configuration.Git.Tests
+{
+    [TestClass]
+    public class HeadFileGitRepositoryProviderTests
+    {
+        [TestMethod]
+        public void HeadFileGitRepositoryProvider_ParseBranchName_MasterBranchRef_MasterIsReturned()
+        {
+            var headRef = "ref: refs/heads/master";
+
+            string branchName = HeadFileGitRepositoryProvider.ParseBranchName(headRef);
+
+            Assert.AreEqual("master", branchName);
+        }
+
+        /// <summary>
+        /// Currently parsing branch name from detached head is not supported, returns null.
+        /// </summary>
+        [TestMethod]
+        public void HeadFileGitRepositoryProvider_ParseBranchName_DetachedHead_Null()
+        {
+            var headRef = "2e479a1f84ad3d6cfd4f5f383a7bb60efada3ccb";
+
+            string branchName = HeadFileGitRepositoryProvider.ParseBranchName(headRef);
+
+            Assert.IsNull(branchName);
+        }
+
+        [TestMethod]
+        public void HeadFileGitRepositoryProvider_ParseBranchName_FeatureBranchRef_FeatureIsReturned()
+        {
+            var headRef = "ref: refs/heads/feature/my-super-cool-feature";
+
+            string branchName = HeadFileGitRepositoryProvider.ParseBranchName(headRef);
+
+            Assert.AreEqual("feature/my-super-cool-feature", branchName);
+        }
+    }
+}

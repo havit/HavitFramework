b708e39a964a6e21da48ecff9822b85349b8c4f1
Louma, Miroslav <louma@havit.cz>
2019-06-18 16:32:03 +0200

    EFCore.BL: added two more overloads for SqlResource extension method (for other ways to specify assembly with resources).
    #44439

--- Havit.Data.EntityFrameworkCore.BusinessLayer/MigrationExtensions.cs
+++ Havit.Data.EntityFrameworkCore.BusinessLayer/MigrationExtensions.cs
@@ -15,13 +15,14 @@ namespace Havit.Data.EntityFrameworkCore.BusinessLayer
         /// Vykoná SQL skript v rámci migrácie, ktorý je načítaný z resources.
         /// </summary>
         /// <param name="migrationBuilder"><see cref="MigrationBuilder"/> používaný v migrácii.</param>
-        /// <param name="resourceName">Názov resource vo volajúcej assembly.</param>
+        /// <param name="resourceName">Názov resource v assembly špecifikovanej v <paramref name="sqlResourceAssembly"/>.</param>
+        /// <param name="sqlResourceAssembly"><see cref="Assembly"/>, kde sa má resource hľadať.</param>
         /// <param name="suppressTransaction">Parameter pre <see cref="MigrationBuilder.Sql"/>: Indicates whether or not transactions will be suppressed while executing the SQL.</param>
-        public static void SqlResource(this MigrationBuilder migrationBuilder, string resourceName, bool suppressTransaction = false)
+        public static void SqlResource(this MigrationBuilder migrationBuilder, string resourceName, Assembly sqlResourceAssembly, bool suppressTransaction)
         {
+            Contract.Requires<ArgumentNullException>(migrationBuilder != null);
             Contract.Requires<ArgumentNullException>(resourceName != null);
-
-            Assembly sqlResourceAssembly = Assembly.GetCallingAssembly();
+            Contract.Requires<ArgumentNullException>(sqlResourceAssembly != null);
 
             using (var stream = sqlResourceAssembly.GetManifestResourceStream(resourceName))
             {
@@ -33,6 +34,37 @@ namespace Havit.Data.EntityFrameworkCore.BusinessLayer
                     migrationBuilder.Sql(sql, suppressTransaction);
                 }
             }
+        }
+
+        /// <summary>
+        /// Vykoná SQL skript v rámci migrácie, ktorý je načítaný z resources.
+        /// </summary>
+        /// <param name="migrationBuilder"><see cref="MigrationBuilder"/> používaný v migrácii.</param>
+        /// <param name="resourceName">Názov resource vo volajúcej assembly.</param>
+        /// <param name="suppressTransaction">Parameter pre <see cref="MigrationBuilder.Sql"/>: Indicates whether or not transactions will be suppressed while executing the SQL.</param>
+        public static void SqlResource(this MigrationBuilder migrationBuilder, string resourceName, bool suppressTransaction = false)
+        {
+            Contract.Requires<ArgumentNullException>(migrationBuilder != null);
+            Contract.Requires<ArgumentNullException>(resourceName != null);
+
+            Assembly sqlResourceAssembly = Assembly.GetCallingAssembly();
+
+            SqlResource(migrationBuilder, resourceName, sqlResourceAssembly, suppressTransaction);
+        }
+
+        /// <summary>
+        /// Vykoná SQL skript v rámci migrácie, ktorý je načítaný z resources.
+        /// </summary>
+        /// <param name="migration"><see cref="Migration"/> objekt. Využije sa iba na získanie reference na assembly s resource súbormi.</param>
+        /// <param name="migrationBuilder"><see cref="MigrationBuilder"/> používaný v migrácii.</param>
+        /// <param name="resourceName">Názov resource vo volajúcej assembly.</param>
+        /// <param name="suppressTransaction">Parameter pre <see cref="MigrationBuilder.Sql"/>: Indicates whether or not transactions will be suppressed while executing the SQL.</param>
+        public static void SqlResource(this Migration migration, MigrationBuilder migrationBuilder, string resourceName, bool suppressTransaction = false)
+        {
+            Contract.Requires<ArgumentNullException>(migration != null);
+
+            Assembly sqlResourceAssembly = migration.GetType().Assembly;
+            SqlResource(migrationBuilder, resourceName, sqlResourceAssembly, suppressTransaction);
         }
     }
 }

2354b85270f2999bdc53d154418c1d49ed4ab8fb
Kanda, Jiří <kanda@havit.cz>
2019-07-16 14:08:35 +0200

    Task 45197: Business Layer: Podpora DateTime2 pro soft-delete (v Query)
    Related Work Items: #45197

---
+++ BusinessLayerTest/SoftDeleteWithDateTimeOffsetCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.Caching;
+using System.Xml;
+using Havit.Business;
+using Havit.Business.Query;
+using Havit.Collections;
+using Havit.Data;
+using Havit.Data.SqlServer;
+using Havit.Data.SqlTypes;
+
+namespace Havit.BusinessLayerTest
+{
+	/// <summary>
+	/// Kolekce business objektů typu Havit.BusinessLayerTest.SoftDeleteWithDateTimeOffset.
+	/// </summary>
+	public partial class SoftDeleteWithDateTimeOffsetCollection : SoftDeleteWithDateTimeOffsetCollectionBase
+	{
+		
+	}
+}

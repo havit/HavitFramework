4c41f4787f00956724166edf688df08739ae9d71
Kanda, Jiří <kanda@havit.cz>
2019-12-19 09:01:04 +0100

    EF Core: AnnotationsWithDefaultsEntityCacheOptionsGenerator - parametrizace třídou, nikoliv parametry typu TimeSpan.

---
+++ Havit.Data.EntityFrameworkCore.Patterns/Caching/AnnotationsWithDefaultsEntityCacheOptionsGeneratorOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Havit.Data.EntityFrameworkCore.Patterns.Caching
+{
+	/// <summary>
+	/// Konfigurace pro AnnotationsWithDefaultsEntityCacheOptionsGenerator.
+	/// </summary>
+	public class AnnotationsWithDefaultsEntityCacheOptionsGeneratorOptions
+	{
+		/// <summary>
+		/// Čas absolutní expirace.
+		/// </summary>
+		public TimeSpan? AbsoluteExpiration { get; set; }
+
+		/// <summary>
+		/// Čas sliding expirace.
+		/// </summary>
+		public TimeSpan? SlidingExpiration { get; set; }
+	}
+}

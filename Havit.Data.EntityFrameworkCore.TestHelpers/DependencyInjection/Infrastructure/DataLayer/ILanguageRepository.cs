63f4a3c4fc45cfb4e72df26511cf2a073437631d
Kanda, Jiří <kanda@havit.cz>
2020-01-07 08:09:12 +0100

    EF Core: Extrakce modelu a dbcontextu pro test DI do samostatné class library

---
+++ Havit.Data.EntityFrameworkCore.TestHelpers/DependencyInjection/Infrastructure/DataLayer/ILanguageRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Havit.Data.EntityFrameworkCore.TestHelpers.DependencyInjection.Infrastructure.Model;
+using Havit.Data.Patterns.Repositories;
+
+namespace Havit.Data.EntityFrameworkCore.TestHelpers.DependencyInjection.Infrastructure.DataLayer
+{
+	public interface ILanguageRepository : IRepository<Language>
+	{
+	}
+}
---
+++ Havit.Data.EntityFrameworkCore.TestHelpers/DependencyInjection/Infrastructure/DataLayer/LanguageDataSource.cs
@@ -0,0 +1,13 @@
+﻿using Havit.Data.EntityFrameworkCore.Patterns.DataSources;
+using Havit.Data.EntityFrameworkCore.Patterns.SoftDeletes;
+using Havit.Data.EntityFrameworkCore.TestHelpers.DependencyInjection.Infrastructure.Model;
+
+namespace Havit.Data.EntityFrameworkCore.TestHelpers.DependencyInjection.Infrastructure.DataLayer
+{
+	public class LanguageDataSource : DbDataSource<Language>, ILanguageDataSource
+	{
+		public LanguageDataSource(IDbContext dbContext, ISoftDeleteManager softDeleteManager) : base(dbContext, softDeleteManager)
+		{
+		}
+	}
+}

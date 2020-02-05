5bd29282ed6ba4d463acc29a9c3fde6ddb465bb8
Louma, Miroslav <louma@havit.cz>
2020-02-05 16:02:56 +0100

    BLG: don't create separate SqlConnection when getting SqlDataReader from SqlCommand in ConnectionHelper
    #48181

--- Havit.Business.BusinessLayerGenerator/Helpers/ConnectionHelper.cs
+++ Havit.Business.BusinessLayerGenerator/Helpers/ConnectionHelper.cs
@@ -1,6 +1,4 @@
-﻿using Havit.Business.BusinessLayerGenerator.Settings;
-using System;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
 
 namespace Havit.Business.BusinessLayerGenerator.Helpers
@@ -9,23 +7,7 @@ namespace Havit.Business.BusinessLayerGenerator.Helpers
 	{
 		public static SqlDataReader GetDataReader(SqlCommand command)
 		{
-			SqlConnection connection;
-
-            if (String.IsNullOrEmpty(GeneratorSettings.Username))
-			{
-				connection = new SqlConnection(
-					String.Format("Data Source = {0}; Initial Catalog = {1}; Integrated Security=SSPI;",
-					    GeneratorSettings.SqlServerName, GeneratorSettings.DatabaseName));
-			}
-			else
-			{
-				connection = new SqlConnection(
-					String.Format("Data Source = {0}; Initial Catalog = {1}; User ID={2};Pwd={3}",
-					    GeneratorSettings.SqlServerName, GeneratorSettings.DatabaseName, GeneratorSettings.Username, GeneratorSettings.Password));
-			}
-			connection.Open();
-
-			command.Connection = connection;
+			command.Connection = DatabaseHelper.Database.Parent.ConnectionContext.SqlConnectionObject;
 			return command.ExecuteReader(CommandBehavior.CloseConnection);
 		}
 	}

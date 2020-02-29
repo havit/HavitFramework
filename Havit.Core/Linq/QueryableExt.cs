f5d8225d3e7a2eaa5a72d2a2aaffd3aaa8f32d51
Robert Haken <haken@havit.cz>
2020-02-29 23:56:47 +0100

    QueryableExt - OrderBy, ThenBy with SortDirection

--- Havit.Core/Linq/QueryableExt.cs
+++ Havit.Core/Linq/QueryableExt.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Havit.Collections;
 
 namespace Havit.Linq
 {
@@ -26,6 +27,48 @@ namespace Havit.Linq
 			return condition
 				? source.Where(predicate)
 				: source;
+		}
+
+		/// <summary>
+		/// Sorts the elements of a sequence in ascending/descending order according to a key and direction.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of source.</typeparam>
+		/// <typeparam name="TKey">The type of the key returned by the function that is represented by keySelector.</typeparam>
+		/// <param name="source">A sequence of values to order.</param>
+		/// <param name="keySelector">A function to extract a key from an element.</param>
+		/// <param name="direction">Direction of the order.</param>
+		/// <returns> An System.Linq.IOrderedQueryable`1 whose elements are sorted according to a key and direction.</returns>
+		public static IOrderedQueryable<TSource> OrderBy<TSource, TKey>(this IQueryable<TSource> source, Expression<Func<TSource, TKey>> keySelector, SortDirection direction)
+		{
+			if (direction == SortDirection.Descending)
+			{
+				return source.OrderByDescending(keySelector);
+			}
+			else
+			{
+				return source.OrderBy(keySelector);
+			}
+		}
+
+		/// <summary>
+		/// Performs a subsequent ordering of the elements in a sequence in ascending/descending order according to a key and direction.
+		/// </summary>
+		/// <typeparam name="TSource">An System.Linq.IOrderedQueryable`1 that contains elements to sort.</typeparam>
+		/// <typeparam name="TKey">The type of the key returned by the function that is represented by keySelector.</typeparam>
+		/// <param name="source">A sequence of values to order.</param>
+		/// <param name="keySelector">A function to extract a key from an element.</param>
+		/// <param name="direction">Direction of the order.</param>
+		/// <returns>An System.Linq.IOrderedQueryable`1 whose elements are sorted according to a key and direction.</returns>
+		public static IOrderedQueryable<TSource> ThenBy<TSource, TKey>(this IOrderedQueryable<TSource> source, Expression<Func<TSource, TKey>> keySelector, SortDirection direction)
+		{
+			if (direction == SortDirection.Descending)
+			{
+				return source.ThenByDescending(keySelector);
+			}
+			else
+			{
+				return source.ThenBy(keySelector);
+			}
 		}
 	}
 }

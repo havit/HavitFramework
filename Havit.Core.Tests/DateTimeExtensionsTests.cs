672f88582fe3eb06e7dfc8519e2f5c9725a3c6a7
Jiří Kanda <kanda@havit.cz>
2020-04-30 19:06:55 +0200

    DateTimeExtensions - GetRemainingToNext

---
+++ Havit.Core.Tests/DateTimeExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Tests
+{
+	[TestClass]
+	public class DateTimeExtensionsTests
+	{
+		[TestMethod]
+		public void DateTimeExtensions_GetRemainingToNext()
+		{
+			Assert.AreEqual(TimeSpan.FromSeconds(1), new DateTime(2020, 1, 1, 10, 14, 59).GetRemainingToNext(TimeSpan.FromMinutes(15)));
+			Assert.AreEqual(TimeSpan.FromMinutes(15), new DateTime(2020, 1, 1, 10, 15, 00).GetRemainingToNext(TimeSpan.FromMinutes(15)));
+			Assert.AreEqual(TimeSpan.FromMinutes(14), new DateTime(2020, 1, 1, 10, 16, 00).GetRemainingToNext(TimeSpan.FromMinutes(15)));
+		}
+	}
+}

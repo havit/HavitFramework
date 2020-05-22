b626e1bcd4c8dc25664c64bc6f66985e2e9431a1
Robert Haken <haken@havit.cz>
2020-05-22 16:35:00 +0200

    ValidatableObjectEntityValidator

---
+++ Havit.Data.EntityFrameworkCore.Patterns/UnitOfWorks/EntityValidation/ValidatableObjectEntityValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Data.EntityFrameworkCore.Patterns.UnitOfWorks.EntityValidation
+{
+	/// <summary>
+	/// Validates the entity using its IValidatableObject.Validate() method.
+	/// </summary>
+	public class ValidatableObjectEntityValidator : IEntityValidator<object>
+	{
+		/// <summary>
+		/// Validates the entity using its IValidatableObject.Validate() method.
+		/// </summary>
+		public IEnumerable<string> Validate(ChangeType changeType, object changingEntity)
+		{
+			if (changingEntity is IValidatableObject validatableObject)
+			{
+				return validatableObject
+					.Validate(new ValidationContext(validatableObject))
+					.Select(validationResult =>
+						validationResult.MemberNames.Any()
+						? validationResult.ErrorMessage + " [" + string.Join(", ", validationResult.MemberNames) + "]"
+						: validationResult.ErrorMessage);
+			}
+			return Enumerable.Empty<string>();
+		}
+	}
+}

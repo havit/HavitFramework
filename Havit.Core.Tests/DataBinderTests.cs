db53d53334f21859cfae9bdfce5c5e94668a6a2b
Robert Haken <haken@havit.cz>
2020-05-23 08:34:36 +0200

    DataBinderExt tests

---
+++ Havit.Core.Tests/DataBinderTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Tests
+{
+    [TestClass]
+	public class DataBinderTests
+    {
+		[TestMethod]
+		public void DataBinderExt_SetValue_PublicProperty()
+		{
+			// arrange
+			var obj = new MyClass();
+
+			// act
+			DataBinderExt.SetValue(obj, "MyPublicProperty", 1);
+
+			// assert
+			Assert.AreEqual(1, obj.MyPublicProperty);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void DataBinderExt_SetValue_ProtectedSetterProperty() 
+		{
+			// arrange
+			var obj = new MyClass();
+
+			// act
+			DataBinderExt.SetValue(obj, "MyPropertyWithProtectedSetter", 1);
+
+			// assert
+			Assert.AreEqual(1, obj.MyPropertyWithProtectedSetter);
+		}
+
+		private class MyClass
+		{
+			public int MyPublicProperty { get; set; }
+			public int MyPropertyWithProtectedSetter { get; protected set; }
+		}
+        
+    }
+}

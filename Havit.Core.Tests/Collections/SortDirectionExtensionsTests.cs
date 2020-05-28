5d2d813555e92392196dbcaeec061d55674a19e9
Jiří Kanda <kanda@havit.cz>
2020-05-28 08:18:01 +0200

    SortDirection.Reverse()

---
+++ Havit.Core.Tests/Collections/SortDirectionExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using Havit.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Tests.Collections
+{
+	[TestClass]
+	public class SortDirectionExtensionsTests
+	{
+		[TestMethod]
+		public void SortDirectionExtensions_Reverse()
+		{
+			// Arrange + Assert
+			Assert.AreEqual(SortDirection.Descending, SortDirection.Ascending.Reverse());
+			Assert.AreEqual(SortDirection.Ascending, SortDirection.Descending.Reverse());
+		}
+	}
+}
---
+++ Havit.Core/Collections/SortDirectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Collections
+{
+	/// <summary>
+	/// Extension metody k SortDirection.
+	/// </summary>
+	public static class SortDirectionExtensions
+	{
+		/// <summary>
+		/// Vrátí opačný směr řazení.
+		/// </summary>
+		public static SortDirection Reverse(this SortDirection source)
+		{
+			return source switch
+			{
+				SortDirection.Ascending => SortDirection.Descending,
+				SortDirection.Descending => SortDirection.Ascending,
+				_ => throw new InvalidOperationException(source.ToString())
+			};
+		}
+	}
+}

a705e06a542e57ffc26dafa36740baeedf236d80
Zbyšek Hlinka <hlinka@hlinka.cz>
2020-12-07 15:42:55 +0100

    51954 AutoCompleteTextBox vs "dlouhý" click/tap
    Related Work Items: #51954

--- Havit.Web/Content/js/HavitFrameworkClientScript.js
+++ Havit.Web/Content/js/HavitFrameworkClientScript.js
@@ -397,10 +397,11 @@ var havitAutoCompleteTextBoxExtensions = {
                 $clearTextLink.show();
             }
         });
-		// řeší problém dlouhého držení vybrané položky myší
-		// div class='.autocomplete-suggestion' se vytváří na formuláři mimo oblast span[data-autocompletetextbox], takže nelze tyto prvky propojit
-		// pokud někdo nepoužije tento styl chybně, pak by nemělo docházet k problémům
-		$(document).on('mousedown', '.autocomplete-suggestion', e => {
+        // řeší problém dlouhého držení vybrané položky myší
+        // div class='.autocomplete-suggestion' se vytváří na formuláři mimo oblast span[data-autocompletetextbox], takže nelze tyto prvky propojit
+        // pokud někdo nepoužije tento styl chybně, pak by nemělo docházet k problémům
+        $(document).off('mousedown', '.autocomplete-suggestion');
+        $(document).on('mousedown', '.autocomplete-suggestion', e => {
             $(e.target).click();
         });
     },

73943920cbb950c79afc172b301856c4e76dba90
Robert Haken <haken@havit.cz>
2021-02-01 17:13:27 +0100

    OperationFailedException (2.0.13)

---
+++ Havit.Core/OperationFailedException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit
+{
+	/// <summary>
+	/// Exception for recoverable failures which should be presented to the user.
+	/// </summary>
+	/// <remarks>
+	/// This exception is handled by our new UI stacks and is usualy displayed ase Messenger error.
+	/// </remarks>
+	public class OperationFailedException : Exception
+    {
+		/// <inheritdoc/>
+		public OperationFailedException(string message) : base(message)
+		{
+			// NOOP
+		}
+
+		/// <inheritdoc/>
+		public OperationFailedException(string message, Exception innerException) : base(message, innerException)
+		{
+			// NOOP
+		}
+	}
+}

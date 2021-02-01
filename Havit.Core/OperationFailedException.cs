c46ba80313a56384e2005318d4752cfddfc98356
Robert Haken <haken@havit.cz>
2021-02-01 17:22:43 +0100

    inheritdoc stylecop

--- Havit.Core/OperationFailedException.cs
+++ Havit.Core/OperationFailedException.cs
@@ -14,13 +14,20 @@ namespace Havit
 	/// </remarks>
 	public class OperationFailedException : Exception
     {
-		/// <inheritdoc/>
+		/// <summary>
+		/// Initializes a new instance of the OperationFailedException class with a specified error message.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
 		public OperationFailedException(string message) : base(message)
 		{
 			// NOOP
 		}
 
-		/// <inheritdoc/>
+		/// <summary>
+		/// Initializes a new instance of the OperationFailedException class with a specified error message and a reference to the inner exception that is the cause of this exception.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		/// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
 		public OperationFailedException(string message, Exception innerException) : base(message, innerException)
 		{
 			// NOOP

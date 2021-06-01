5ed8a8aa02bc67d74720be1b77943fd6e5597f8a
Robert Haken <haken@havit.cz>
2021-06-01 23:41:41 +0200

    ApiContractAttribute; #54407 Automatická registrace facades ve Web.Client (gRPC clients); #54408 Web.Server - Automatická registrace gRPC endpointů pro facades

---
+++ Havit.Core/ComponentModel/ApiContractAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Havit.ComponentModel
+{
+	/// <summary>
+	/// Marks interfaces which should be published/consumed as API.
+	/// Used for automatic registration of gRPC endpoints (Web.Server) and gRPC clients (Web.Client).
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Interface, Inherited = false)]
+	public class ApiContractAttribute : Attribute
+	{
+		/// <summary>
+		/// Indicates whether the API requires authorization (e.g. authorization token) or not (allows anonymous access). Default is <c>true</c>.
+		/// </summary>
+		public bool RequireAuthorization { get; set; } = true;
+	}
+}

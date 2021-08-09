207cb278d6c332fc056dc1b730db0d428bdc484b
Louma, Miroslav <louma@havit.cz>
2021-08-09 13:15:04 +0200

    EF Core CodeGenerator dotnet tool: use DependencyModel package to load Entity.dll dependencies properly
    Fixes: #56753

---
+++ Havit.Data.EntityFrameworkCore.CodeGenerator.Tool/AssemblyPathFixingCompilationAssemblyResolverFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.DependencyModel.Resolution;
+
+namespace Havit.Data.EntityFrameworkCore.CodeGenerator.Tool
+{
+	public class AssemblyPathFixingCompilationAssemblyResolverFactory
+	{
+		public static readonly ICompilationAssemblyResolver[] DefaultResolvers =
+		{
+			new ReferenceAssemblyPathResolver(),
+			new PackageCompilationAssemblyResolver()
+		};
+		
+		private readonly CompositeCompilationAssemblyResolver innerResolver;
+
+		public AssemblyPathFixingCompilationAssemblyResolverFactory(string appBasePath)
+		{
+			innerResolver = new CompositeCompilationAssemblyResolver(
+				new[] { new AppBaseCompilationAssemblyResolver(appBasePath) }
+					.Concat(DefaultResolvers).ToArray());
+		}
+
+		public AssemblyPathFixingCompilationAssemblyResolver Create(DependencyContext dependencyContext)
+			=> new AssemblyPathFixingCompilationAssemblyResolver(dependencyContext, innerResolver);
+	}
+}
--- Havit.Data.EntityFrameworkCore.CodeGenerator.Tool/Program.cs
+++ Havit.Data.EntityFrameworkCore.CodeGenerator.Tool/Program.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Loader;
+using Microsoft.Extensions.DependencyModel;
 
 namespace Havit.Data.EntityFrameworkCore.CodeGenerator.Tool
 {
@@ -13,7 +15,7 @@ namespace Havit.Data.EntityFrameworkCore.CodeGenerator.Tool
 			Console.WriteLine("----------------------------------------------");
 
 			DirectoryInfo solutionDirectory = new DirectoryInfo(Environment.CurrentDirectory);// @"D:\Dev\002.HFW-NewProjectTemplate";
-			while (System.IO.Directory.GetFiles(solutionDirectory.FullName, "*.sln", SearchOption.TopDirectoryOnly).Length == 0)
+			while (Directory.GetFiles(solutionDirectory.FullName, "*.sln", SearchOption.TopDirectoryOnly).Length == 0)
 			{
 				if (solutionDirectory.Root.FullName == solutionDirectory.FullName)
 				{
@@ -23,11 +25,19 @@ namespace Havit.Data.EntityFrameworkCore.CodeGenerator.Tool
 				solutionDirectory = solutionDirectory.Parent;
 			}
 
-			string[] files = System.IO.Directory
-				.GetFiles(Path.Combine(solutionDirectory.FullName, @"DataLayer\bin"), "*.Entity.dll", SearchOption.AllDirectories)
-				.Where(file => !file.EndsWith(@"Havit.Entity.dll"))
-				.Where(file => !file.Contains(@"ref\"))
-				.OrderByDescending(item => System.IO.File.GetLastAccessTime(item))
+			var dataLayerBinDirectory = new DirectoryInfo(Path.Combine(solutionDirectory.FullName, @"DataLayer\bin"));
+			if (!dataLayerBinDirectory.Exists)
+			{
+				Console.WriteLine($"Bin directory for project DataLayer not found: {dataLayerBinDirectory}");
+				Console.WriteLine("Make sure the project is properly built");
+				return;
+			}
+
+			FileInfo[] files = dataLayerBinDirectory
+				.GetFiles("*.Entity.dll", SearchOption.AllDirectories)
+				.Where(file => !file.Name.EndsWith("Havit.Entity.dll"))
+				.Where(file => !file.Name.Contains(@"ref\"))
+				.OrderByDescending(item => item.LastAccessTime)
 				.ToArray();
 
 			if (files.Length == 0)
@@ -36,12 +46,50 @@ namespace Havit.Data.EntityFrameworkCore.CodeGenerator.Tool
 				return;
 			}
 
-			string applicationEntityAssembly = files.First();
-			string workingFolder = System.IO.Path.GetDirectoryName(applicationEntityAssembly);
+			FileInfo applicationEntityAssembly = files.First();
+			DirectoryInfo workingFolder = applicationEntityAssembly.Directory;
 			Console.WriteLine($"Using folder {workingFolder}.");
 
-			AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly(workingFolder);
-			Assembly assembly = Assembly.Load(new AssemblyName { Name = "Havit.Data.EntityFrameworkCore.CodeGenerator" });
+			var dataLayerDepsFile = new FileInfo(applicationEntityAssembly.FullName.Replace(".Entity.dll", ".DataLayer.deps.json"));
+			if (!dataLayerDepsFile.Exists)
+			{
+				Console.WriteLine($"Deps.json file for *.DataLayer.dll not found: {dataLayerDepsFile.GetRelativePath(solutionDirectory)}");
+				Console.WriteLine("Make sure DataLayer project is properly built.");
+				return;
+			}
+
+			var dataLayerDependencyContext = new DependencyContextJsonReader().Read(dataLayerDepsFile.OpenRead());
+			Console.WriteLine($"Resolving assemblies using {dataLayerDepsFile.GetRelativePath(solutionDirectory)}");
+
+			DirectoryInfo[] objDirectory = dataLayerDepsFile.Directory.Parent.Parent.Parent.GetDirectories("obj");
+			var assetsJsonFile = objDirectory[0].GetFiles("project.assets.json")[0];
+
+			if (!assetsJsonFile.Exists)
+			{
+				Console.WriteLine($"project.assets.json for DataLayer not found: {dataLayerDepsFile.GetRelativePath(solutionDirectory)}");
+				Console.WriteLine("Make sure DataLayer project is properly built.");
+				return;
+			}
+
+			var dataLayerAssetsContext = new DependencyContextJsonReader().Read(assetsJsonFile.OpenRead());
+			Console.WriteLine($"Resolving assemblies using {assetsJsonFile.GetRelativePath(solutionDirectory)}");
+
+			// Default AssemblyLoadContext has to be used, because CodeGenerator itself uses it (and currently it's not passed over as parameter).
+			var assemblyLoadContext = AssemblyLoadContext.Default;
+			var assemblyLoader = new DependencyContextAssemblyLoader(dataLayerDependencyContext, dataLayerAssetsContext, workingFolder.FullName);
+			assemblyLoader.RegisterResolvingEvent(assemblyLoadContext);
+
+			Assembly assembly;
+			try
+			{
+				assembly = assemblyLoadContext.LoadFromAssemblyName(new AssemblyName { Name = "Havit.Data.EntityFrameworkCore.CodeGenerator" });
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Failed to load Havit.Data.EntityFrameworkCore.CodeGenerator");
+				Console.WriteLine(ex);
+				throw;
+			}
 
 			if (assembly == null)
 			{
@@ -63,33 +111,11 @@ namespace Havit.Data.EntityFrameworkCore.CodeGenerator.Tool
 				return;
 			}
 
-			Console.WriteLine("Starting codegenerator...");
-			main.Invoke(null, new object[] { new string[] { solutionDirectory.FullName, System.IO.Path.GetFileNameWithoutExtension(applicationEntityAssembly) } });
-		}
-
-		private static ResolveEventHandler ResolveAssembly(string appBasePath)
-		{
-			return (object sender, ResolveEventArgs args) =>
+			Console.WriteLine("Starting CodeGenerator...");
+			main.Invoke(null, new object[]
 			{
-				var assemblyName = new AssemblyName(args.Name);
-
-				foreach (var extension in new[] { ".dll", ".exe" })
-				{
-					var path = Path.Combine(appBasePath, assemblyName.Name + extension);
-					if (File.Exists(path))
-					{
-						try
-						{
-							return Assembly.LoadFrom(path);
-						}
-						catch
-						{
-							// NOOP
-						}
-					}
-				}
-				return null;
-			};
+				new string[] { solutionDirectory.FullName, Path.GetFileNameWithoutExtension(applicationEntityAssembly.FullName) }
+			});
 		}
 	}
 }

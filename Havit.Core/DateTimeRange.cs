4b1935e01c257522e1909e37c832ac2200b1bae5
Robert Haken <haken@havit.cz>
2022-01-29 01:56:07 +0100

    DateTimeRange (release Havit.Core 2.0.22)

---
+++ Havit.Core/DateTimeRange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Havit;
+
+/// <summary>
+/// Indicates range of date - start date and end date.
+/// </summary>
+public record struct DateTimeRange
+{
+	/// <summary>
+	/// Start date of the range.
+	/// </summary>
+	public DateTime? StartDate { get; private set; }
+
+	/// <summary>
+	/// End date of the range.
+	/// </summary>
+	public DateTime? EndDate { get; private set; }
+
+	/// <summary>
+	/// Creates new instance of the struct.
+	/// </summary>
+	public DateTimeRange(DateTime? startDate, DateTime? endDate)
+	{
+		StartDate = startDate;
+		EndDate = endDate;
+	}
+}

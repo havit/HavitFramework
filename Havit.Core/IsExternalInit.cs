9468f2aa08d6b4baad89f74be878c400450f24dd
Robert Haken <haken@havit.cz>
2022-01-29 17:23:28 +0100

    comment

--- Havit.Core/IsExternalInit.cs
+++ Havit.Core/IsExternalInit.cs
@@ -3,6 +3,7 @@
 namespace System.Runtime.CompilerServices;
 
 /// <summary>
+/// Allows using init properties in netstandard2.0.
 /// Reserved to be used by the compiler for tracking metadata.
 /// This class should not be used by developers in source code.
 /// </summary>

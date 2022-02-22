9099d7775a4c14d242c874be620ab167abb872d7
Kanda, Jiří <kanda@havit.cz>
2022-02-22 16:45:51 +0100

    Havit.Data.Patterns: (Dočasné) navrácení Trsancations.Internal (pro možnost aktualizace závislostí v EFCore5 projektech)

---
+++ Havit.Data.Patterns/Transactions/Internal/ITransactionWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.Data.Patterns.Transactions.Internal
+{
+	/// <summary>
+	/// Zajišťuje spuštění předaného kódu v transakci.
+	/// Viz implementace.
+	/// </summary>
+	// TODO EF Core 6: Přesunout pryč z Havit.Data.Patterns (což možná zjednoduší závislosti)
+	public interface ITransactionWrapper
+	{
+		/// <summary>
+		/// Zadaná akce je spuštěna s transakcí.
+		/// </summary>
+		void ExecuteWithTransaction(Action action);
+	}
+}

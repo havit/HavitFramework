853eec23d30f93463e5e700f0b050398863a3286
Robert Haken <haken@havit.cz>
2022-11-20 14:41:17 +0100

    Havit.Web - DynarchCalendar - doplnění ABOUT lokalizace do turečtiny TR

--- Havit.Web/UI/WebControls/DynarchCalendar/calendar-tr.js
+++ Havit.Web/UI/WebControls/DynarchCalendar/calendar-tr.js
@@ -20,6 +20,21 @@ Calendar._SMN = new Array("Oca", "Şub", "Mar", "Nis", "May", "Haz", "Tem", "Ağ
 // tooltips
 Calendar._TT = {};
 Calendar._TT["INFO"] = "About the calendar";
+Calendar._TT["ABOUT"] =
+	"DHTML Date/Time Selector\n" +
+	"(c) dynarch.com 2002-2005 / Author: Mihai Bazon\n" + // don't translate this this ;-)
+	"For latest version visit: http://www.dynarch.com/projects/calendar/\n" +
+	"Distributed under GNU LGPL.  See http://gnu.org/licenses/lgpl.html for details." +
+	"\n\n" +
+	"Date selection:\n" +
+	"- Use the \xab, \xbb buttons to select year\n" +
+	"- Use the " + String.fromCharCode(0x2039) + ", " + String.fromCharCode(0x203a) + " buttons to select month\n" +
+	"- Hold mouse button on any of the above buttons for faster selection.";
+Calendar._TT["ABOUT_TIME"] = "\n\n" +
+	"Time selection:\n" +
+	"- Click on any of the time parts to increase it\n" +
+	"- or Shift-click to decrease it\n" +
+	"- or click and drag for faster selection.";
 Calendar._TT["TOGGLE"] = "Haftanýn ilk gününü kaydýr";
 Calendar._TT["PREV_YEAR"] = "Önceki Yýl (Menü için basýlý tutunuz)";
 Calendar._TT["PREV_MONTH"] = "Önceki Ay (Menü için basýlý tutunuz)";

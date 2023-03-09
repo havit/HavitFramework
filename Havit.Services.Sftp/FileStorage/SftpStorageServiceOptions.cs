2ef2d61e916a84ed2e3ad2d5b5b1cce2a1d01c25
Jiří Kanda <kanda@havit.cz>
2023-03-09 10:53:20 +0100

    Kosmetika

--- Havit.Services.Sftp/FileStorage/SftpStorageServiceOptions.cs
+++ Havit.Services.Sftp/FileStorage/SftpStorageServiceOptions.cs
@@ -15,6 +15,14 @@ namespace Havit.Services.Sftp.FileStorage
 		/// <remarks>
 		/// Díky typu ConnectionInfo publikujeme závislost na použité knihovně, vzhledem ke scope knihovny a vzhledem k možnostem,
 		/// které to přináší pro možnost konfigurace připojení, toto necháváme jako OK.
+		///
+		/// Příklad použití:
+		/// using var service = new SftpStorageService(new SftpStorageServiceOptions { ConnectionInfoFunc = () => new ConnectionInfo("sfteu.xerox.com", "ESPChester", new Renci.SshNet.PrivateKeyAuthenticationMethod("ESPChester", new PrivateKeyFile("D:\\Temp\\ESPChester.new"))) });
+		/// 
+		/// jak získat tento soubor (ESPChester.new)?
+		/// a) stáhnout puttygen, spustit
+		/// b) načíst *.ppk přes menu Conversions, Import Key
+		/// c) exportovat přes Conversion, Export OpenSSH key (nikoliv new format, nikoliv ssh.com!)
 		/// </remarks>
 		public Func<ConnectionInfo> ConnectionInfoFunc { get; set; }
 	}

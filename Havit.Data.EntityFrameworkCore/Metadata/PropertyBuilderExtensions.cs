377295c9e685ddd78cb4f30b09562e55d04b1c3d
Jiří Kanda <kanda@havit.cz>
2023-03-17 14:10:45 +0100

    Kosmetika

--- Havit.Data.EntityFrameworkCore/Metadata/PropertyBuilderExtensions.cs
+++ Havit.Data.EntityFrameworkCore/Metadata/PropertyBuilderExtensions.cs
@@ -2,8 +2,14 @@
 
 namespace Havit.Data.EntityFrameworkCore.Metadata;
 
+/// <summary>
+/// Extension metody k PropertyBuilder&lt;TProperty&gt;.
+/// </summary>
 public static class PropertyBuilderExtensions
 {
+	/// <summary>
+	/// Doplní k property anotaci povolující pojmenování vlastnosti s 'Id' na konci, přestože nejde o cizí klíč.
+	/// </summary>
 	public static PropertyBuilder<TProperty> AllowNonForeignKeyToEndWithId<TProperty>(this PropertyBuilder<TProperty> propertyBuilder)
 	{
 		return propertyBuilder.HasAnnotation(ModelValidationAnnotationContants.AllowNonForeignKeyToEndWithIdAnnotationName, true);

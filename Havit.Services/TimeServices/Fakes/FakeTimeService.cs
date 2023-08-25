d60db4029eb1fb255b789073328a72bd71c85234
Jiří Kanda <kanda@havit.cz>
2023-08-25 10:08:52 +0200

    FakeTimeService

---
+++ Havit.Services/TimeServices/Fakes/FakeTimeService.cs
@@ -0,0 +1,26 @@
+﻿namespace Havit.Services.TimeServices.Fakes;
+
+public class FakeTimeService : ITimeService
+{
+	private readonly Func<DateTime> nowFunc;
+
+	public FakeTimeService(DateTime now) : this(() => now)
+	{
+		// NOOP
+	}
+
+	public FakeTimeService(Func<DateTime> nowFunc)
+	{
+		this.nowFunc = nowFunc;
+	}
+
+	public DateTime GetCurrentDate()
+	{
+		return GetCurrentTime().Date;
+	}
+
+	public DateTime GetCurrentTime()
+	{
+		return nowFunc();
+	}
+}

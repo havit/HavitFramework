29e4b4d501a4b42c3170879c14438a946b2e578c
Robert Haken <haken@havit.cz>
2023-09-23 21:37:22 +0200

    HttpRequestExceptionFormatter - Activity

--- Havit.AspNetCore/ExceptionMonitoring/Formatters/HttpRequestExceptionFormatter.cs
+++ Havit.AspNetCore/ExceptionMonitoring/Formatters/HttpRequestExceptionFormatter.cs
@@ -10,54 +10,71 @@ namespace Havit.AspNetCore.ExceptionMonitoring.Formatters;
 /// <summary>
 /// Formatter výjimky s popisem prostředí http serveru.
 /// </summary>
-    public class HttpRequestExceptionFormatter : IExceptionFormatter
-    {
-        private readonly IHttpContextAccessor httpContextAccessor;
+public class HttpRequestExceptionFormatter : IExceptionFormatter
+{
+	private readonly IHttpContextAccessor httpContextAccessor;
 
 	/// <summary>
 	/// Konstruktor.
 	/// </summary>
-        public HttpRequestExceptionFormatter(IHttpContextAccessor httpContextAccessor = null)
-        {
-            this.httpContextAccessor = httpContextAccessor;
-        }
+	public HttpRequestExceptionFormatter(IHttpContextAccessor httpContextAccessor = null)
+	{
+		this.httpContextAccessor = httpContextAccessor;
+	}
 
 	/// <summary>
 	/// Vrátí text reprezentující lidsky čitelné informace o výjimce (a http requestu a http contextu).
 	/// </summary>
-        public string FormatException(Exception exception)
-        {
-            StringBuilder sb = new StringBuilder();
+	public string FormatException(Exception exception)
+	{
+		StringBuilder sb = new StringBuilder();
 
-            AppendExceptionInformation(sb, exception);
+		AppendExceptionInformation(sb, exception);
 
-            if (httpContextAccessor != null)
-            {
-                HttpContext httpContext = httpContextAccessor.HttpContext;
-                AppendRequestInformation(sb, httpContext);
-            }
+		if (httpContextAccessor != null)
+		{
+			HttpContext httpContext = httpContextAccessor.HttpContext;
+			AppendRequestInformation(sb, httpContext);
+		}
 
-            AppendExceptionDetails(sb, exception);
-            AppendExceptionDemistified(sb, exception);
-            AppendApplicationInformation(sb);
-            AppendEventInformation(sb);
+		AppendExceptionDetails(sb, exception);
+		AppendExceptionDemistified(sb, exception);
+		AppendApplicationInformation(sb);
+		AppendActivityInformation(sb);
+		AppendEventInformation(sb);
 
-            return sb.ToString();
-        }
+		return sb.ToString();
+	}
 
-        private void AppendExceptionInformation(StringBuilder sb, Exception exception)
-        {
-            sb.AppendLine("Exception information");
-            AppendValueLine(sb, "Exception type", () => exception.GetType().ToString());
-            AppendValueLine(sb, "Exception message", () => exception.Message);
-            sb.AppendLine();
-        }
+	private void AppendActivityInformation(StringBuilder sb)
+	{
+		// ApplicationInsights
+#if NET6_0_OR_GREATER
+		if (Activity.Current is not null)
+		{
+			sb.AppendLine("Activity information:");
+			sb.AppendLine("    ID: " + Activity.Current.Id);
+			sb.AppendLine("    Root ID: " + Activity.Current.RootId + " (ApplicationInsights Operation Id)");
+			sb.AppendLine("    Parent ID: " + Activity.Current.ParentId);
+			sb.AppendLine("    Operation Name: " + Activity.Current.OperationName);
+			sb.AppendLine("    Kind: " + Activity.Current.Kind);
+		}
+#endif
+	}
 
-        private void AppendRequestInformation(StringBuilder sb, HttpContext context)
-        {
+	private void AppendExceptionInformation(StringBuilder sb, Exception exception)
+	{
+		sb.AppendLine("Exception information");
+		AppendValueLine(sb, "Exception type", () => exception.GetType().ToString());
+		AppendValueLine(sb, "Exception message", () => exception.Message);
+		sb.AppendLine();
+	}
+
+	private void AppendRequestInformation(StringBuilder sb, HttpContext context)
+	{
 		sb.AppendLine("Request information");
 		if (context == null)
-		{				
+		{
 			sb.AppendLine("No request information available.");
 		}
 		else
@@ -71,82 +88,83 @@ namespace Havit.AspNetCore.ExceptionMonitoring.Formatters;
 			AppendValueLine(sb, "Client URI", () => context.Request.Headers["hx-client-uri"], skipIfNullOrEmpty: true);
 			AppendValueLine(sb, "Referer", () => context.Request.Headers["Referer"]);
 			AppendValueLine(sb, "User agent", () => context.Request.Headers["User-Agent"]);
+			AppendValueLine(sb, "TraceIdentifier", () => context.TraceIdentifier, skipIfNullOrEmpty: true);
 		}
-            //AppendValueLine(sb, "Culture", () => context.Features.Get<IRequestCultureFeature>().RequestCulture.Culture.Name);
-            //AppendValueLine(sb, "UI Culture", () => context.Features.Get<IRequestCultureFeature>().RequestCulture.UICulture.Name);
-            sb.AppendLine();
-        }
+		//AppendValueLine(sb, "Culture", () => context.Features.Get<IRequestCultureFeature>().RequestCulture.Culture.Name);
+		//AppendValueLine(sb, "UI Culture", () => context.Features.Get<IRequestCultureFeature>().RequestCulture.UICulture.Name);
+		sb.AppendLine();
+	}
 
-        private void AppendExceptionDetails(StringBuilder sb, Exception exception)
-        {
-            if (exception != null)
-            {
-                sb.AppendLine("Exception details");
-                sb.AppendLine(exception.ToString());
-                sb.AppendLine();
-            }
-        }
+	private void AppendExceptionDetails(StringBuilder sb, Exception exception)
+	{
+		if (exception != null)
+		{
+			sb.AppendLine("Exception details");
+			sb.AppendLine(exception.ToString());
+			sb.AppendLine();
+		}
+	}
 
-        private void AppendExceptionDemistified(StringBuilder sb, Exception exception)
-        {
-            if (exception != null)
-            {
-                string demistifiedException;
+	private void AppendExceptionDemistified(StringBuilder sb, Exception exception)
+	{
+		if (exception != null)
+		{
+			string demistifiedException;
 
-                try
-                {
-                    demistifiedException = exception.Demystify().ToString();
-                }
-                catch
-                {
-                    return;
-                }
+			try
+			{
+				demistifiedException = exception.Demystify().ToString();
+			}
+			catch
+			{
+				return;
+			}
 
-                sb.AppendLine("Exception details (with Ben.Demystifier)");
-                sb.AppendLine(demistifiedException);
-                sb.AppendLine();
-            }
-        }
+			sb.AppendLine("Exception details (with Ben.Demystifier)");
+			sb.AppendLine(demistifiedException);
+			sb.AppendLine();
+		}
+	}
 
-        private void AppendApplicationInformation(StringBuilder sb)
-        {
-            Assembly assembly = Assembly.GetEntryAssembly();
+	private void AppendApplicationInformation(StringBuilder sb)
+	{
+		Assembly assembly = Assembly.GetEntryAssembly();
 
-            if (assembly != null)
-            {
-                sb.AppendLine("Application information");
-                AppendValueLine(sb, "Assembly", () => assembly.GetName().Name);
-                AppendValueLine(sb, "Assembly Version", () => assembly.GetName().Version.ToString());
-                AppendValueLine(sb, "Product Version", () => System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion);
-                sb.AppendLine();
-            }
-        }
+		if (assembly != null)
+		{
+			sb.AppendLine("Application information");
+			AppendValueLine(sb, "Assembly", () => assembly.GetName().Name);
+			AppendValueLine(sb, "Assembly Version", () => assembly.GetName().Version.ToString());
+			AppendValueLine(sb, "Product Version", () => System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion);
+			sb.AppendLine();
+		}
+	}
 
-        private void AppendEventInformation(StringBuilder sb)
-        {
-            DateTimeOffset now = DateTimeOffset.Now;
+	private void AppendEventInformation(StringBuilder sb)
+	{
+		DateTimeOffset now = DateTimeOffset.Now;
 
-            sb.AppendLine("Event information");
-            AppendValueLine(sb, "Event time", () => now.ToString(CultureInfo.InstalledUICulture));
-            AppendValueLine(sb, "Event UTC time", () => now.ToUniversalTime().ToString(CultureInfo.InstalledUICulture));
-            sb.AppendLine();
-        }
+		sb.AppendLine("Event information");
+		AppendValueLine(sb, "Event time", () => now.ToString(CultureInfo.InstalledUICulture));
+		AppendValueLine(sb, "Event UTC time", () => now.ToUniversalTime().ToString(CultureInfo.InstalledUICulture));
+		sb.AppendLine();
+	}
 
-        private void AppendValueLine(StringBuilder sb, string key, Func<string> getValueFunc, bool skipIfNullOrEmpty = false)
-        {
-            string value;
-            try
-            {
-                value = getValueFunc.Invoke();
-            }
-            catch
-            {
-                value = "(exception)";
-            }
+	private void AppendValueLine(StringBuilder sb, string key, Func<string> getValueFunc, bool skipIfNullOrEmpty = false)
+	{
+		string value;
+		try
+		{
+			value = getValueFunc.Invoke();
+		}
+		catch
+		{
+			value = "(exception)";
+		}
 		if (skipIfNullOrEmpty && String.IsNullOrEmpty(value))
 		{
 			return;
 		}
-            sb.AppendLine("    " + key + ": " + value);
-        }
-    }
+		sb.AppendLine("    " + key + ": " + value);
+	}
+}

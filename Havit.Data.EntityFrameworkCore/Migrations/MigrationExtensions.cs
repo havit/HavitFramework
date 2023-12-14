f1fd2ad68ba2a18dafb9a80168c88c46269bc28e
Jiří Kanda <kanda@havit.cz>
2023-12-14 16:47:26 +0100

    EFCore: MigrationExtensions.SqlResource

---
+++ Havit.Data.EntityFrameworkCore/Migrations/MigrationExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Havit.Diagnostics.Contracts;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Havit.Data.EntityFrameworkCore.Migrations;
+
+/// <summary>
+/// Extension metody pre použití v migracích.
+/// </summary>
+public static class MigrationExtensions
+{
+	/// <summary>
+	/// Spustí v rámci migrace SQL skript z Embedded Resources.
+	/// </summary>
+	/// <param name="migrationBuilder"><see cref="MigrationBuilder"/></param>
+	/// <param name="resourceName">Název resource, který je vyhledán v resources dodané assembly a následně předán ke spuštění v migraci.</param>
+	/// <param name="sqlResourceAssembly">Assembly, ve které je hledán resource.</param>
+	public static void SqlResource(this MigrationBuilder migrationBuilder, string resourceName, Assembly sqlResourceAssembly)
+	{
+		Contract.Requires<ArgumentNullException>(resourceName != null);
+
+		using var stream = sqlResourceAssembly.GetManifestResourceStream(resourceName);
+		Contract.Assert<ArgumentException>(stream != null, $"Resource name '{resourceName}' does not exist");
+
+		string sql;
+		using (var textStream = new StreamReader(stream))
+		{
+			sql = textStream.ReadToEnd();
+		}
+		migrationBuilder.Sql(sql);
+	}
+}

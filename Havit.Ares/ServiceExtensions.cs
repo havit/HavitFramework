a46703b83abe58da796479fbd65a927d471beca5
Jiří Kanda <kanda@havit.cz>
2024-03-25 12:23:27 +0100

    Ares - AddAresServices

---
+++ Havit.Ares/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Havit.Ares.FinancniSprava;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Havit.Ares;
+
+/// <summary>
+/// Extensions k <see cref="IServiceCollection"/>.
+/// </summary>
+public static class ServiceExtensions
+{
+	/// <summary>
+	/// Zaregistruje služby <see cref="IAresService"/>, <see cref="IPlatceDphService"/> a <see cref="IAresDphService"/> do DI containeru.
+	/// </summary>
+	public static IServiceCollection AddAresServices(this IServiceCollection services)
+	{
+		services.AddTransient<IAresService, AresService>();
+		services.AddTransient<IPlatceDphService, PlatceDphService>();
+		services.AddTransient<IAresDphService, AresDphService>();
+
+		return services;
+	}
+}

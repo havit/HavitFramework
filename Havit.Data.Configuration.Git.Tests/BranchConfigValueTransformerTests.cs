28ae868be804a1267e51b776fb6137ec2065b29f
Gregor, Jiří <gregor@havit.cz>
2024-05-13 11:42:01 +0200

    #74913 Konfigurace aplikací - možnost nahrazovat název branche nejen + značky pro řešení situace s podtržítkem, lomítkem v názvu branche, atp. - EMPTYMASTER logika

--- Havit.Data.Configuration.Git.Tests/BranchConfigValueTransformerTests.cs
+++ Havit.Data.Configuration.Git.Tests/BranchConfigValueTransformerTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Havit.Data.Configuration.Git.Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
 
 namespace Havit.Data.Configuration.Git.Tests;
 
@@ -17,11 +16,28 @@ public class BranchConfigValueTransformerTests
 		// Act + Assert
 		Assert.AreEqual("master", transformer.TransformConfigValue("#BRANCH_NAME#", "master"));
 		Assert.AreEqual("_master", transformer.TransformConfigValue("#_BRANCH_NAME#", "master"));
-		Assert.AreEqual("-master", transformer.TransformConfigValue("#-BRANCH_NAME#", "master"));
+		Assert.AreEqual("-master", transformer.TransformConfigValue("#-BRANCH-NAME#", "master"));
 		Assert.AreEqual("master", transformer.TransformConfigValue("{BRANCH_NAME}", "master"));
 		Assert.AreEqual("_master", transformer.TransformConfigValue("{_BRANCH_NAME}", "master"));
-		Assert.AreEqual("-master", transformer.TransformConfigValue("{-BRANCH_NAME}", "master"));
+		Assert.AreEqual("-master", transformer.TransformConfigValue("{-BRANCH-NAME}", "master"));
+	}
 
+	[TestMethod]
+	public void BranchConnectionStringConfigurationTransformer_TransformConnectionString_MasterBranch_EmptyMaster()
+	{
+		// Arrange		
+		var transformer = new BranchConfigValueTransformer();
+
+		// Act + Assert
+		Assert.AreEqual("", transformer.TransformConfigValue("#BRANCH_NAME_EMPTYMASTER#", "master"));
+		Assert.AreEqual("", transformer.TransformConfigValue("#BRANCH_NAME_EMPTYMASTER#", "Master"));
+		Assert.AreEqual("", transformer.TransformConfigValue("#BRANCH_NAME_EMPTYMASTER#", "MASTER"));
+
+		Assert.AreEqual("", transformer.TransformConfigValue("#_BRANCH_NAME_EMPTYMASTER#", "master"));
+		Assert.AreEqual("", transformer.TransformConfigValue("#-BRANCH-NAME-EMPTYMASTER#", "master"));
+		Assert.AreEqual("", transformer.TransformConfigValue("{BRANCH_NAME_EMPTYMASTER}", "master"));
+		Assert.AreEqual("", transformer.TransformConfigValue("{_BRANCH_NAME_EMPTYMASTER}", "master"));
+		Assert.AreEqual("", transformer.TransformConfigValue("{-BRANCH-NAME-EMPTYMASTER}", "master"));
 	}
 
 	[TestMethod]
@@ -33,11 +49,27 @@ public class BranchConfigValueTransformerTests
 		// Act + Assert
 		Assert.AreEqual("feature_test", transformer.TransformConfigValue("#BRANCH_NAME#", "feature/test"));
 		Assert.AreEqual("_feature_test", transformer.TransformConfigValue("#_BRANCH_NAME#", "feature/test"));
-		Assert.AreEqual("-feature_test", transformer.TransformConfigValue("#-BRANCH_NAME#", "feature/test"));
+		Assert.AreEqual("-feature-test", transformer.TransformConfigValue("#-BRANCH-NAME#", "feature/test"));
 
 		Assert.AreEqual("feature_test", transformer.TransformConfigValue("{BRANCH_NAME}", "feature/test"));
 		Assert.AreEqual("_feature_test", transformer.TransformConfigValue("{_BRANCH_NAME}", "feature/test"));
-		Assert.AreEqual("-feature_test", transformer.TransformConfigValue("{-BRANCH_NAME}", "feature/test"));
+		Assert.AreEqual("-feature-test", transformer.TransformConfigValue("{-BRANCH-NAME}", "feature/test"));
+	}
+
+	[TestMethod]
+	public void BranchConnectionStringConfigurationTransformer_TransformConnectionString_FeatureBranch_EmptyMaster()
+	{
+		// Arrange		
+		var transformer = new BranchConfigValueTransformer();
+
+		// Act + Assert
+		Assert.AreEqual("feature_test", transformer.TransformConfigValue("#BRANCH_NAME_EMPTYMASTER#", "feature/test"));
+		Assert.AreEqual("_feature_test", transformer.TransformConfigValue("#_BRANCH_NAME_EMPTYMASTER#", "feature/test"));
+		Assert.AreEqual("-feature-test", transformer.TransformConfigValue("#-BRANCH-NAME-EMPTYMASTER#", "feature/test"));
+
+		Assert.AreEqual("feature_test", transformer.TransformConfigValue("{BRANCH_NAME_EMPTYMASTER}", "feature/test"));
+		Assert.AreEqual("_feature_test", transformer.TransformConfigValue("{_BRANCH_NAME_EMPTYMASTER}", "feature/test"));
+		Assert.AreEqual("-feature-test", transformer.TransformConfigValue("{-BRANCH-NAME-EMPTYMASTER}", "feature/test"));
 	}
 
 	[TestMethod]
@@ -49,20 +81,20 @@ public class BranchConfigValueTransformerTests
 		// Act + Assert
 		Assert.AreEqual("#BRANCH_NAME#", transformer.TransformConfigValue("#BRANCH_NAME#", null));
 		Assert.AreEqual("#_BRANCH_NAME#", transformer.TransformConfigValue("#_BRANCH_NAME#", null));
-		Assert.AreEqual("#-BRANCH_NAME#", transformer.TransformConfigValue("#-BRANCH_NAME#", null));
+		Assert.AreEqual("#-BRANCH-NAME#", transformer.TransformConfigValue("#-BRANCH-NAME#", null));
 
 		Assert.AreEqual("{BRANCH_NAME}", transformer.TransformConfigValue("{BRANCH_NAME}", null));
 		Assert.AreEqual("{_BRANCH_NAME}", transformer.TransformConfigValue("{_BRANCH_NAME}", null));
-		Assert.AreEqual("{-BRANCH_NAME}", transformer.TransformConfigValue("{-BRANCH_NAME}", null));
+		Assert.AreEqual("{-BRANCH-NAME}", transformer.TransformConfigValue("{-BRANCH-NAME}", null));
 
 
 		Assert.AreEqual("#BRANCH_NAME#", transformer.TransformConfigValue("#BRANCH_NAME#", String.Empty));
 		Assert.AreEqual("#_BRANCH_NAME#", transformer.TransformConfigValue("#_BRANCH_NAME#", String.Empty));
-		Assert.AreEqual("#-BRANCH_NAME#", transformer.TransformConfigValue("#-BRANCH_NAME#", String.Empty));
+		Assert.AreEqual("#-BRANCH-NAME#", transformer.TransformConfigValue("#-BRANCH-NAME#", String.Empty));
 
 		Assert.AreEqual("{BRANCH_NAME}", transformer.TransformConfigValue("{BRANCH_NAME}", String.Empty));
 		Assert.AreEqual("{_BRANCH_NAME}", transformer.TransformConfigValue("{_BRANCH_NAME}", String.Empty));
-		Assert.AreEqual("{-BRANCH_NAME}", transformer.TransformConfigValue("{-BRANCH_NAME}", String.Empty));
+		Assert.AreEqual("{-BRANCH-NAME}", transformer.TransformConfigValue("{-BRANCH-NAME}", String.Empty));
 
 
 	}
@@ -74,6 +106,6 @@ public class BranchConfigValueTransformerTests
 		var transformer = new BranchConfigValueTransformer();
 
 		// Act + Assert
-		Assert.AreEqual("master _master -master master _master -master", transformer.TransformConfigValue("#BRANCH_NAME# #_BRANCH_NAME# #-BRANCH_NAME# {BRANCH_NAME} {_BRANCH_NAME} {-BRANCH_NAME}", "master"));
+		Assert.AreEqual("master _master -master master _master -master", transformer.TransformConfigValue("#BRANCH_NAME# #_BRANCH_NAME# #-BRANCH-NAME# {BRANCH_NAME} {_BRANCH_NAME} {-BRANCH-NAME}", "master"));
 	}
 }

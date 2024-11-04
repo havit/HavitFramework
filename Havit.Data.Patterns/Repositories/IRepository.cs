b5e2fa87e00b9fa68eb29acb8ca3c9058729200d
Jiří Kanda <kanda@havit.cz>
2024-11-04 11:57:54 +0100

    EF Core: IRepository převedeno z Task na ValueTask

--- Havit.Data.Patterns/Repositories/IRepository.cs
+++ Havit.Data.Patterns/Repositories/IRepository.cs
@@ -16,7 +16,7 @@ public interface IRepository<TEntity>
 	/// Vrací instanci objektu dle Id.
 	/// </summary>
 	/// <exception cref="Havit.Data.Patterns.Exceptions.ObjectNotFoundException">Objekt s daným Id nebyl nalezen.</exception>
-	Task<TEntity> GetObjectAsync(int id, CancellationToken cancellationToken = default);
+	ValueTask<TEntity> GetObjectAsync(int id, CancellationToken cancellationToken = default);
 
 	/// <summary>
 	/// Vrací instance objektů dle Id.
@@ -26,7 +26,7 @@ public interface IRepository<TEntity>
 	/// <summary>
 	/// Vrací instance objektů dle Id.
 	/// </summary>
-	Task<List<TEntity>> GetObjectsAsync(int[] ids, CancellationToken cancellationToken = default);
+	ValueTask<List<TEntity>> GetObjectsAsync(int[] ids, CancellationToken cancellationToken = default);
 
 	/// <summary>
 	/// Vrací seznam všech (příznakem nesmazaných) objektů typu TEntity.
@@ -36,5 +36,5 @@ public interface IRepository<TEntity>
 	/// <summary>
 	/// Vrací seznam všech (příznakem nesmazaných) objektů typu TEntity.
 	/// </summary>
-	Task<List<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+	ValueTask<List<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
 }

326c876543ed7e62a773d4fd35c00d205d6c2f18
Jiří Kanda <kanda@havit.cz>
2025-01-27 15:44:30 +0100

    Havit.Extensions.DependencyInjection.SourceGenerators - modifikace unit testu pro využití "project reference" namísto "package reference".

--- Havit.Extensions.DependencyInjection.SourceGenerators.Tests/ServiceRegistrationsTests.Infrastucture.cs
+++ Havit.Extensions.DependencyInjection.SourceGenerators.Tests/ServiceRegistrationsTests.Infrastucture.cs
@@ -3,6 +3,7 @@ using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
 using Havit.Extensions.DependencyInjection.SourceGenerators.Tests.Infrastructure;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -15,7 +16,6 @@ public partial class ServiceRegistrationsTests
 		.Net
 		.Net80
 		.AddPackages(ImmutableArray.Create(
-			new PackageIdentity("Havit.Extensions.DependencyInjection.Abstractions", "2.0.20"),
 			new PackageIdentity("Microsoft.Extensions.DependencyInjection", "8.0.0"),
 			new PackageIdentity("Microsoft.Extensions.Hosting.Abstractions", "8.0.0")));
 
@@ -24,6 +24,16 @@ public partial class ServiceRegistrationsTests
 		var test = new Microsoft.CodeAnalysis.CSharp.Testing.CSharpSourceGeneratorTest<SourceGeneratorAdapter<ServiceRegistrationsGenerator>, Microsoft.CodeAnalysis.Testing.DefaultVerifier>
 		{
 			ReferenceAssemblies = Reference,
+			SolutionTransforms =
+			{
+				(solution, projectId) =>
+				{
+					var project = solution.GetProject(projectId);
+					project = project.AddMetadataReference(MetadataReference.CreateFromFile(typeof(Havit.Extensions.DependencyInjection.Abstractions.ServiceAttribute).Assembly.Location));
+
+					return project.Solution;
+				}
+			},
 			TestState =
 			{
 				Sources = { sourceInput },

89e818dc660879cd2338c092f4b67ae7bbbbc0a5
Jiří Kanda <kanda@havit.cz>
2025-09-05 08:54:54 +0200

    EFCore: QueryTagBuilder - memberName má nyní default z [CallerMemberName]

---
+++ Havit.Data.EntityFrameworkCore.Tests/QueryTagBuilderTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Havit.Data.EntityFrameworkCore.Tests;
+
+[TestClass]
+public class QueryTagBuilderTests
+{
+	[TestMethod]
+	public void QueryTagBuilder_CreateTag()
+	{
+		// Arrange
+		// Act + Assert
+		Assert.AreEqual("QueryTagBuilderTests.TestMember", QueryTagBuilder.CreateTag(typeof(QueryTagBuilderTests), "TestMember")); // explicit memberName
+		Assert.AreEqual("QueryTagBuilderTests", QueryTagBuilder.CreateTag(typeof(QueryTagBuilderTests), null)); // explicit memberName, but null
+		Assert.AreEqual("QueryTagBuilderTests.QueryTagBuilder_CreateTag", QueryTagBuilder.CreateTag(typeof(QueryTagBuilderTests))); // default value for memberName by CallerMemberNameAttribute
+	}
+}

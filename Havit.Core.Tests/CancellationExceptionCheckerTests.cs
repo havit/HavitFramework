ec93634e43fdf822e1b2e6d22993c29dafff8f7e
Jiří Kanda <kanda@havit.cz>
2026-01-19 09:56:01 +0100

    CancellationExceptionCheckerTests - úpravy pro vyšší bezpečnost testu (build server poskytl falešný výsledek) + uvedení výjimky v textu selhání testu

--- Havit.Core.Tests/CancellationExceptionCheckerTests.cs
+++ Havit.Core.Tests/CancellationExceptionCheckerTests.cs
@@ -16,48 +16,49 @@ public class CancellationExceptionCheckerTests
 	public async Task CancellationExceptionChecker_IsCancellationException_ReturnsTrueWhenSqlCommandCancelled(string culture)
 	{
 		// Arrange
-		SqlException sqlException = null;
-
-		// we need to simulate SqlException caused by command cancellation		
-		var sqlConnection = new SqlConnection("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=master;Application Name=Havit.Core.Tests;ConnectRetryCount=0");
-		await sqlConnection.OpenAsync(TestContext.CancellationToken);
-
-		try
+		using (var cultureInfoScope = CultureInfoExt.EnterScope(new System.Globalization.CultureInfo(culture))) // set localization
 		{
-			var sqlCommand = sqlConnection.CreateCommand();
+			SqlException sqlException = null;
 
-			sqlCommand.CommandText = "PRINT 1;"; // warm-up
-			sqlCommand.CommandTimeout = 3600 /* 1 hour */;
-			await sqlCommand.ExecuteNonQueryAsync(TestContext.CancellationToken);
-
-			sqlCommand.CommandText = "WAITFOR DELAY '01:00:00';"; // simulate a long-running query
-			CancellationTokenSource cts = CancellationTokenSource.CreateLinkedTokenSource(TestContext.CancellationToken);
-			cts.CancelAfter(10); // 10 milliseconds
+			// we need to simulate SqlException caused by command cancellation		
+			var sqlConnection = new SqlConnection("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=master;Application Name=Havit.Core.Tests;ConnectRetryCount=0");
+			await sqlConnection.OpenAsync(TestContext.CancellationToken);
 
 			try
 			{
-				using (var cultureInfoScope = CultureInfoExt.EnterScope(new System.Globalization.CultureInfo(culture))) // set localization
+				var sqlCommand = sqlConnection.CreateCommand();
+
+				sqlCommand.CommandText = "PRINT 1;"; // warm-up
+				sqlCommand.CommandTimeout = 3600 /* 1 hour */;
+				await sqlCommand.ExecuteNonQueryAsync(TestContext.CancellationToken);
+
+				sqlCommand.CommandText = "WAITFOR DELAY '01:00:00';"; // simulate a long-running query
+				CancellationTokenSource cts = CancellationTokenSource.CreateLinkedTokenSource(TestContext.CancellationToken);
+				cts.CancelAfter(10); // 10 milliseconds
+
+				try
 				{
 					await sqlCommand.ExecuteNonQueryAsync(cts.Token); // execute the sql command but cancel it after 10 ms
 				}
+				catch (SqlException catchedSqlException)
+				{
+					sqlException = catchedSqlException;
+				}
 			}
-			catch (SqlException catchedSqlException)
+			finally
 			{
-				sqlException = catchedSqlException;
-			}
-		}
-		finally
-		{
 #if NETFRAMEWORK
-			sqlConnection.Close();
+				sqlConnection.Close();
 #else
-			await sqlConnection.CloseAsync();
+				await sqlConnection.CloseAsync();
 #endif
+			}
+
+			Contract.Assert(sqlException != null);
+
+			// Act + Assert
+			Assert.IsTrue(CancellationExceptionChecker.IsCancellationException(sqlException),
+				message: $"SqlException was expected to be recognized as cancellation exception, but it was not. Exception: {sqlException}");
 		}
-
-		Contract.Assert(sqlException != null);
-
-		// Act + Assert
-		Assert.IsTrue(CancellationExceptionChecker.IsCancellationException(sqlException));
 	}
 }

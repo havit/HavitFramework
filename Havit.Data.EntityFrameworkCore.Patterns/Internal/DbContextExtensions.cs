f7b5cf859f45c663fa746edfbc0372c1e2e945c3
Jiří Kanda <kanda@havit.cz>
2026-01-28 16:03:59 +0100

    Task #88853: DbDataLoader dělá chunks i když nejsou potřeba (fix non SQL Server providers)

--- Havit.Data.EntityFrameworkCore.Patterns/Internal/DbContextExtensions.cs
+++ Havit.Data.EntityFrameworkCore.Patterns/Internal/DbContextExtensions.cs
@@ -9,13 +9,14 @@ internal static class DbContextExtensions
 {
 	internal const int ContainsChunkSize = 5_000;
 
-	internal static bool SupportsSqlServerOpenJson(this IDbContext dbContext)
+	private static bool SupportsSqlServerOpenJson(this IDbContext dbContext)
 	{
 		ArgumentNullException.ThrowIfNull(dbContext);
 
 		// Podmínka vychází z Microsoft.EntityFrameworkCore.SqlServer.Query.Internal.SqlServerQueryableMethodTranslatingExpressionVisitor,
 		// metoda TranslatePrimitiveCollection.
 		// https://github.com/dotnet/efcore/blob/931a67c0d6dc1738faf0b2ecf04f242a3789c4e7/src/EFCore.SqlServer/Query/Internal/SqlServerQueryableMethodTranslatingExpressionVisitor.cs#L127
+		// Vyžaduje SQL Server provider!
 		var sqlServerSingletonOptions = (dbContext as Microsoft.EntityFrameworkCore.DbContext)?.GetService<ISqlServerSingletonOptions>();
 
 		return (sqlServerSingletonOptions == null)
@@ -23,7 +24,7 @@ internal static class DbContextExtensions
 			: (sqlServerSingletonOptions.EngineType == SqlServerEngineType.SqlServer) && (sqlServerSingletonOptions.SqlServerCompatibilityLevel >= 130);
 	}
 
-	internal static ParameterTranslationMode? GetParameterTranslationMode(this IDbContext dbContext)
+	private static ParameterTranslationMode? GetParameterTranslationMode(this IDbContext dbContext)
 	{
 		RelationalOptionsExtension sqlServerOptionsExtension = null;
 		var dboContextOptions = (dbContext as Microsoft.EntityFrameworkCore.DbContext)?.GetService<DbContextOptions>();
@@ -40,8 +41,15 @@ internal static class DbContextExtensions
 	/// <summary>
 	/// Vrací true, pokud je potřeba použít chunking při načítání dat s podmínkou Contains (IN (...)).
 	/// </summary>
-	internal static bool ShouldUseChunkingForContainsCondition(this IDbContext _dbContext, int valuesToLoadCount, out int chunkSize)
+	internal static bool ShouldUseChunkingForContainsCondition(this IDbContext dbContext, int valuesToLoadCount, out int chunkSize)
 	{
+		chunkSize = ContainsChunkSize;
+
+		if (!dbContext.Database.IsSqlServer())
+		{
+			return false;
+		}
+
 		// +--------------------------+-----------------------+-----------------------+--------------------------+-----------------------------------------+
 		// | ParameterTranslationMode | OPENJSON 200 záznamů  | OPENJSON 5000 záznamů | bez openjson 200 záznamů | bez openjson 5000 záznamů               |
 		// +--------------------------+-----------------------+-----------------------+--------------------------+-----------------------------------------+
@@ -57,10 +65,9 @@ internal static class DbContextExtensions
 		// budou data rozdělena na chunks a každý chunks se načte s jedním WHERE x >= min AND x <= max (bez použití IN (...)).
 		// Považujeme to za okrajový scénář, který neřešíme.
 
-		bool supportSqlServerOpenJson = _dbContext.SupportsSqlServerOpenJson();
-		var parameterTranslationMode = _dbContext.GetParameterTranslationMode();
+		bool supportSqlServerOpenJson = dbContext.SupportsSqlServerOpenJson();
+		var parameterTranslationMode = dbContext.GetParameterTranslationMode();
 
-		chunkSize = ContainsChunkSize;
 
 		// Pokud není OPENJSON a je použit ParameterTranslationMode.Parameter, EF Core
 		// použije IN (@p1, @p2, @p3) bez ohledu na počet parametrů. Potřebujeme použít maximálně 2100 parametrů.

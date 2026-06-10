e86b9c03afeb041808a0309fd19746e19fb09751
Jiří Kanda <kanda@havit.cz>
2026-06-10 18:55:26 +0200

    Kosmetika kódu

--- Havit.Services.Sftp.Tests/Infrastructure/SftpContainerService.cs
+++ Havit.Services.Sftp.Tests/Infrastructure/SftpContainerService.cs
@@ -1,5 +1,4 @@
 ﻿using DotNet.Testcontainers.Builders;
-using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
 
 namespace Havit.Services.Sftp.Tests.Infrastructure;
@@ -15,7 +14,7 @@ internal class SftpContainerService
 	{
 		// Není použit atmoz/sftp:alpine z Testcontainers.Sftp, protože neumí nastavit oprávnění pro zápis do rootové složky.
 		s_SftpContainer = new ContainerBuilder("emberstack/sftp:latest")
-			.WithResourceMapping(new FileInfo("sftp.json"), new FileInfo("/app/config/sftp.json")) 
+			.WithResourceMapping(new FileInfo("sftp.json"), new FileInfo("/app/config/sftp.json"))
 			.WithPortBinding(22, true) // SFTP
 			.WithWaitStrategy(Wait.ForUnixContainer().UntilInternalTcpPortIsAvailable(22))
 			.Build();

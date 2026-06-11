275069d661912dcf067424e9c199e2f073e93f69
Jiří Kanda <kanda@havit.cz>
2026-06-11 18:33:33 +0200

    EF Core: Korekce času timeoutu v DbLockerCriticalSection

--- Havit.Data.EntityFrameworkCore/Threading/DbLockedCriticalSection.cs
+++ Havit.Data.EntityFrameworkCore/Threading/DbLockedCriticalSection.cs
@@ -7,7 +7,7 @@ namespace Havit.Data.EntityFrameworkCore.Threading;
 public class DbLockedCriticalSection : IDbLockedCriticalSection
 {
 	private const int LockTimeoutMilliseconds = 10 * 60 * 1000 /* ms*/; // 10 minut - maximální doba čekání na získání zámku
-	private const int GetLockCommandTimeoutSeconds = 15 /* seconds */; // s rezervou nad LockTimeoutMilliseconds, aby vypršení čekání na zámek skončilo result codem Timeout (a tedy DbLockedCriticalSectionException), nikoliv SqlException z timeoutu commandu
+	private const int GetLockCommandTimeoutSeconds = (LockTimeoutMilliseconds / 1000) + 30 /* seconds */; // s rezervou nad LockTimeoutMilliseconds, aby vypršení čekání na zámek skončilo result codem Timeout (a tedy DbLockedCriticalSectionException), nikoliv SqlException z timeoutu commandu
 
 	private readonly Func<SqlConnection> _sqlConnectionFactory;
 	private readonly bool _ownsConnection;
@@ -251,50 +251,82 @@ public class DbLockedCriticalSection : IDbLockedCriticalSection
 
 	private void ReleaseLock(string lockValue, DbConnection sqlConnection)
 	{
-		if (sqlConnection.State == System.Data.ConnectionState.Closed)
+		if ((sqlConnection.State == System.Data.ConnectionState.Closed) || (sqlConnection.State == System.Data.ConnectionState.Broken))
 		{
-			// Pokud je spojení zavřené, nemůžeme na něm spouštět dotazy do databáze.
-			// Pokud je spojení zavřené, jsme si jisti, že zámek byl uvolněn, takže nemusíme nic dělat.
+			// Pokud je spojení zavřené (či rozbité), nemůžeme na něm spouštět dotazy do databáze.
+			// Pokud je spojení zavřené (či rozbité), jsme si jisti, že zámek byl uvolněn (zámek je vlastněn session), takže nemusíme nic dělat.
 			// JK: Jak se stane, že je spojení zavřené, je mi záhadou. Stává se jen v testech na build serveru, jinak se nepodařilo zreprodukovat.
 			return;
 		}
 
-		using (DbCommand command = ReleaseLock_PrepareCommand(lockValue, sqlConnection))
+		using (DbCommand command = ReleaseLock_PrepareCommand(lockValue, sqlConnection, out DbParameter resultCodeSqlParameter))
 		{
 			command.ExecuteNonQuery();
+			ReleaseLock_VerifyResultCode(ReleaseLock_GetResultCode(resultCodeSqlParameter), lockValue);
 		}
 	}
 
 	private async Task ReleaseLockAsync(string lockValue, DbConnection sqlConnection)
 	{
-		if (sqlConnection.State == System.Data.ConnectionState.Closed)
+		if ((sqlConnection.State == System.Data.ConnectionState.Closed) || (sqlConnection.State == System.Data.ConnectionState.Broken))
 		{
-			// Pokud je spojení zavřené, nemůžeme na něm spouštět dotazy do databáze.
-			// Pokud je spojení zavřené, jsme si jisti, že zámek byl uvolněn, takže nemusíme nic dělat.
+			// Pokud je spojení zavřené (či rozbité), nemůžeme na něm spouštět dotazy do databáze.
+			// Pokud je spojení zavřené (či rozbité), jsme si jisti, že zámek byl uvolněn (zámek je vlastněn session), takže nemusíme nic dělat.
 			// JK: Jak se stane, že je spojení zavřené, je mi záhadou. Stává se jen v testech na build serveru, jinak se nepodařilo zreprodukovat.
 			return;
 		}
 
-		using (DbCommand command = ReleaseLock_PrepareCommand(lockValue, sqlConnection))
+		using (DbCommand command = ReleaseLock_PrepareCommand(lockValue, sqlConnection, out DbParameter resultCodeSqlParameter))
 		{
 			await command.ExecuteNonQueryAsync(CancellationToken.None).ConfigureAwait(false);
+			ReleaseLock_VerifyResultCode(ReleaseLock_GetResultCode(resultCodeSqlParameter), lockValue);
 		}
 	}
 
-	private DbCommand ReleaseLock_PrepareCommand(string lockValue, DbConnection sqlConnection)
+	private DbCommand ReleaseLock_PrepareCommand(string lockValue, DbConnection sqlConnection, out DbParameter resultCodeSqlParameter)
 	{
 		DbCommand sqlCommand = sqlConnection.CreateCommand();
 		sqlCommand.CommandType = System.Data.CommandType.Text;
-		sqlCommand.CommandText = "EXEC sp_releaseapplock @Resource, 'Session'";
+		sqlCommand.CommandText = "EXEC @ResultCode = sp_releaseapplock @Resource, 'Session'";
 
 		DbParameter resourceParameter = sqlCommand.CreateParameter();
 		resourceParameter.ParameterName = "@Resource";
 		resourceParameter.Value = lockValue;
 
+		resultCodeSqlParameter = sqlCommand.CreateParameter();
+		resultCodeSqlParameter.ParameterName = "@ResultCode";
+		resultCodeSqlParameter.DbType = System.Data.DbType.Int32;
+		resultCodeSqlParameter.Direction = System.Data.ParameterDirection.Output;
+
 		sqlCommand.Parameters.Add(resourceParameter);
+		sqlCommand.Parameters.Add(resultCodeSqlParameter);
 
 		sqlCommand.CommandTimeout = 10 * 60; // 10 minut
 		return sqlCommand;
+	}
+
+	private static SpReleaseAppLockResultCode ReleaseLock_GetResultCode(DbParameter resultCodeSqlParameter)
+	{
+		// při selhání commandu nemusí být output parametr nastaven (DBNull)
+		if (resultCodeSqlParameter.Value is int resultCode)
+		{
+			return (SpReleaseAppLockResultCode)resultCode;
+		}
+
+		throw new InvalidOperationException("sp_releaseapplock did not return a result code.");
+	}
+
+	private void ReleaseLock_VerifyResultCode(SpReleaseAppLockResultCode releaseAppLockResultCode, string lockValue)
+	{
+		switch (releaseAppLockResultCode)
+		{
+			case SpReleaseAppLockResultCode.Released:
+				return;
+			case SpReleaseAppLockResultCode.Error:
+				throw new DbLockedCriticalSectionException($"Unable to release lock for resource '{lockValue}'. Result code: '{Enum.GetName(typeof(SpReleaseAppLockResultCode), releaseAppLockResultCode)}'");
+			default:
+				throw new InvalidOperationException($"Unknown SpReleaseAppLockResultCode: {Enum.GetName(typeof(SpReleaseAppLockResultCode), releaseAppLockResultCode)}");
+		}
 	}
 
 	/// <summary>
